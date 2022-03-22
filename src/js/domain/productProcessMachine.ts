e77cb2acc5760a5f07dc621ab0d87ed1c82056c8
Hwang Jung Min <hwangstar156@gmail.com>
2022-03-23 00:13:32 +0900

    feat: 상품을 관리하는 도메인 로직 추가

---
+++ src/js/domain/productProcessMachine.ts
@@ -0,0 +1,66 @@
+import {
+  ProductDomain,
+  Update,
+  Add,
+  Delete,
+  GetProducts,
+} from "../interface/product.interface";
+
+class ProductProcessMachine implements ProductDomain {
+  products = [];
+
+  add: Add = (newProduct) => {
+    this.checkNameLength(newProduct.name);
+    this.checkValidPrice(newProduct.price);
+    this.checkValidCount(newProduct.count);
+
+    this.products.push(newProduct);
+  };
+
+  getProducts: GetProducts = () => {
+    return this.products;
+  };
+
+  update: Update = (idx, name, price, count) => {
+    name && this.checkNameLength(name);
+    price && this.checkValidPrice(price);
+    count && this.checkValidCount(count);
+    this.updateStatus(idx, name, "name");
+    this.updateStatus(idx, price, "price");
+    this.updateStatus(idx, count, "count");
+  };
+
+  delete: Delete = (idx) => {
+    this.products.splice(idx, 1);
+  };
+
+  updateStatus = (idx: number, status: number | string, key: string) => {
+    this.products[idx][`${key}`] = status
+      ? status
+      : this.products[idx][`${key}`];
+  };
+
+  checkNameLength = (name: string): void => {
+    if (name.length > 10) {
+      throw new Error("상품명은 10자이하로 입력해주세요");
+    }
+  };
+
+  checkValidPrice = (price: number): void => {
+    if (price < 100 || price > 10000 || price % 10 !== 0) {
+      throw new Error("유효한 가격을 입력해주세요");
+    }
+  };
+
+  checkValidCount = (count: number): void => {
+    if (count <= 0) {
+      throw new Error("추가하는 수량은 0이하가 될수가 없습니다.");
+    }
+
+    if (count > 20) {
+      throw new Error("수량은 최대 20개까지 추가 가능합니다.");
+    }
+  };
+}
+
+export default ProductProcessMachine;
