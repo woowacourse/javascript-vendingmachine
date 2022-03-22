29d2ec10eaa5866cac8917fe43af71c7b25c9557
intae92 <kangit2013@gmail.com>
2022-03-22 16:35:11 +0900

    feat: 잔동 충전 기능 구현

--- src/js/model/VendingMachine.ts
+++ src/js/model/VendingMachine.ts
@@ -14,10 +14,12 @@ interface Product {
 class VendingMachine {
   products: Array<Product>; // name amount price
   changes: Coin; // 자판기가 보유하고 있는 돈 = 잔돈
+  totalMoney: number;
 
   constructor() {
     this.products = [];
     this.changes = { coin10: 0, coin50: 0, coin100: 0, coin500: 0 };
+    this.totalMoney = 0;
   }
 
   addProduct(product: Product) {
@@ -48,5 +50,43 @@ class VendingMachine {
   modifyProduct(oldProduct: Product, newProduct: Product) {
     const oldProductIndex = this.findProductIndex(oldProduct.name);
     this.products[oldProductIndex] = newProduct;
+  }
+
+  inputChanges(money: number) {
+    // 1. 돈이 10원으로 나누어지는지 -> 어디에 로직을 둘것인지 킵
+
+    if (money > 100000) {
+      alert('자판기가 보유할 수 있는 최대 금액은 100,000원 입니다.');
+      return;
+    }
+
+    this.totalMoney += money;
+
+    const coin = this.getChangeCoin(money);
+
+    switch (coin) {
+      case 500:
+        this.changes.coin500 += 1;
+        break;
+      case 100:
+        this.changes.coin100 += 1;
+        break;
+      case 50:
+        this.changes.coin50 += 1;
+        break;
+      case 10:
+        this.changes.coin10 += 1;
+        break;
+    }
+  }
+
+  getRandomInt(max: number) {
+    return Math.floor(Math.random() * max + 1); // 0 ~ max
+  }
+
+  getChangeCoin(money: number) {
+    const coins = [500, 100, 50, 10].filter(coin => coin <= money);
+    const index = this.getRandomInt(coins.length - 1);
+    return coins[index];
   }
 }
