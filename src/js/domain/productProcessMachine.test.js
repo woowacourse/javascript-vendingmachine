942c5b0fd6761118a0488075112cf256b06cb00d
Hwang Jung Min <hwangstar156@gmail.com>
2022-03-23 00:14:09 +0900

    test: 상품을 관리하는 도메인에 관한 테스트 코드 추가

---
+++ src/js/domain/productProcessMachine.test.js
@@ -0,0 +1,95 @@
+import ProductProcessMachine from "./ProductProcessMachine";
+
+describe("상품 관리하는 도메인 테스트", () => {
+  test("상품명, 가격, 수량을 입력해 상품을 추가할 수 있다.", () => {
+    const productProcessMachine = new ProductProcessMachine();
+    productProcessMachine.add({ name: "호프", price: 110, count: 2 });
+    productProcessMachine.add({ name: "스밍", price: 9990, count: 1 });
+
+    expect(productProcessMachine.getProducts()).toStrictEqual([
+      { name: "호프", price: 110, count: 2 },
+      { name: "스밍", price: 9990, count: 1 },
+    ]);
+  });
+
+  test("상품명은 최대 10글자이상일 경우 에러를 던진다", () => {
+    const productProcessMachine = new ProductProcessMachine();
+    expect(() => {
+      productProcessMachine.add({
+        name: "스밍스밍스밍스밍스밍스밍",
+        price: 100,
+        count: 1,
+      });
+    }).toThrowError("상품명은 10자이하로 입력해주세요");
+  });
+
+  test("상품가격이 유효하지 않는다면 에러를 던진다.", () => {
+    const productProcessMachine = new ProductProcessMachine();
+    expect(() => {
+      productProcessMachine.add({
+        name: "호프",
+        price: 90,
+        count: 1,
+      });
+    }).toThrowError("유효한 가격을 입력해주세요");
+
+    expect(() => {
+      productProcessMachine.add({
+        name: "스밍",
+        price: 10010,
+        count: 1,
+      });
+    }).toThrowError("유효한 가격을 입력해주세요");
+
+    expect(() => {
+      productProcessMachine.add({
+        name: "스밍",
+        price: 9999,
+        count: 1,
+      });
+    }).toThrowError("유효한 가격을 입력해주세요");
+  });
+
+  test("한 제품의 수량이 20개 이상일 경우 에러를 던진다.", () => {
+    const productProcessMachine = new ProductProcessMachine();
+
+    expect(() => {
+      productProcessMachine.add({
+        name: "호오프",
+        price: 2000,
+        count: 0,
+      });
+    }).toThrowError("추가하는 수량은 0이하가 될수가 없습니다.");
+    expect(() => {
+      productProcessMachine.add({
+        name: "호오프",
+        price: 2000,
+        count: 21,
+      });
+    }).toThrowError("수량은 최대 20개까지 추가 가능합니다.");
+  });
+
+  test("제품의 정보를 수정 할 수 있다.", () => {
+    const productProcessMachine = new ProductProcessMachine();
+    productProcessMachine.add({ name: "호프", price: 110, count: 2 });
+    productProcessMachine.add({ name: "스밍", price: 9990, count: 1 });
+
+    productProcessMachine.update(0, "밝아진 호프", null, 4);
+    expect(productProcessMachine.getProducts()[0]).toStrictEqual({
+      name: "밝아진 호프",
+      price: 110,
+      count: 4,
+    });
+  });
+
+  test("제품을 삭제할수 있다.", () => {
+    const productProcessMachine = new ProductProcessMachine();
+    productProcessMachine.add({ name: "호프", price: 110, count: 2 });
+    productProcessMachine.add({ name: "스밍", price: 9990, count: 1 });
+
+    productProcessMachine.delete(0);
+    expect(productProcessMachine.getProducts()).toStrictEqual([
+      { name: "스밍", price: 9990, count: 1 },
+    ]);
+  });
+});
