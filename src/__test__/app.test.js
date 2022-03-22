d81d5a775f3e7e461e84e2b57ad11d8f6bbb96a2
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-03-22 18:29:08 +0900

    feat: 상품명, 가격, 수량에 대한 예외처리
    detail:
    - 상품명은 최대 10글자
    - 상품가격은 100원 이상, 10,000원 미만
    - 상품가격은 10원단위
    - 상품갯수는 최대 20개까지
    
    Co-authored-by: sangminsong <ssmin940606@gmail.com>

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -14,6 +14,51 @@ test('상품명, 가격, 수량을 입력하여 물품을 등록할 수 있다.'
   });
 });
 
+test('상품명은 최대 10글자까지 가능하다.', () => {
+  const productCatalog = new ProductCatalog();
+  try {
+    productCatalog.addProduct('코카콜라맛있다맛있으면또먹지딩동댕동', 1000, 20);
+  } catch (err) {
+    expect(err).toStrictEqual(new Error('10글자 미만의 이름을 넣어주세요~'));
+  }
+});
+
+test('상품가격은 100원이상 이어야 한다.', () => {
+  const productCatalog = new ProductCatalog();
+  try {
+    productCatalog.addProduct('코카콜라', 10, 20);
+  } catch (err) {
+    expect(err).toStrictEqual(new Error('100원 이상, 10,000원 이하의 돈을 넣어주세요~'));
+  }
+});
+
+test('상품가격은 10,000원 이하 이어야 한다.', () => {
+  const productCatalog = new ProductCatalog();
+  try {
+    productCatalog.addProduct('코카콜라', 1000000, 20);
+  } catch (err) {
+    expect(err).toStrictEqual(new Error('100원 이상, 10,000원 이하의 돈을 넣어주세요~'));
+  }
+});
+
+test('상품가격은 10원 단위여야 한다', () => {
+  const productCatalog = new ProductCatalog();
+  try {
+    productCatalog.addProduct('코카콜라', 153, 20);
+  } catch (err) {
+    expect(err).toStrictEqual(new Error('10원단위로 돈을 넣어주세요~'));
+  }
+});
+
+test('상품수량은 최대 20개까지 가능하다', () => {
+  const productCatalog = new ProductCatalog();
+  try {
+    productCatalog.addProduct('코카콜라', 1000, 2000);
+  } catch (err) {
+    expect(err).toStrictEqual(new Error('상품수량은 최대 20개까지만 가능합니다~'));
+  }
+});
+
 test('상품을 수정할 수 있다.', () => {
   const product = new Product('코카콜라', 1000, 20);
 
--- src/domain/Product.ts
+++ src/domain/Product.ts
@@ -4,21 +4,61 @@ export class Product {
   private quantity: number;
 
   constructor(name: string, price: number, quantity: number) {
-    this.name = name;
-    this.price = price;
-    this.quantity = quantity;
+    this.setName(name);
+    this.setPrice(price);
+    this.setQuantity(quantity);
   }
 
   setName(name: string) {
-    this.name = name;
+    try {
+      this.validateName(name);
+      this.name = name;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  validateName(name: string) {
+    if (name.length > 10) {
+      throw new Error('10글자 미만의 이름을 넣어주세요~');
+    }
+    return;
   }
 
   setPrice(price: number) {
-    this.price = price;
+    try {
+      this.validatePrice(price);
+      this.price = price;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  validatePrice(price: number) {
+    if (price < 100 || price > 10000) {
+      throw new Error('100원 이상, 10,000원 이하의 돈을 넣어주세요~');
+    }
+    if (price % 10 !== 0) {
+      throw new Error('10원단위로 돈을 넣어주세요~');
+    }
+    return;
   }
 
   setQuantity(quantity: number) {
-    this.quantity = quantity;
+    try {
+      this.validateQuantity(quantity);
+      this.quantity = quantity;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  validateQuantity(quantity: number) {
+    if (quantity > 20) {
+      throw new Error('상품수량은 최대 20개까지만 가능합니다~');
+    }
+
+    return;
   }
 
   getName() {
