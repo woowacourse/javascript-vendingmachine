bcaaee48b0ca259a1d38273c9be5dee7faeadc65
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-23 16:27:18 +0900

    feat: 상품 관리 영역 구현
    Co-authored-by: Soyi Jeon <soyi.dev@gmail.com>

--- src/js/domain/Store/ProductManager.ts
+++ src/js/domain/Store/ProductManager.ts
@@ -12,16 +12,58 @@ interface IProductManager {
   state: IState;
   subscribers: Array<object>;
 
-  addSubscriber() : void;
+  addSubscriber(subscriber: object) : void;
   setState(newState: IState): void;
 
   addProduct(name: string, price: number, quantity: number): void;
   updateProduct(name: string, price: number, quantity: number): void;
   removeProductByName(name: string): void;
-  findProductByName(name: string): IProduct;
+  findProductIndexByName(name: string): number;
 }
 
 class ProductManager implements IProductManager {
+  state = {
+    products: [],
+  };
+
+  subscribers = [];
+
+  addSubscriber(subscriber: object) {
+    this.subscribers.push(subscriber);
+  }
+
+  setState(newState: IState) {
+    this.state = { ...this.state, ...newState };
+    this.subscribers.forEach(subscriber => subscriber(this.state));
+  }
+
+  addProduct(name: string, price: number, quantity: number) {
+    this.setState({
+      products: [...this.state.products, { name, price, quantity }],
+    });
+  }
+
+  updateProduct(name: string, price: number, quantity: number) {
+    const targetIndex = this.findProductIndexByName(name);
+    const updateProducts = [...this.state.products].splice(targetIndex, 1, { name, price, quantity });
+
+    this.setState({
+      products: updateProducts,
+    });
+  }
+
+  removeProductByName(name: string) {
+    const targetIndex = this.findProductIndexByName(name);
+    const updateProducts = [...this.state.products].splice(targetIndex, 1);
+
+    this.setState({
+      products: updateProducts,
+    });
+  }
+
+  findProductIndexByName(name: string): number {
+    return this.state.products.findIndex((product) => product.name === name);
+  }
 }
 
 export default new ProductManager();
