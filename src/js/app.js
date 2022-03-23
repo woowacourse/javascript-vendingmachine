8c984739e6444aec14795b6761bfae1078d02e35
Hwang Jung Min <hwangstar156@gmail.com>
2022-03-23 21:38:44 +0900

    feat: 해쉬뱅을 이용하여 spa 기능 추가

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,39 @@
 import ProductModerator from "./moderator/productModerator";
+import ChangesModerator from "./moderator/changesModerator";
 
-const productModerator = new ProductModerator();
-productModerator.init();
+class App {
+  constructor() {
+    this.productModerator = new ProductModerator();
+    this.changesModerator = new ChangesModerator();
+
+    this.$nav = document.querySelector("#page-tab-container");
+    this.$nav.addEventListener("click", this.onClickNavButton);
+    window.addEventListener("hashchange", this.onChangePage);
+
+    this.onChangePage();
+  }
+
+  onClickNavButton = ({ target }) => {
+    if (target.classList.contains("product-management-button")) {
+      this.productModerator.init();
+    }
+
+    if (target.classList.contains("changes-charge-button")) {
+      this.changesModerator.init();
+    }
+  };
+
+  onChangePage = () => {
+    const hash = location.hash;
+
+    if (hash === "#!productManagement") {
+      this.productModerator.init();
+    }
+
+    if (hash === "#!changesCharge") {
+      this.changesModerator.init();
+    }
+  };
+}
+
+new App();
