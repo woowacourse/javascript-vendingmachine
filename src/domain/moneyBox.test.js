59bddf98be099053229dc0ed3bf92c73f87d6ce0
uk960214 <uk960214@gmail.com>
2022-03-23 20:31:39 +0900

    refactor: 돈통 클래스 테스트 리팩토링
    Co-authored-by: lokba <lokba@users.noreply.github.com>
    Co-authored-by: JUDONGHYEOK <judonghyeok@gmail.com>

--- src/domain/moneyBox.test.js
+++ src/domain/moneyBox.test.js
@@ -2,10 +2,11 @@
 import MoneyBox from './MoneyBox';
 
 describe('돈통 클래스 테스트', () => {
-  test('금액을 투입했을 때 금액에 맞는 동전을 충전할 수 있다.', () => {
-    const inputMoney = 3000;
+  let moneyBox;
+  let mockCoinStatusList;
 
-    const mockCoinStatusList = [
+  function setMockDistributeStrategy() {
+    mockCoinStatusList = [
       { name: 'FIVE_HUNDRED_WON', value: 500, count: 5 },
       { name: 'ONE_HUNDRED_WON', value: 100, count: 4 },
       { name: 'FIFTY_WON', value: 50, count: 1 },
@@ -17,8 +18,18 @@ describe('돈통 클래스 테스트', () => {
         return mockCoinStatusList;
       },
     };
-    const moneyBox = new MoneyBox();
+
     moneyBox.strategy = mockCoinDistributeStrategy;
+  }
+
+  beforeEach(() => {
+    moneyBox = new MoneyBox();
+  });
+
+  test('금액을 투입했을 때 금액에 맞는 동전을 충전할 수 있다.', () => {
+    const inputMoney = 3000;
+
+    setMockDistributeStrategy();
     moneyBox.charge(inputMoney);
 
     expect(moneyBox.coinStatusList).toEqual(mockCoinStatusList);
@@ -26,44 +37,18 @@ describe('돈통 클래스 테스트', () => {
 
   test('금액을 투입하고 총 보유 금액을 확인할 수 있다.', () => {
     const inputMoney = 3000;
-
-    const mockCoinStatusList = [
-      { name: 'FIVE_HUNDRED_WON', value: 500, count: 5 },
-      { name: 'ONE_HUNDRED_WON', value: 100, count: 4 },
-      { name: 'FIFTY_WON', value: 50, count: 1 },
-      { name: 'TEN_WON', value: 10, count: 5 },
-    ];
-
-    const mockCoinDistributeStrategy = {
-      distribute() {
-        return mockCoinStatusList;
-      },
-    };
-    const moneyBox = new MoneyBox();
-    moneyBox.strategy = mockCoinDistributeStrategy;
     moneyBox.charge(inputMoney);
+
     expect(moneyBox.totalAmount).toEqual(inputMoney);
   });
 
   test('금액을 추가 투입하면 총 보유 금액에 더해진다.', () => {
-    const inputMoney = 3000;
+    const firstInputMoney = 3000;
+    moneyBox.charge(firstInputMoney);
 
-    const mockCoinStatusList = [
-      { name: 'FIVE_HUNDRED_WON', value: 500, count: 5 },
-      { name: 'ONE_HUNDRED_WON', value: 100, count: 4 },
-      { name: 'FIFTY_WON', value: 50, count: 1 },
-      { name: 'TEN_WON', value: 10, count: 5 },
-    ];
+    const secondInputMoney = 2000;
+    moneyBox.charge(secondInputMoney);
 
-    const mockCoinDistributeStrategy = {
-      distribute() {
-        return mockCoinStatusList;
-      },
-    };
-    const moneyBox = new MoneyBox();
-    moneyBox.strategy = mockCoinDistributeStrategy;
-    moneyBox.charge(inputMoney);
-    moneyBox.charge(inputMoney);
-    expect(moneyBox.totalAmount).toEqual(inputMoney * 2);
+    expect(moneyBox.totalAmount).toEqual(firstInputMoney + secondInputMoney);
   });
 });
