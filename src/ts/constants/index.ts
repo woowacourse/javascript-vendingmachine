abed4262ce757b58a62eeaf41e9cbc85507ac703
이아현 <lah1203@naver.com>
2022-03-23 15:32:40 +0900

    feat: 상품 가격 유효성 검사 기능 구현
    Co-authored-by: 김의진 <gytks4@naver.com>

--- src/ts/constants/index.ts
+++ src/ts/constants/index.ts
@@ -1,8 +1,18 @@
 const MAX_NAME_LENGTH = 10;
+const PRICE_RULE = {
+  MIN: 100,
+  MAX: 10000,
+  UNIT: 10,
+};
 
 const MESSAGE = {
   ERROR_SAME_PRODUCT: '이미 동일한 이름의 상품이 존재합니다.',
   ERROR_OVER_MAX_LENGTH: `상품명을 ${MAX_NAME_LENGTH}글자 이하로 입력해주세요.`,
+  ERROR_INVALID_PRICE: `가격은 ${
+    PRICE_RULE.MIN
+  }원 이상 ${PRICE_RULE.MAX.toLocaleString()}원 이하여야 하며, ${
+    PRICE_RULE.UNIT
+  }으로 나누어 떨어져야 합니다.`,
 };
 
 export { MESSAGE, MAX_NAME_LENGTH };
--- src/ts/utils/validator.ts
+++ src/ts/utils/validator.ts
@@ -6,26 +6,41 @@ const hasSameProduct = (products: ProductImpl[], newProduct: ProductInfo) => {
   return products.some(productInfo => productInfo.name === newProduct.name);
 };
 
-const isOverMaxLength = name => {
+const isOverMaxLength = (name: string) => {
   return name.length > MAX_NAME_LENGTH;
 };
+
+const isInvalidPrice = (price: number) => {
+  const isRanged = price >= 100 && price <= 10000;
+  const isDivisible = price % 10 === 0;
+  return !(isRanged && isDivisible);
+};
+
+const productInfoValidator = (
+  products: ProductImpl[],
+  newProduct: ProductInfo,
+) => [
+  {
+    test: hasSameProduct(products, newProduct),
+    errorMsg: MESSAGE.ERROR_SAME_PRODUCT,
+  },
+  {
+    test: isOverMaxLength(newProduct.name),
+    errorMsg: MESSAGE.ERROR_OVER_MAX_LENGTH,
+  },
+  {
+    test: isInvalidPrice(newProduct.price),
+    errorMsg: MESSAGE.ERROR_INVALID_PRICE,
+  },
+];
 
 const validateProductInfo = (
   products: ProductImpl[],
   newProduct: ProductInfo,
 ) => {
-  const productInfoValidator = [
-    {
-      test: hasSameProduct(products, newProduct),
-      errorMsg: MESSAGE.ERROR_SAME_PRODUCT,
-    },
-    {
-      test: isOverMaxLength(newProduct.name),
-      errorMsg: MESSAGE.ERROR_OVER_MAX_LENGTH,
-    },
-  ];
+  const validator = productInfoValidator(products, newProduct);
 
-  productInfoValidator.every(({ test, errorMsg }) => {
+  validator.every(({ test, errorMsg }) => {
     if (test) throw new Error(errorMsg);
     return true;
   });
