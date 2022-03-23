45edb34db9b8dd07d20a3aea85635f15972210ff
InKyoJeong <viapolar@naver.com>
2022-03-23 16:36:25 +0900

    feat: 메뉴 탭 구현 Co-authored-by: yunjin-kim <yunjin-kim@users.noreply.github.com>

---
+++ src/ts/components/App.ts
@@ -0,0 +1,46 @@
+import { $ } from "../utils";
+import Charge from "./charge/Charge";
+import MenuTab from "./menuTab/MenuTab";
+import Product from "./product/Product";
+
+class App {
+  app: HTMLElement;
+  menuTab: MenuTab;
+  charge: Charge;
+  product: Product;
+
+  constructor() {
+    this.app = $("#app");
+    this.app.insertAdjacentHTML(
+      "beforeend",
+      `<nav class="nav"> 
+        <button type="button" class="button nav__button button-click" data-menu="/product">
+        상품 관리
+        </button> 
+        <button type="button" class="button nav__button" data-menu="/charge">
+        잔돈 충전
+        </button> 
+       </nav>
+       <main class="main"></main>`
+    );
+
+    this.mountComponent();
+  }
+
+  mountComponent() {
+    this.menuTab = new MenuTab({ convertTemplate: this.convertTemplate });
+    this.charge = new Charge();
+    this.product = new Product();
+  }
+
+  convertTemplate = (path: string) => {
+    const routes = {
+      "/charge": () => this.charge.render(),
+      "/product": () => this.product.render(),
+    };
+
+    routes[path]();
+  };
+}
+
+export default App;
---
+++ src/ts/components/menuTab/MenuTab.ts
@@ -0,0 +1,32 @@
+import { $, addEvent } from "../../utils";
+
+type ConvertTemplate = (path: string) => void;
+
+class MenuTab {
+  convertTemplate: ConvertTemplate;
+  app: HTMLElement;
+
+  constructor({ convertTemplate }) {
+    this.convertTemplate = convertTemplate;
+
+    this.app = $("#app");
+    addEvent(this.app, "click", this.handleMenuTab);
+  }
+
+  handleMenuTab = (e) => {
+    e.preventDefault();
+    if (!e.target.classList.contains("nav__button")) {
+      return;
+    }
+
+    history.pushState(
+      { path: e.target.dataset.menu },
+      null,
+      e.target.dataset.menu
+    );
+
+    this.convertTemplate(history.state.path);
+  };
+}
+
+export default MenuTab;
