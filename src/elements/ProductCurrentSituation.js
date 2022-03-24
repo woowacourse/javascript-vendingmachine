cc38a15a3a74d78ce39051588a0526a083cb2c65
KIM YEJI <rladpwl0512@gmail.com>
2022-03-24 22:43:34 +0900

    :sparkles: feat(ui): 상품 추가 구현
    Co-authored-by: Daye Lee <508yeah@gmail.com>

--- src/elements/ProductCurrentSituation.js
+++ src/elements/ProductCurrentSituation.js
@@ -1,7 +1,13 @@
+import ProductStore from '../domains/stores/ProductStore';
 import CustomElement from '../abstracts/CustomElement';
+import { $ } from '../utils/dom';
 
 class ProductCurrentSituation extends CustomElement {
-  // eslint-disable-next-line max-lines-per-function
+  connectedCallback() {
+    super.connectedCallback();
+    ProductStore.instance.subscribe(this);
+  }
+
   template() {
     return `
       <h2>상품 현황</h2>
@@ -13,17 +19,38 @@ class ProductCurrentSituation extends CustomElement {
             <th>수량</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
+        <tbody></tbody>
       </table>
     `;
   }
+
+  rerender(newProduct) {
+    $('tbody', $('.product-current-situation')).insertAdjacentHTML('beforeend', this.tableBodyRowTemplate(newProduct));
+    this.setEventAfterRerender();
+  }
+
+  tableBodyRowTemplate({ name, price, quantity }) {
+    return `
+      <tr>
+        <td>${name}</td>
+        <td>${price}</td>
+        <td>${quantity}</td>
+        <td>
+          <button class="table__product-modify-button">수정</button>
+          <button class="table__product-delete-button">삭제</button>
+        </td>
+      </tr>
+    `;
+  }
+
+  setEventAfterRerender() {
+    $('.table__product-modify-button').addEventListener('click', this.handleProductModifyButtonClick);
+    $('.table__product-delete-button').addEventListener('click', this.handleProductDeleteButtonClick);
+  }
+
+  handleProductModifyButtonClick = () => {};
+
+  handleProductDeleteButtonClick = () => {};
 }
 
 customElements.define('product-current-situation', ProductCurrentSituation);
