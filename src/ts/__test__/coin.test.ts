f751a7b04bb32d60162115a30621e4773aefad8c
이아현 <lah1203@naver.com>
2022-03-24 16:48:23 +0900

    test: 잔돈 충전 도메인 초기화
    Co-authored-by: 김의진 <gytks4@naver.com>

--- src/ts/__test__/coin.test.ts
+++ src/ts/__test__/coin.test.ts
@@ -1,25 +1,21 @@
 import CoinManagementDomain from '../domain/CoinManagement';
 
 describe('잔돈 도메인 테스트', () => {
-  const coinDomain = new CoinManagementDomain();
-  const clearDomain = () => {
-    coinDomain.addCash(-coinDomain.totalCash);
-  };
   const cash = 1000;
 
-  beforeEach(() => {
-    clearDomain();
-  });
-
   it('금액을 입력 받으면, 총액에 더해진다.', () => {
+    const coinDomain = new CoinManagementDomain();
+
     expect(coinDomain.totalCash).toBe(0);
     coinDomain.addCash(cash);
     expect(coinDomain.totalCash).toBe(cash);
   });
 
   it('랜덤으로 생성한 동전의 총액이 투입 금액과 일치한다.', () => {
-    coinDomain.addCoins(cash);
+    const coinDomain = new CoinManagementDomain();
     const coins = coinDomain.coins;
+    coinDomain.addCoins(cash);
+
     const cashFromCoins = Object.keys(coins).reduce(
       (prev, current) => prev + Number(current) * coins[current],
       0,
