b3ad208f56d8bd2cad1bff888ddef0543329e33f
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-03-24 16:32:31 +0900

    :sparkles: feat: add storage for vending machine
    Co-authored-by: 홍영민 <typingjs@gmail.com>

---
+++ src/storage.ts
@@ -0,0 +1,11 @@
+const storage = {
+  setLocalStorage(key: string, value: any) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+
+  getLocalStorage(key: string) {
+    return JSON.parse(localStorage.getItem(key)) ?? [];
+  },
+};
+
+export default storage;
