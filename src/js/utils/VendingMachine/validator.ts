b272b305f3ad7f5989d2eb971ed8457bccb1a3b0
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-24 19:03:20 +0900

    feat: 상품 정보 유효성 검사 기능 추가

---
+++ src/js/utils/VendingMachine/validator.ts
@@ -0,0 +1,20 @@
+import { IProduct } from '@Domain/Store/Interface';
+import { isStringLengthInRange, isNumberInRange, isCorrectNumberUnit } from '@Utils/index';
+
+export const validateProduct = (product: IProduct) => {
+  const { name, price, quantity } = product;
+
+  if (name === '') throw new Error('상품명을 입력해주세요.');
+  if (!isStringLengthInRange(name, 1, 10))
+    throw new Error('상품명은 1자에서 10자까지 입력할 수 있습니다.');
+
+  if (!Number.isInteger(price)) throw new Error('상품 가격은 숫자만 입력할 수 있습니다.');
+  if (!isNumberInRange(price, 100, 10000))
+    throw new Error('상품 가격은 100원에서 10000원까지 입력할 수 있습니다.');
+  if (!isCorrectNumberUnit(price, 10))
+    throw new Error('상품 가격은 10원 단위로 입력할 수 있습니다.');
+
+  if (!Number.isInteger(quantity)) throw new Error('상품 수량은 숫자만 입력할 수 있습니다.');
+  if (!isNumberInRange(quantity, 1, 20))
+    throw new Error('상품 수량은 1개에서 최대 20개까지만 입력할 수 있습니다.');
+};
--- src/js/utils/index.ts
+++ src/js/utils/index.ts
@@ -1,2 +1,13 @@
+export const $ = (selector, node = document) => node.querySelector(selector);
+export const $$ = (selector, node = document) => node.querySelectorAll(selector);
+
 export const getRandomNumber = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1) + min);
+
+export const isNumberInRange = (value: number, min: number, max: number) =>
+  value >= min && value <= max;
+
+export const isStringLengthInRange = (value: string, min: number, max: number) =>
+  value.length >= min && value.length <= max;
+
+export const isCorrectNumberUnit = (value: number, base: number) => value % base === 0;
