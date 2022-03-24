a4f8dadddbd494166e9dd1174b20f77c84fca198
JASUN LEE <liswktjs@gmail.com>
2022-03-24 17:09:50 +0900

    feat: 상품관리 기능들을 구현 Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,6 +4,8 @@ export const ERROR_MSG = {
   PRICE_OUT_OF_RANGE: '상품가격을 100이상 10000이하로 입력해주세요.',
   PRICE_NOT_DIVISIBLE_BY_10: '상품가격을 10으로 나누어 떨어지도록 입력해주세요.',
   QUANTITY_OUT_OF_RANGE: '상품 수량을 0이상 20이하의 수로 입력해주세요.',
+  PRODUCT_SOLD_OUT: '상품이 매진되었습니다.',
+  CAN_NOT_REFERENCE_STATE: '조회할 수 없는 상태값 입니다',
 };
 
 export const NAME_LENGHT_LIMIT = 10;
