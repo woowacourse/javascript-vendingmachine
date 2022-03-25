2763535f84beac655f90ef2aaf07f6f5951a6b72
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-25 20:52:26 +0900

    chore: Jest 절대 경로 설정
    Co-authored-by: Soyi Jeon <soyi.dev@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  moduleFileExtensions: ["js", "ts"],
+  moduleFileExtensions: ['js', 'ts'],
   transform: {
-    "^.+\\.js$": "babel-jest",
-    "^.+\\.ts$": "ts-jest",
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.ts$': 'ts-jest',
   },
-  testEnvironment: "node",
+  testEnvironment: 'node',
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
+    '^@/(.*)$': '<rootDir>/$1',
+    '^@Display/(.*)$': '<rootDir>/src/es/Display/$1',
+    '^@Store/(.*)$': '<rootDir>/src/es/Store/$1',
+    '^@Utils/(.*)$': '<rootDir>/src/es/utils/$1',
+    '^@Constants/(.*)$': '<rootDir>/src/es/constants/$1',
   },
-  testMatch: ["<rootDir>/**/*.test.(js|ts)"],
-  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  testMatch: ['<rootDir>/**/*.test.(js|ts)'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
