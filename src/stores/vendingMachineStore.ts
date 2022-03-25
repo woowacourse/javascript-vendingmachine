78daf300405ace159597492b1fba86cd45c5c49d
JASUN LEE <liswktjs@gmail.com>
2022-03-25 16:54:09 +0900

    refactor: mutateState 함수로 통합

--- src/stores/vendingMachineStore.ts
+++ src/stores/vendingMachineStore.ts
@@ -1,12 +1,13 @@
 import CoinWallet from '../domains/coinWallet';
 import Product from '../domains/product';
-import { VENDING_MACHINE_STATE_KEYS, ERROR_MSG } from '../utils/constants';
+import { ERROR_MSG, ACTION_TYPES } from '../utils/constants';
 import { IVendingMachineStore, TState, TSubsrcribedComponents } from './types';
 
 class VendingMachineStore implements IVendingMachineStore {
   subscribedComponents: TSubsrcribedComponents;
 
   state: TState;
+
   constructor() {
     this.subscribedComponents = {
       PRODUCT_LIST: [],
@@ -19,23 +20,45 @@ class VendingMachineStore implements IVendingMachineStore {
       INPUT_CHARGE: 0,
     };
   }
-  mutateProductList(actionType, payload) {
-    if (actionType === 'addProduct') {
+
+  mutateState({ actionType, payload, stateKey }) {
+    this.reducer[actionType](payload);
+    this.notifySubscribedView(stateKey);
+  }
+
+  subscribe(stateType, component) {
+    this.subscribedComponents[stateType].push(component);
+  }
+
+  getState(stateType, component) {
+    if (this.subscribedComponents[stateType].includes(component)) {
+      return this.state[stateType];
+    }
+    throw new Error(ERROR_MSG.CAN_NOT_REFERENCE_STATE);
+  }
+
+  notifySubscribedView(stateType) {
+    this.subscribedComponents[stateType].forEach(component => component.wakeUp());
+  }
+
+  private reducer = {
+    [ACTION_TYPES.ADD_PRODUCT]: payload => {
+      console.log(payload);
       const { name, price, quantity } = payload;
 
       const product = new Product(name, price, quantity);
 
       this.state.PRODUCT_LIST.push(product);
-    }
-    if (actionType === 'editProduct') {
+    },
+    [ACTION_TYPES.EDIT_PRODUCT]: payload => {
       const { id, name, price, quantity } = payload;
 
       const editProduct = this.state.PRODUCT_LIST.find(
         product => product.getProductInfo().id === id,
       );
       editProduct.editProductInfo({ name, price, quantity });
-    }
-    if (actionType === 'deleteProduct') {
+    },
+    [ACTION_TYPES.DELETE_PRODUCT]: payload => {
       const { id } = payload;
 
       const deletedProductList = this.state.PRODUCT_LIST.filter(product => {
@@ -43,30 +66,13 @@ class VendingMachineStore implements IVendingMachineStore {
         return productId !== id;
       });
       this.state.PRODUCT_LIST = deletedProductList;
-    }
-    this.notifySubscribedView(VENDING_MACHINE_STATE_KEYS.PRODUCT_LIST);
-  }
-  mutateCoinWallet(actionType, payload) {
-    if (actionType === 'rechargeChange') {
+    },
+    [ACTION_TYPES.RECHARGE_CHANGE]: payload => {
       const { changeInput } = payload;
 
       this.state.COIN_WALLET.rechargeCoinWallet(changeInput);
-    }
-    this.notifySubscribedView(VENDING_MACHINE_STATE_KEYS.COIN_WALLET);
-  }
-  mutateInputCharge() {}
-  subscribe(stateType, component) {
-    this.subscribedComponents[stateType].push(component);
-  }
-  getState(stateType, component) {
-    if (this.subscribedComponents[stateType].includes(component)) {
-      return this.state[stateType];
-    }
-    throw new Error(ERROR_MSG.CAN_NOT_REFERENCE_STATE);
-  }
-  notifySubscribedView(stateType) {
-    this.subscribedComponents[stateType].forEach(component => component.wakeUp());
-  }
+    },
+  };
 }
 
 export default new VendingMachineStore();
