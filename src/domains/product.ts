99de4494124eeb1809b6e10d82f155be2e9eb403
JASUN LEE <liswktjs@gmail.com>
2022-03-25 22:13:48 +0900

    feat: 상품 아이디가 중복되지 않게 생성되는 기능 구현

--- src/domains/product.ts
+++ src/domains/product.ts
@@ -1,36 +1,50 @@
 import { IProduct, TProduct } from './types';
 import { ERROR_MSG } from '../utils/constants';
-
+const { generateNonDuplicatedId } = (function () {
+  const memo = {};
+  function generateId(name: string) {
+    return `${name}${[...new Array(10)].map(() => Math.floor(Math.random() * 10)).join('')}`;
+  }
+  return {
+    generateNonDuplicatedId(name: string) {
+      let id = generateId(name);
+      while (memo[id]) {
+        id = generateId(name);
+      }
+      memo[id] = id;
+      return id;
+    },
+  };
+})();
 class Product implements IProduct {
   product: TProduct;
-  constructor(name, price, quantity) {
+  constructor(name: string, price: number, quantity: number) {
     this.product = {
-      id: this.generateProductId(name),
+      id: generateNonDuplicatedId(name),
       name,
       price,
       quantity,
     };
   }
+
   purchaseProduct() {
     if (this.product.quantity === 0) {
       throw new Error(ERROR_MSG.PRODUCT_SOLD_OUT);
     }
     this.product.quantity--;
   }
+
   getProductInfo() {
     return this.product;
   }
-  editProductInfo({ name, price, quantity }) {
+
+  editProductInfo({ name, price, quantity }: { name: string; price: number; quantity: number }) {
     this.product = {
       ...this.product,
       name: name ?? this.product.name,
       price: price ?? this.product.price,
       quantity: quantity ?? this.product.quantity,
     };
-  }
-  generateProductId(name) {
-    /** 나중에 중복 점검 필요 */
-    return `${name}${[...new Array(10)].map(() => Math.floor(Math.random() * 10)).join('')}`;
   }
 }
 
