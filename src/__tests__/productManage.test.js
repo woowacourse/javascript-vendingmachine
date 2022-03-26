1b74f9758b898089ed439f329861b42fdbea4bce
Ahn <jin7969@gmail.com>
2022-03-26 23:49:08 +0900

    test: 상품관리 테스트 작성
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- src/__tests__/productManage.test.js
+++ src/__tests__/productManage.test.js
@@ -1,22 +1,124 @@
-import { validProductPrice } from '../js/utils/validation.js';
-// - [ ] 상품 관리탭은 자판기가 보유하고 있는 상품을 추가하는 기능을 수행한다.
-// - [ ] 최초 상품 목록은 비워진 상태이다.
-// - [ ] 상품명, 가격, 수량을 입력해 상품을 추가할 수 있다.
-//   - [x] 상품명
-//     - [x] 1글자 이상으로, 최대 10글자까지 가능하다.
-//   - [x] 상품 가격
-//     - [x] 100원부터 시작하며, 최대 10,000원까지 가능하다.
-//     - [x] 10원으로 나누어 떨어져야 한다.
-//   - [ ] 수량
-//     - [ ] 한 제품당 수량은 1개 이상, 최대 20개까지 넣을 수있다.
-// - [ ] 관리자는 추가한 상품을 확인할 수 있다.
-// - [ ] 관리자는 추가한 상품을 수정, 삭제할 수 있다.
-// - [ ] 수정 시 상품명, 가격, 수량 정보 영역 자체가 인풋 영역으로 변경된다.
-// - [ ] 삭제 시 confirm을 활용하여 사용자에게 다시 한번 확인한다.
+import ProductManager from '../js/models/ProductManger.ts';
+import { ERROR_MESSAGE } from '../js/constants/constants.js';
 
 describe('상품 관리 탭 테스트', () => {
-  it('상품 가격은 10원 단위이고 100원 이상 10000원 이하로 입력할 수 있다.', () => {
-    const price = 1000;
-    expect(validProductPrice(price)).toBeTruthy();
+  it('조건에 맞는 상품명, 가격, 상품 수량을 입력할 수 있다.', () => {
+    const productManager = new ProductManager();
+    const product = {
+      name: '콜라',
+      price: 1000,
+      quantity: 10,
+    };
+    productManager.addProduct(product);
+    expect(productManager.getProducts()).toContain(product);
+  });
+
+  it('상품명은 빈값을 입력할 수 없다.', () => {
+    const productManager = new ProductManager();
+    const product = {
+      name: '',
+      price: 1000,
+      quantity: 10,
+    };
+    expect(() => productManager.addProduct(product)).toThrowError(ERROR_MESSAGE.EMPTY_NAME);
+  });
+
+  it('상품명은 10글자 초과로 입력할 수 없다.', () => {
+    const productManager = new ProductManager();
+    const product = {
+      name: '가나다라마바사아자차카',
+      price: 1000,
+      quantity: 10,
+    };
+    expect(() => productManager.addProduct(product)).toThrowError(ERROR_MESSAGE.OVER_MAX_LENGTH);
+  });
+
+  it('상품 가격은 10원 단위가 아니면 입력할 수 없다.', () => {
+    const productManager = new ProductManager();
+    const product = {
+      name: '콜라',
+      price: 1001,
+      quantity: 10,
+    };
+    expect(() => productManager.addProduct(product)).toThrowError(ERROR_MESSAGE.NOT_DIVIDE_NUMBER);
+  });
+
+  it('상품 가격은 100원 미만으로 입력할 수 없다.', () => {
+    const productManager = new ProductManager();
+    const product = {
+      name: '콜라',
+      price: 90,
+      quantity: 10,
+    };
+    expect(() => productManager.addProduct(product)).toThrowError(ERROR_MESSAGE.OUT_OF_PRICE_RANGE);
+  });
+
+  it('상품 가격은 10000원을 초과할 수 없다.', () => {
+    const productManager = new ProductManager();
+    const product = {
+      name: '콜라',
+      price: 10010,
+      quantity: 10,
+    };
+    expect(() => productManager.addProduct(product)).toThrowError(ERROR_MESSAGE.OUT_OF_PRICE_RANGE);
+  });
+
+  it('상품 수량은 1개 미만으로 입력할 수 없다.', () => {
+    const productManager = new ProductManager();
+    const product = {
+      name: '콜라',
+      price: 1000,
+      quantity: 0,
+    };
+    expect(() => productManager.addProduct(product)).toThrowError(
+      ERROR_MESSAGE.OUT_OF_QUANTITY_RANGE
+    );
+  });
+
+  it('상품 수량은 20개 초과로 입력할 수 없다.', () => {
+    const productManager = new ProductManager();
+    const product = {
+      name: '콜라',
+      price: 1000,
+      quantity: 21,
+    };
+    expect(() => productManager.addProduct(product)).toThrowError(
+      ERROR_MESSAGE.OUT_OF_QUANTITY_RANGE
+    );
+  });
+});
+
+describe('상품 정보 수정 테스트', () => {
+  it('조건에 맞는 상품명, 가격, 상품 수량으로 정보를 수정할 수 있다.', () => {
+    const productManager = new ProductManager();
+    const oldProductIndex = 0;
+    const oldProduct = {
+      name: '콜라',
+      price: 1000,
+      quantity: 10,
+    };
+    const newProduct = {
+      name: '사이다',
+      price: 2000,
+      quantity: 15,
+    };
+    productManager.addProduct(oldProduct);
+    productManager.modifyProduct(oldProductIndex, newProduct);
+    const productList = productManager.getProducts();
+    expect(productList).not.toContain(oldProduct);
+    expect(productList).toContain(newProduct);
+  });
+
+  it('상품 정보를 삭제할 수 있다.', () => {
+    const productManager = new ProductManager();
+    const oldProductIndex = 0;
+    const oldProduct = {
+      name: '콜라',
+      price: 1000,
+      quantity: 10,
+    };
+    productManager.addProduct(oldProduct);
+    productManager.deleteProduct(oldProductIndex);
+    expect(productManager.getProducts()).not.toContain(oldProduct);
   });
 });
