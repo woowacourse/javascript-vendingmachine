94141577cc21c883e11a460a37ed2cfc7e9145d9
uk960214 <uk960214@gmail.com>
2022-03-26 17:11:30 +0900

    refactor: manage tab 매서드 리팩토링
    Co-authored-by: lokba <lokba@users.noreply.github.com>
    Co-authored-by: JUDONGHYEOK <judonghyeok@gmail.com>

--- src/js/view/ManageProductTab.js
+++ src/js/view/ManageProductTab.js
@@ -24,6 +24,10 @@ export default class ManageProductTab {
     this.#productStatusTable.addEventListener('click', this.#handleProductStatus);
   }
 
+  get tabElements() {
+    return this.#manageContainer;
+  }
+
   #handleAddProductForm = (e) => {
     e.preventDefault();
     const name = this.#addProductNameInput.value;
@@ -51,18 +55,20 @@ export default class ManageProductTab {
   }
 
   #handleProductStatus = ({ target }) => {
-    if (target.classList.contains('update-product-button')) {
+    const { classList } = target;
+
+    if (classList.contains('update-product-button')) {
       this.#handleProductUpdate(target);
     }
 
     if (
-      target.classList.contains('remove-product-button') &&
+      classList.contains('remove-product-button') &&
       window.confirm('정말 삭제하겠습니까?')
     ) {
       this.#handleProductRemove(target);
     }
 
-    if (target.classList.contains('confirm-update-button')) {
+    if (classList.contains('confirm-update-button')) {
       this.#handleProductUpdateConfirm(target);
     }
   };
@@ -72,31 +78,27 @@ export default class ManageProductTab {
     const name = selectDom('.product-name', targetTableRow).textContent;
     const price = selectDom('.product-price', targetTableRow).textContent;
     const stock = selectDom('.product-stock', targetTableRow).textContent;
+    const { productId: id } = target.dataset;
+
     targetTableRow.insertAdjacentHTML(
       'afterend',
-      updateProductTableRow({
-        name,
-        price,
-        stock,
-        id: target.dataset.productId,
-      })
+      updateProductTableRow({ name, price, stock, id })
     );
     targetTableRow.remove();
   };
 
   #handleProductUpdateConfirm = (target) => {
     const targetTableRow = target.closest('tr');
-
     const name = selectDom('.update-product-name-input', targetTableRow).value;
     const price = selectDom('.update-product-price-input', targetTableRow).valueAsNumber;
     const stock = selectDom('.update-product-stock-input', targetTableRow).valueAsNumber;
+    const { productId: id } = target.dataset;
 
-    const { productId } = target.dataset;
     try {
-      this.#vendingMachine.updateProduct(productId, { name, price, stock });
+      this.#vendingMachine.updateProduct(id, { name, price, stock });
       targetTableRow.insertAdjacentHTML(
         'afterend',
-        productTableRow({ name, price, stock, id: productId })
+        productTableRow({ name, price, stock, id })
       );
       targetTableRow.remove();
     } catch ({ message }) {
@@ -105,16 +107,12 @@ export default class ManageProductTab {
   };
 
   #handleProductRemove = (target) => {
-    const { productId } = target.dataset;
+    const { productId: id } = target.dataset;
     try {
-      this.#vendingMachine.removeProduct(productId);
+      this.#vendingMachine.removeProduct(id);
       target.closest('tr').remove();
     } catch ({ message }) {
       alert(message);
     }
   };
-
-  get tabElements() {
-    return this.#manageContainer;
-  }
 }
