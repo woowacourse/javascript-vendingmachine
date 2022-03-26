ae7e285ddd8c82ca0634ec1f0c9bcc1661c5db55
gytks4 <gytks4@naver.com>
2022-03-26 13:08:21 +0900

    chore: production, development mode 분리

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.ts',
   resolve: {
     extensions: ['.js', '.css', '.ts'],
