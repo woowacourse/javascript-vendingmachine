f59a818e135b9823d7c5983ef869755a860f409b
intae92 <kangit2013@gmail.com>
2022-03-27 17:22:32 +0900

    refactor: constants 매직넘버 제거

--- src/js/constants.ts
+++ src/js/constants.ts
@@ -3,21 +3,24 @@ const RULES = {
   MIN_PRODUCT_PRICE: 100,
   MAX_PRODUCT_AMOUNT: 20,
   MIN_PRODUCT_AMOUNT: 0,
-  MINIMUM_CHANGE: 10,
   MAX_LENGTH_PRODUCT_NAME: 10,
+  MINIMUM_CHANGE: 10,
+  CHANGE_UNITS: [500, 100, 50, 10],
+  MIN_INPUT_MONEY: 0,
   MAX_VENDING_MACHINE_CHANGE: 100000,
   NOT_EXIST_INDEX: -1,
-  CHANGE_UNITS: [500, 100, 50, 10],
 };
 
 const ERROR_MESSAGE = {
   PRODUCT_NAME_IS_DUPLICATED: '이미 존재하는 이름의 상품입니다.',
-  PRODUCT_NAME_LENGTH: '상품명은 최대 10글자까지 입력해주세요.',
-  PRODUCT_PRICE: '상품가격은 100원~10,000원 사이여야 하며 10원으로 나누어 떨어져야 합니다.',
-  PRODUCT_AMOUNT: '한 제품당 수량은 최대 20개 입니다.',
-  TOO_MUCH_VENDING_MACHINE_CHANGE: '자판기가 보유할 수 있는 최대 금액은 100,000원 입니다.',
-  IS_NOT_UNIT_OF_TEN: '투입할 금액의 단위는 10원입니다.',
-  IS_NOT_POSITIVE_INTEGER: '투입할 금액은 0보다 큰 금액이어야 합니다.',
+  PRODUCT_NAME_LENGTH: `상품명은 최대 ${RULES.MAX_LENGTH_PRODUCT_NAME}글자까지 입력해주세요.`,
+  PRODUCT_PRICE: `상품가격은 ${RULES.MIN_PRODUCT_PRICE}원~${RULES.MAX_PRODUCT_PRICE.toLocaleString()}원 사이여야 하며 ${
+    RULES.MINIMUM_CHANGE
+  }원으로 나누어 떨어져야 합니다.`,
+  PRODUCT_AMOUNT: `한 제품당 수량은 최대 ${RULES.MAX_PRODUCT_AMOUNT}개 입니다.`,
+  TOO_MUCH_VENDING_MACHINE_CHANGE: `자판기가 보유할 수 있는 최대 금액은 ${RULES.MAX_VENDING_MACHINE_CHANGE.toLocaleString()}원 입니다.`,
+  IS_NOT_UNIT_OF_TEN: `투입할 금액의 단위는 ${RULES.MINIMUM_CHANGE}원입니다.`,
+  IS_NOT_POSITIVE_INTEGER: `투입할 금액은 ${RULES.MIN_INPUT_MONEY}보다 큰 금액이어야 합니다.`,
 };
 
 const REMOVE_CONFIRM_MESSAGE = '정말로 삭제하시겠습니까?';
