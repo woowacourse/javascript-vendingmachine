3809aa0f4b8f9a2cbe5be4ab286931a16150f65e
woose28 <dldntmdsla@naver.com>
2022-03-27 15:49:54 +0900

    feat: favicon 설정

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./index.html",
+      favicon: './src/assets/images/favicon.png',
     }),
   ],
 };
