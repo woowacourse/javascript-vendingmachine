5e076ce6ccb824320525b9922fbcc77fc574f79c
sanaandmomo <qajjang46@naver.com>
2022-03-27 18:02:44 +0900

    refactor: 기존 파일 삭제

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var VendingMachine_1 = require("./ts/resource/VendingMachine");
-require("./css/index.css");
-new VendingMachine_1["default"]().handlePopstate();
