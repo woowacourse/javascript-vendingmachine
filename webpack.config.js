5f555e1c8e248e7b9edd5cfc556d8ab433fb5a8f
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-03-27 14:17:27 +0900

    chore: gh-page 설정

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   devtool: "source-map",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
   },
   module: {
     rules: [
