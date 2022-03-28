6f59ef2607e205c5b87a8d0d43aabe19c2596a51
gytks4 <gytks4@naver.com>
2022-03-29 00:46:26 +0900

    refactor: 모든 금액에 toLocaleString 적용

--- src/ts/constants/index.ts
+++ src/ts/constants/index.ts
@@ -17,16 +17,12 @@ const CASH_RULE = {
 const MESSAGE = {
   ERROR_SAME_PRODUCT: '이미 동일한 이름의 상품이 존재합니다.',
   ERROR_OVER_MAX_LENGTH: `상품명을 ${MAX_NAME_LENGTH}글자 이하로 입력해주세요.`,
-  ERROR_INVALID_PRICE: `가격은 ${
-    PRICE_RULE.MIN
-  }원 이상 ${PRICE_RULE.MAX.toLocaleString()}원 이하여야 하며, ${
+  ERROR_INVALID_PRICE: `가격은 ${PRICE_RULE.MIN.toLocaleString()}원 이상 ${PRICE_RULE.MAX.toLocaleString()}원 이하여야 하며, ${
     PRICE_RULE.UNIT
   }으로 나누어 떨어져야 합니다.`,
   ERROR_OVER_MAX_QUANTITY: `상품 수량은 1개 이상 ${MAX_QUANTITY}개 이하로 입력해주세요.`,
   ERROR_EMPTY_VALUE: '상품 정보를 공백 없이 입력해주세요.',
-  ERROR_INVALID_CASH: `금액은 ${CASH_RULE.MAX.toLocaleString()}원 이하여야 하며, ${
-    CASH_RULE.UNIT
-  }으로 나누어 떨어져야 합니다.`,
+  ERROR_INVALID_CASH: `금액은 ${CASH_RULE.MAX.toLocaleString()}원 이하여야 하며, ${CASH_RULE.UNIT.toLocaleString()}으로 나누어 떨어져야 합니다.`,
   CONFIRM_DELETE_PRODUCT: '을(를) 삭제하시겠습니까?',
 };
 
