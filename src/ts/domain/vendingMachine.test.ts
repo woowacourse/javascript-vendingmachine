90ec0814f22a853998dfb960ea53c0f1a0d413a0
wonsss <bigdatajang@gmail.com>
2022-03-29 21:04:45 +0900

    :recycle: 테스트값 공유하지 않도록 테스트코드 수정

--- src/ts/domain/vendingMachine.test.ts
+++ src/ts/domain/vendingMachine.test.ts
@@ -7,22 +7,24 @@ global.localStorage = new LocalStorageMock();
 
 describe('상품 관리 테스트', () => {
   const vendingMachine = new VendingMachine();
-  const validProduct = { name: '코카콜라', price: 1000, quantity: 10 };
-  const privateValidProduct = { _name: '코카콜라', _price: 1000, _quantity: 10 };
-
-  beforeEach(() => {
-    vendingMachine.addProduct(validProduct);
-  });
 
   afterEach(() => {
     vendingMachine.products = [];
   });
 
   it('상품을 자판기에 추가할 수 있다.', () => {
+    const validProduct = { name: '코카콜라', price: 1000, quantity: 10 };
+    const privateValidProduct = { _name: '코카콜라', _price: 1000, _quantity: 10 };
+    vendingMachine.addProduct(validProduct);
+
     expect(vendingMachine.products).toContainEqual(privateValidProduct);
   });
 
   it('상품을 자판기에 삭제할 수 있다.', () => {
+    const validProduct = { name: '코카콜라', price: 1000, quantity: 10 };
+    const privateValidProduct = { _name: '코카콜라', _price: 1000, _quantity: 10 };
+    vendingMachine.addProduct(validProduct);
+
     const productNameToDelete = '코카콜라';
     vendingMachine.deleteProduct(productNameToDelete);
 
@@ -30,6 +32,9 @@ describe('상품 관리 테스트', () => {
   });
 
   it('중복된 상품명으로 상품을 추가하면 에러를 발생시킨다.', () => {
+    const validProduct = { name: '코카콜라', price: 1000, quantity: 10 };
+    vendingMachine.addProduct(validProduct);
+
     const duplicatedProduct = { name: '코카콜라', price: 900, quantity: 5 };
 
     expect(() => {
@@ -38,6 +43,10 @@ describe('상품 관리 테스트', () => {
   });
 
   it('상품 정보를 수정하면 수정된 상품 정보가 저장돼야 한다.', () => {
+    const validProduct = { name: '코카콜라', price: 1000, quantity: 10 };
+    const privateValidProduct = { _name: '코카콜라', _price: 1000, _quantity: 10 };
+    vendingMachine.addProduct(validProduct);
+
     const targetName = '코카콜라';
     const editedProduct = { name: '사이다', price: 1500, quantity: 15 };
     const privateEditedProduct = { _name: '사이다', _price: 1500, _quantity: 15 };
@@ -49,6 +58,9 @@ describe('상품 관리 테스트', () => {
   });
 
   it('상품 정보를 수정할 시, 수정한 상품명이 기존 상품명과 중복되면 에러를 발생시킨다.', () => {
+    const validProduct = { name: '코카콜라', price: 1000, quantity: 10 };
+    vendingMachine.addProduct(validProduct);
+
     const productToEdit = { name: '사이다', price: 1500, quantity: 15 };
     vendingMachine.addProduct(productToEdit);
 
@@ -59,6 +71,9 @@ describe('상품 관리 테스트', () => {
   });
 
   it('상품 정보를 수정할 시, 상품명이 공백으로만 이루어져있거나 빈 값이면 에러를 발생시킨다.', () => {
+    const validProduct = { name: '코카콜라', price: 1000, quantity: 10 };
+    vendingMachine.addProduct(validProduct);
+
     const productToEditEmpty = { name: '', price: 1500, quantity: 15 };
     const productToEditBlank = { name: '  ', price: 1500, quantity: 15 };
 
@@ -72,6 +87,9 @@ describe('상품 관리 테스트', () => {
   });
 
   it('상품 정보를 수정할 시, 상품명이 10글자 초과하면 에러를 발생시킨다.', () => {
+    const validProduct = { name: '코카콜라', price: 1000, quantity: 10 };
+    vendingMachine.addProduct(validProduct);
+
     const overMaxNameLength = { name: '코카콜라열글자넘는이름', price: 1000, quantity: 10 };
 
     expect(() => {
@@ -80,6 +98,9 @@ describe('상품 관리 테스트', () => {
   });
 
   it('상품 정보를 수정할 시, 상품 가격이 100원 미만이면 에러를 발생시킨다.', () => {
+    const validProduct = { name: '코카콜라', price: 1000, quantity: 10 };
+    vendingMachine.addProduct(validProduct);
+
     const underMinPrice = { name: '코카콜라', price: 99, quantity: 10 };
 
     expect(() => {
@@ -88,6 +109,9 @@ describe('상품 관리 테스트', () => {
   });
 
   it('상품 정보를 수정할 시, 상품 가격이 10,000원을 초과하면 에러를 발생시킨다.', () => {
+    const validProduct = { name: '코카콜라', price: 1000, quantity: 10 };
+    vendingMachine.addProduct(validProduct);
+
     const overMaxPrice = { name: '코카콜라', price: 10001, quantity: 10 };
 
     expect(() => {
@@ -96,6 +120,9 @@ describe('상품 관리 테스트', () => {
   });
 
   it('상품 정보를 수정할 시, 상품 가격이 10의 배수가 아니면 에러를 발생시킨다.', () => {
+    const validProduct = { name: '코카콜라', price: 1000, quantity: 10 };
+    vendingMachine.addProduct(validProduct);
+
     const invalidUnitPrice = { name: '코카콜라', price: 1513, quantity: 10 };
 
     expect(() => {
@@ -104,6 +131,9 @@ describe('상품 관리 테스트', () => {
   });
 
   it('상품 정보를 수정할 시, 상품 수량이 20개를 초과하면 에러를 발생시킨다.', () => {
+    const validProduct = { name: '코카콜라', price: 1000, quantity: 10 };
+    vendingMachine.addProduct(validProduct);
+
     const overMaxQuantity = { name: '코카콜라', price: 1000, quantity: 21 };
 
     expect(() => {
