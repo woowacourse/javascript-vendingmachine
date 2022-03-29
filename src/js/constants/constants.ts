eaf43407877e0d0506dec88d1de005ebc3fef437
cks3066 <cks3066@naver.com>
2022-03-30 01:29:40 +0900

    refactor: 1차 피드백 반영
    - enum 적용

--- src/js/constants/constants.ts
+++ src/js/constants/constants.ts
@@ -48,30 +48,25 @@ export const COINS: CoinsConstantType = {
   ten: 10,
 };
 
-export const ERROR_MESSAGE = {
-  ITEM_NAME: {
-    DUPLICATE_ITEM: '이미 등록된 상품명입니다.',
-    EMPTY_NAME: '공백으로된 상품명을 입력할 수 없습니다.',
-    OVER_MAX_LENGTH: '10자 이상의 상품명을 입력할 수 없습니다.',
-  },
-  ITEM_PRICE: {
-    NOT_INTEGER: '정수가 아닌 숫자는 가격으로 입력할 수 없습니다.',
-    UNDER_MIN: '100원보다 낮은 가격은 입력할 수 없습니다.',
-    OVER_MAX: '10,000원 보다 큰 가격을 입력할 수 없습니다.',
-    INVALID_UNIT: '10원 단위의 가격만 입력할 수 있습니다.',
-  },
-  ITEM_QUANTITY: {
-    NOT_INTEGER: '정수가 아닌 숫자는 수량으로 입력할 수 없습니다.',
-    UNDER_MIN: '0이하의 수는 수량으로 입력할 수 없습니다.',
-    OVER_MAX: '20개 보다 많은 수량을 입력할 수 없습니다.',
-  },
-  INPUT_MONEY: {
-    NOT_INTEGER: '정수가 아닌 숫자는 금액으로 입력할 수 없습니다.',
-    UNDER_MIN: '0원이하의 금액은 투입할 수 없습니다.',
-    OVER_MAX: '100,000원 보다 많은 금액을 투입할 수 없습니다.',
-    INVALID_UNIT: '10원 단위의 금액만 투입할 수 있습니다.',
-  },
-};
+export enum ERROR_MESSAGE {
+  ITEM_NAME_DUPLICATED = '이미 등록된 상품명입니다.',
+  ITEM_NAME_EMPTY_NAME = '공백으로된 상품명을 입력할 수 없습니다.',
+  ITEM_NAME_OVER_MAX_LENGTH = '10자 이상의 상품명을 입력할 수 없습니다.',
+
+  ITEM_PRICE_NOT_INTEGER = '정수가 아닌 숫자는 가격으로 입력할 수 없습니다.',
+  ITEM_PRICE_UNDER_MIN = '100원보다 낮은 가격은 입력할 수 없습니다.',
+  ITEM_PRICE_OVER_MAX = '10,000원 보다 큰 가격을 입력할 수 없습니다.',
+  ITEM_PRICE_INVALID_UNIT = '10원 단위의 가격만 입력할 수 있습니다.',
+
+  ITEM_QUANTITY_NOT_INTEGER = '정수가 아닌 숫자는 수량으로 입력할 수 없습니다.',
+  ITEM_QUANTITY_UNDER_MIN = '0이하의 수는 수량으로 입력할 수 없습니다.',
+  ITEM_QUANTITY_OVER_MAX = '20개 보다 많은 수량을 입력할 수 없습니다.',
+
+  INPUT_MONEY_NOT_INTEGER = '정수가 아닌 숫자는 금액으로 입력할 수 없습니다.',
+  INPUT_MONEY_UNDER_MIN = '0원이하의 금액은 투입할 수 없습니다.',
+  INPUT_MONEY_OVER_MAX = '100,000원 보다 많은 금액을 투입할 수 없습니다.',
+  INPUT_MONEY_INVALID_UNIT = '10원 단위의 금액만 투입할 수 있습니다.',
+}
 
 export const CONFIRM_MESSAGE = {
   DELETE: '정말로 삭제하시겠습니까?',
--- src/js/validates/validates.ts
+++ src/js/validates/validates.ts
@@ -3,53 +3,53 @@ import { ERROR_MESSAGE, ITEM, MONEY } from '../constants/constants';
 
 export const validateAddItemInput = ({ name, price, quantity }: ItemType) => {
   if (name.length === 0) {
-    throw new Error(ERROR_MESSAGE.ITEM_NAME.EMPTY_NAME);
+    throw new Error(ERROR_MESSAGE.ITEM_NAME_EMPTY_NAME);
   }
   if (name.length > ITEM.NAME.MAX_LENGTH) {
-    throw new Error(ERROR_MESSAGE.ITEM_NAME.OVER_MAX_LENGTH);
+    throw new Error(ERROR_MESSAGE.ITEM_NAME_OVER_MAX_LENGTH);
   }
   if (!Number.isInteger(price)) {
-    throw new Error(ERROR_MESSAGE.ITEM_PRICE.NOT_INTEGER);
+    throw new Error(ERROR_MESSAGE.ITEM_PRICE_NOT_INTEGER);
   }
 
   if (price < ITEM.PRICE.MIN) {
-    throw new Error(ERROR_MESSAGE.ITEM_PRICE.UNDER_MIN);
+    throw new Error(ERROR_MESSAGE.ITEM_PRICE_UNDER_MIN);
   }
   if (price > ITEM.PRICE.MAX) {
-    throw new Error(ERROR_MESSAGE.ITEM_PRICE.OVER_MAX);
+    throw new Error(ERROR_MESSAGE.ITEM_PRICE_OVER_MAX);
   }
   if (price % ITEM.PRICE.UNIT !== 0) {
-    throw new Error(ERROR_MESSAGE.ITEM_PRICE.INVALID_UNIT);
+    throw new Error(ERROR_MESSAGE.ITEM_PRICE_INVALID_UNIT);
   }
 
   if (!Number.isInteger(quantity)) {
-    throw new Error(ERROR_MESSAGE.ITEM_QUANTITY.NOT_INTEGER);
+    throw new Error(ERROR_MESSAGE.ITEM_QUANTITY_NOT_INTEGER);
   }
   if (quantity <= ITEM.QUANTITY.MIN) {
-    throw new Error(ERROR_MESSAGE.ITEM_QUANTITY.UNDER_MIN);
+    throw new Error(ERROR_MESSAGE.ITEM_QUANTITY_UNDER_MIN);
   }
   if (quantity > ITEM.QUANTITY.MAX) {
-    throw new Error(ERROR_MESSAGE.ITEM_QUANTITY.OVER_MAX);
+    throw new Error(ERROR_MESSAGE.ITEM_QUANTITY_OVER_MAX);
   }
 };
 
 export const validateInputMoney = (inputMoney: number) => {
   if (!Number.isInteger(inputMoney)) {
-    throw new Error(ERROR_MESSAGE.INPUT_MONEY.NOT_INTEGER);
+    throw new Error(ERROR_MESSAGE.INPUT_MONEY_NOT_INTEGER);
   }
   if (inputMoney <= MONEY.MIN) {
-    throw new Error(ERROR_MESSAGE.INPUT_MONEY.UNDER_MIN);
+    throw new Error(ERROR_MESSAGE.INPUT_MONEY_UNDER_MIN);
   }
   if (inputMoney > MONEY.MAX) {
-    throw new Error(ERROR_MESSAGE.INPUT_MONEY.OVER_MAX);
+    throw new Error(ERROR_MESSAGE.INPUT_MONEY_OVER_MAX);
   }
   if (inputMoney % MONEY.UNIT !== 0) {
-    throw new Error(ERROR_MESSAGE.INPUT_MONEY.INVALID_UNIT);
+    throw new Error(ERROR_MESSAGE.INPUT_MONEY_INVALID_UNIT);
   }
 };
 
 export const checkDuplicatedItem = (items: ItemType[], newItem: ItemType, targetIndex: number) => {
   if (items.find((item, index) => index !== targetIndex && item.name === newItem.name)) {
-    throw new Error(ERROR_MESSAGE.ITEM_NAME.DUPLICATE_ITEM);
+    throw new Error(ERROR_MESSAGE.ITEM_NAME_DUPLICATED);
   }
 };
