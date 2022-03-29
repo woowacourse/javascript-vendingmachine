745694dd765b6089f67adb3b06ab7e868ec9c212
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-03-29 22:31:14 +0900

    test: 상품 수정 후 취소 기능 테스트 작성

--- cypress/integration/itemManageTab.spec.js
+++ cypress/integration/itemManageTab.spec.js
@@ -30,6 +30,16 @@ describe('상품 관리 탭 테스트', () => {
     cy.get('tr').eq(1).should('have.attr', 'data-item-name', '사이다');
   });
 
+  it('추가한 상품 수정 후, 취소 버튼을 누르면 변경 사항을 되돌릴 수 있다.', () => {
+    cy.addItem(itemName, itemPrice, itemQuantity);
+
+    cy.get('.edit-item-button').click();
+    cy.get('.item-info-input-cell').eq(0).clear().type('사이다');
+    cy.get('.cancel-item-button').click();
+
+    cy.get('tr').eq(1).should('have.attr', 'data-item-name', itemName);
+  });
+
   it('추가한 상품에 삭제 버튼을 누르면 상품을 삭제할 수 있다.', () => {
     cy.addItem(itemName, itemPrice, itemQuantity);
 
