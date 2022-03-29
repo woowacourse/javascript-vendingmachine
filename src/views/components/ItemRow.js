27dc81240baea3ef58781275b1f3cb2716ab36d9
prefer2 <sunho6200@gmail.com>
2022-03-29 23:57:32 +0900

    refactor: 가독성을 위해 early return

--- src/views/components/ItemRow.js
+++ src/views/components/ItemRow.js
@@ -11,53 +11,49 @@ class ItemRow extends TableRow {
     const { name, price, quantity } = this.props;
     const { isEditing } = this.state;
 
+    if (isEditing) {
+      return `
+      <td class="item-name styled-td">
+        <input
+          value="${name}"
+          class="item-name-edit-input transparent-input"
+          type="text"
+          maxlength="${ITEM.NAME.MAX_LENGTH}"
+        >
+      </td>
+      <td class="item-price styled-td">
+        <input
+          value="${price}"
+          class="item-price-edit-input transparent-input"
+          type="number"
+          step="${MONEY_UNIT}"
+          min="${ITEM.PRICE.MIN}"
+          max="${ITEM.PRICE.MAX}"
+        >
+      </td>
+      <td class="item-quantity styled-td">
+        <input
+          value="${quantity}"
+          class="item-quantity-edit-input transparent-input"
+          type="number"
+          step="1"
+          min="${ITEM.QUANTITY.MIN}"
+          max="${ITEM.QUANTITY.MAX}"
+        >
+      </td>
+      <td class="item-button-container">
+        <button class="item-update-button styled-button">완료</button>
+      </td>`;
+    }
+
     return `
-       ${
-         isEditing
-           ? `
-            <td class="item-name styled-td">
-              <input
-                value="${name}"
-                class="item-name-edit-input transparent-input"
-                type="text"
-                maxlength="${ITEM.NAME.MAX_LENGTH}"
-              >
-            </td>
-            <td class="item-price styled-td">
-              <input
-                value="${price}"
-                class="item-price-edit-input transparent-input"
-                type="number"
-                step="${MONEY_UNIT}"
-                min="${ITEM.PRICE.MIN}"
-                max="${ITEM.PRICE.MAX}"
-              >
-            </td>
-            <td class="item-quantity styled-td">
-              <input
-                value="${quantity}"
-                class="item-quantity-edit-input transparent-input"
-                type="number"
-                step="1"
-                min="${ITEM.QUANTITY.MIN}"
-                max="${ITEM.QUANTITY.MAX}"
-              >
-            </td>
-            <td class="item-button-container">
-              <button class="item-update-button styled-button">완료</button>
-            </td>
-          `
-           : `
-            <td class="item-name styled-td">${name}</td>
-            <td class="item-price styled-td">${price}</td>
-            <td class="item-quantity styled-td">${quantity}</td>
-            <td class="item-button-container">
-              <button class="item-edit-button styled-button" type="button">수정</button>
-              <button class="item-remove-button styled-button" type="button">삭제</button>
-            </td>
-          `
-       }
-    `;
+      <td class="item-name styled-td">${name}</td>
+      <td class="item-price styled-td">${price}</td>
+      <td class="item-quantity styled-td">${quantity}</td>
+      <td class="item-button-container">
+        <button class="item-edit-button styled-button" type="button">수정</button>
+        <button class="item-remove-button styled-button" type="button">삭제</button>
+      </td>`;
   }
 
   setEvent() {
