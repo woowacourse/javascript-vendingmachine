02c7a88b834a97d099abcdb01f6f695053dd414f
intae92 <kangit2013@gmail.com>
2022-03-29 18:32:19 +0900

    test: 이상, 이하등의 범위에 대한 경계 테스트 추가

--- src/js/__test__/vendingMachine.test.js
+++ src/js/__test__/vendingMachine.test.js
@@ -16,13 +16,11 @@ describe('자판기 기본 기능 테스트', () => {
     });
 
     it('자판기에 같은 이름의 상품은 추가할 수 없어야 한다.', () => {
-      const product = {
-        name: "코카콜라",
-        price: 2000,
-        amount: 8,
-      }
+      const product1 = { name: "사이다", price: 1000, amount: 8 }
+      const product2 = { name: "사이다", price: 2000, amount: 8 }
 
-      expect(() => vendingMachine.addProduct(product)).toThrowError(ERROR_MESSAGE.PRODUCT_NAME_IS_DUPLICATED);
+      vendingMachine.addProduct(product1);
+      expect(() => vendingMachine.addProduct(product2)).toThrowError(ERROR_MESSAGE.PRODUCT_NAME_IS_DUPLICATED);
     });
 
     it('자판기에 추가 될 상품의 이름은 10글자를 초과할 수 없어야 한다.', () => {
@@ -35,7 +33,7 @@ describe('자판기 기본 기능 테스트', () => {
       expect(() => vendingMachine.addProduct(product)).toThrowError(ERROR_MESSAGE.PRODUCT_NAME_LENGTH);
     });
 
-    it('자판기에 추가 될 상품의 가격은 100원 이상이어야 한다.', () => {
+    it('자판기에 추가 될 상품의 가격이 100원 미만은 에러를 발생시킨다.', () => {
       const product = {
         name: "펩시",
         price: 99,
@@ -45,7 +43,17 @@ describe('자판기 기본 기능 테스트', () => {
       expect(() => vendingMachine.addProduct(product)).toThrowError(ERROR_MESSAGE.PRODUCT_PRICE);
     });
 
-    it('자판기에 추가 될 상품의 가격은 10,000원 이하여야 한다.', () => {
+    it('자판기에 추가 될 상품의 가격은 100원 이상이여야 한다.', () => {
+      const product = {
+        name: "펩시",
+        price: 100,
+        amount: 5,
+      }
+
+      expect(() => vendingMachine.addProduct(product)).not.toThrowError(ERROR_MESSAGE.PRODUCT_PRICE);
+    });
+
+    it('자판기에 추가 될 상품의 가격이 10,000원을 초과하면 에러를 발생시킨다.', () => {
       const product = {
         name: "제로펩시",
         price: 10001,
@@ -55,7 +63,17 @@ describe('자판기 기본 기능 테스트', () => {
       expect(() => vendingMachine.addProduct(product)).toThrowError(ERROR_MESSAGE.PRODUCT_PRICE);
     });
 
-    it('자판기에 추가 될 상품의 수량은 20개 이하여야 한다.', () => {
+    it('자판기에 추가 될 상품의 가격은 10,000원 이하여야 한다.', () => {
+      const product = {
+        name: "제로펩시",
+        price: 10000,
+        amount: 5,
+      }
+
+      expect(() => vendingMachine.addProduct(product)).not.toThrowError(ERROR_MESSAGE.PRODUCT_PRICE);
+    });
+
+    it('자판기에 추가 될 상품의 수량이 20개를 초과하면 에러를 발생시킨다.', () => {
       const product = {
         name: "제로코카콜라",
         price: 1000,
@@ -64,22 +82,49 @@ describe('자판기 기본 기능 테스트', () => {
 
       expect(() => vendingMachine.addProduct(product)).toThrowError(ERROR_MESSAGE.PRODUCT_AMOUNT);
     });
+
+    it('자판기에 추가 될 상품의 수량은 20개 이하여야 한다.', () => {
+      const product = {
+        name: "제로코카콜라",
+        price: 1000,
+        amount: 20,
+      }
+
+      expect(() => vendingMachine.addProduct(product)).not.toThrowError(ERROR_MESSAGE.PRODUCT_AMOUNT);
+    });
   })
   
   describe('잔돈 충전 기능 테스트', () => {
-    it('자판기가 가진 금액은 100,000원 이하여야 한다.', () => {
+    it('자판기가 가진 금액이 100,000원를 에러를 발생시킨다.', () => {
       const money = 100010;
+      
       expect(() => vendingMachine.addChange(money)).toThrowError(ERROR_MESSAGE.TOO_MUCH_VENDING_MACHINE_CHANGE);
     });
 
-    it('자판기에 충전할 금액은 양수이어야 한다.', () => {
+    it('자판기가 가진 금액은 100,000원 이하여야 한다.', () => {
+      const money = 100000;
+      
+      expect(() => vendingMachine.addChange(money)).not.toThrowError(ERROR_MESSAGE.TOO_MUCH_VENDING_MACHINE_CHANGE);
+    });
+
+    it('자판기에 충전할 금액이 양수가 아니면 에러를 발생시킨다.', () => {
       const money = -1000;
       expect(() => vendingMachine.addChange(money)).toThrowError(ERROR_MESSAGE.IS_NOT_POSITIVE_INTEGER);
     });
 
-    it('자판기에 충전할 수 있는 금액은 10원으로 나누어 떨어지는 금액이어야 한다.', () => {
+    it('자판기에 충전할 금액은 양수여야 한다.', () => {
+      const money = 1000;
+      expect(() => vendingMachine.addChange(money)).not.toThrowError(ERROR_MESSAGE.IS_NOT_POSITIVE_INTEGER);
+    });
+
+    it('자판기에 충전할 수 있는 금액이 10원으로 나누어 떨어지지 않으면 에럴르 발생시킨다.', () => {
       const money = 9;
       expect(() => vendingMachine.addChange(money)).toThrowError(ERROR_MESSAGE.IS_NOT_UNIT_OF_TEN);
+    });
+
+    it('자판기에 충전할 수 있는 금액은 10원으로 나누어 떨어지는 금액이어야 한다.', () => {
+      const money = 10;
+      expect(() => vendingMachine.addChange(money)).not.toThrowError(ERROR_MESSAGE.IS_NOT_UNIT_OF_TEN);
     });
   });
 })
