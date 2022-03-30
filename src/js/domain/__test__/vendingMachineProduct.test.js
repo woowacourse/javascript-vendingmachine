87b728a5ad878571c849fdc8de9fefd40e0affc7
JUDONGHYEOK <judonghyeok@gmail.com>
2022-03-30 15:28:33 +0900

    refactor: vendingMachineProductTest 널병합연산자 제거
    - 널 병합 연산자가 진정한 의미의 빈값을 수행하지 못하므로 제거하였다

--- src/js/domain/__test__/vendingMachineProduct.test.js
+++ src/js/domain/__test__/vendingMachineProduct.test.js
@@ -5,20 +5,19 @@ import VendingMachineProduct from '../VendingMachineProduct';
 
 function generateProduct(inputData = {}) {
   const data = {
-    name: inputData.name ?? '콜라',
-    price: inputData.price ?? 1500,
-    stock: inputData.stock ?? 20,
+    name: inputData.name,
+    price: inputData.price,
+    stock: inputData.stock,
   };
 
   return new VendingMachineProduct(data);
 }
-
 function modifyProduct(inputData = {}) {
-  const newProduct = generateProduct();
+  const newProduct = generateProduct(inputData);
   const data = {
-    name: inputData.name ?? '사이다',
-    price: inputData.price ?? 1200,
-    stock: inputData.stock ?? 5,
+    name: inputData.name,
+    price: inputData.price,
+    stock: inputData.stock,
   };
 
   newProduct.modify(data);
@@ -27,11 +26,10 @@ function modifyProduct(inputData = {}) {
 }
 
 describe('상품 클래스 테스트', () => {
+  const name = '콜라';
+  const price = 1500;
+  const stock = 10;
   test('상품명, 가격, 수량을 입력하면 입력 값이 정상적으로 저장된다.', () => {
-    const name = '콜라';
-    const price = 1500;
-    const stock = 10;
-
     const newProduct = generateProduct({ name, price, stock });
 
     const productData = {
@@ -44,12 +42,12 @@ describe('상품 클래스 테스트', () => {
   });
 
   test('상품의 데이터를 수정할 수 있다.', () => {
-    const newProduct = modifyProduct();
+    const newName = '사이다';
+    const newPrice = 1200;
+    const newStock = 5;
 
-    const name = '사이다';
-    const price = 1200;
-    const stock = 5;
-    const newData = { name, price, stock };
+    const newData = { name: newName, price: newPrice, stock: newStock };
+    const newProduct = modifyProduct(newData);
 
     const productData = {
       name: newProduct.name,
@@ -65,27 +63,24 @@ describe('상품 클래스 테스트', () => {
 });
 
 function validationTests(testType, testFunction) {
+  const name = '콜라';
+  const price = 1500;
+  const stock = 10;
   describe(`${testType} 유효성 검사`, () => {
     test(`${testType} 상품명이 입력되지 않으면 오류가 발생한다.`, () => {
-      const emptyName = '';
-
-      expect(() => testFunction({ name: emptyName })).toThrow(
+      expect(() => testFunction({ price, stock })).toThrow(
         ERROR_MESSAGE.CONTAIN_EMPTY_FIELD_IN_FORM
       );
     });
 
     test(`${testType} 가격이 입력되지 않으면 오류가 발생한다.`, () => {
-      const emptyPrice = '';
-
-      expect(() => testFunction({ price: emptyPrice })).toThrow(
+      expect(() => testFunction({ name, stock })).toThrow(
         ERROR_MESSAGE.CONTAIN_EMPTY_FIELD_IN_FORM
       );
     });
 
     test(`${testType} 수량이 입력되지 않으면 오류가 발생한다.`, () => {
-      const emptyStock = '';
-
-      expect(() => testFunction({ stock: emptyStock })).toThrow(
+      expect(() => testFunction({ name, price })).toThrow(
         ERROR_MESSAGE.CONTAIN_EMPTY_FIELD_IN_FORM
       );
     });
@@ -93,7 +88,7 @@ function validationTests(testType, testFunction) {
     test(`${testType} 상품명이 ${PRODUCT_RULES.MAX_NAME_LENGTH}글자를 초과하면 오류가 발생한다.`, () => {
       const invalidName = '콜라콜라맛있다맛있으면';
 
-      expect(() => testFunction({ name: invalidName })).toThrow(
+      expect(() => testFunction({ name: invalidName, price, stock })).toThrow(
         ERROR_MESSAGE.EXCEED_MAX_PRODUCT_NAME_LENGTH
       );
     });
@@ -101,7 +96,7 @@ function validationTests(testType, testFunction) {
     test(`${testType} 상품 가격이 ${PRODUCT_RULES.MIN_PRICE}원 미만인 경우 오류가 발생한다.`, () => {
       const lowPrice = 90;
 
-      expect(() => testFunction({ price: lowPrice })).toThrow(
+      expect(() => testFunction({ name, price: lowPrice, stock })).toThrow(
         ERROR_MESSAGE.OUT_OF_PRODUCT_PRICE_RANGE
       );
     });
@@ -109,7 +104,7 @@ function validationTests(testType, testFunction) {
     test(`${testType} 상품 가격이 ${PRODUCT_RULES.MAX_PRICE}원을 초과한 경우 오류가 발생한다.`, () => {
       const highPrice = 10010;
 
-      expect(() => testFunction({ price: highPrice })).toThrow(
+      expect(() => testFunction({ name, price: highPrice, stock })).toThrow(
         ERROR_MESSAGE.OUT_OF_PRODUCT_PRICE_RANGE
       );
     });
@@ -117,7 +112,7 @@ function validationTests(testType, testFunction) {
     test(`${testType} 상품 가격은 ${PRODUCT_RULES.PRICE_UNIT}원 단위가 아닐 경우 오류가 발생한다.`, () => {
       const invalidUnitPrice = 111;
 
-      expect(() => testFunction({ price: invalidUnitPrice })).toThrow(
+      expect(() => testFunction({ name, price: invalidUnitPrice, stock })).toThrow(
         ERROR_MESSAGE.INVALID_UNIT_PRODUCT_PRICE
       );
     });
@@ -125,7 +120,7 @@ function validationTests(testType, testFunction) {
     test(`${testType} 상품 수량은 ${PRODUCT_RULES.MAX_STOCK}개 초과할 경우 오류가 발생한다.`, () => {
       const invalidStock = 21;
 
-      expect(() => testFunction({ stock: invalidStock })).toThrow(
+      expect(() => testFunction({ name, price, stock: invalidStock })).toThrow(
         ERROR_MESSAGE.OUT_OF_PRODUCT_STOCK_RANGE
       );
     });
@@ -133,7 +128,7 @@ function validationTests(testType, testFunction) {
     test(`${testType} 상품 수량은 ${PRODUCT_RULES.MIN_STOCK}개 미만일 경우 오류가 발생한다.`, () => {
       const invalidStock = -1;
 
-      expect(() => testFunction({ stock: invalidStock })).toThrow(
+      expect(() => testFunction({ name, price, stock: invalidStock })).toThrow(
         ERROR_MESSAGE.OUT_OF_PRODUCT_STOCK_RANGE
       );
     });
@@ -141,7 +136,7 @@ function validationTests(testType, testFunction) {
     test(`${testType} 상품 수량이 자연수가 아닐 경우 오류가 발생한다.`, () => {
       const invalidStock = 1.1;
 
-      expect(() => testFunction({ stock: invalidStock })).toThrow(
+      expect(() => testFunction({ name, price, stock: invalidStock })).toThrow(
         ERROR_MESSAGE.INVALID_PRODUCT_STOCK
       );
     });
