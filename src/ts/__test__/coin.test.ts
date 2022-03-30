11ef5c0f23035b184158cd1e8cfb5b710dee1e49
이아현 <lah1203@naver.com>
2022-03-30 12:47:10 +0900

    test: 코인 도메인 테스트 병합

--- src/ts/__test__/coin.test.ts
+++ src/ts/__test__/coin.test.ts
@@ -1,21 +1,14 @@
 import CoinManagementDomain from '../domain/CoinManagement';
 
 describe('잔돈 도메인 테스트', () => {
-  const cash = 1000;
-
-  it('금액을 입력 받으면, 총액에 더해진다.', () => {
+  it('금액을 입력 받으면, 랜덤으로 동전이 생성되어 총액에 더해진다.', () => {
     const coinDomain = new CoinManagementDomain();
+    const cash = 1000;
 
-    expect(coinDomain.totalCash).toBe(0);
     coinDomain.addCash(cash);
     expect(coinDomain.totalCash).toBe(cash);
-  });
 
-  it('랜덤으로 생성한 동전의 총액이 투입 금액과 일치한다.', () => {
-    const coinDomain = new CoinManagementDomain();
-    const coins = coinDomain.coins;
-    coinDomain.addCoins(cash);
-
+    const { coins } = coinDomain;
     const cashFromCoins = Object.keys(coins).reduce(
       (prev, current) => prev + Number(current) * coins[current],
       0,
