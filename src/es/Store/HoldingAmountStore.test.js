a185a4a588845e5c92c80d07f70eca600855873f
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-30 22:38:02 +0900

    ✅ : 자판기 동전 충전 테스트 코드 추가

--- src/es/Store/HoldingAmountStore.test.js
+++ src/es/Store/HoldingAmountStore.test.js
@@ -7,8 +7,22 @@ describe('자판기 보유 동전 관리 로직 테스트', () => {
 
   test('1. 동전을 정상적으로 충전할 수 있어야 한다.', () => {
     const inputValue = 5000;
-    HoldingAmountStore.addAmount(inputValue);
+    const updateCoins = [0, 0, 0, 10];
 
-    expect(HoldingAmountStore.getTotalAmount()).toBe(inputValue);
+    const spy = jest
+      .spyOn(HoldingAmountStore, 'getRandomCoinsFromAmount')
+      .mockImplementation(() => updateCoins);
+
+    HoldingAmountStore.addAmount(inputValue);
+    expect(HoldingAmountStore.getState().coins).toStrictEqual(updateCoins);
+
+    spy.mockRestore();
+  });
+
+  test('2. 보유한 동전의 총액을 계산할 수 있어야 한다.', () => {
+    const inputValue = 5000;
+
+    HoldingAmountStore.addAmount(inputValue);
+    expect(HoldingAmountStore.getTotalAmount()).toStrictEqual(inputValue);
   });
 });
