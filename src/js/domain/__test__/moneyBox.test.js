f050b000e45bc6fe0d2ea5c770ba397fda115101
JUDONGHYEOK <judonghyeok@gmail.com>
2022-03-30 22:21:43 +0900

    refactor: moneyBox test 동전 갯수 확인 테스트 삭제
    - 동전 갯수를 그냥 대입하는 것에 지나지 않으므로 테스트의 의미가 없다고 생각하여 삭제

--- src/js/domain/__test__/moneyBox.test.js
+++ src/js/domain/__test__/moneyBox.test.js
@@ -3,36 +3,9 @@ import MoneyBox from '../MoneyBox';
 
 describe('돈통 클래스 테스트', () => {
   let moneyBox;
-  let mockCoinStatusList;
-
-  function setMockDistributeStrategy() {
-    mockCoinStatusList = [
-      { name: 'FIVE_HUNDRED_WON', value: 500, count: 5 },
-      { name: 'ONE_HUNDRED_WON', value: 100, count: 4 },
-      { name: 'FIFTY_WON', value: 50, count: 1 },
-      { name: 'TEN_WON', value: 10, count: 5 },
-    ];
-
-    const mockCoinDistributeStrategy = {
-      distribute() {
-        return mockCoinStatusList;
-      },
-    };
-
-    moneyBox.strategy = mockCoinDistributeStrategy;
-  }
 
   beforeEach(() => {
     moneyBox = new MoneyBox();
-  });
-
-  test('금액을 투입했을 때 금액에 맞는 동전을 충전할 수 있다.', () => {
-    const inputMoney = 3000;
-
-    setMockDistributeStrategy();
-    moneyBox.addChange(inputMoney);
-
-    expect(moneyBox.coinStatusList).toEqual(mockCoinStatusList);
   });
 
   test('금액을 투입하고 총 보유 금액을 확인할 수 있다.', () => {
