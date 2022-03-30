44c33b1229c5aedf5c2ec95543dc3e6c8cec90a5
airman5573 <rpf5573@kakao.com>
2022-03-31 01:06:25 +0900

    :recycle: refactor: 가독성 향상
    if안의 조건문에 이름을 붙이고 개행을 해서 가독성을 향상시킨다

--- src/validation/validators.ts
+++ src/validation/validators.ts
@@ -8,32 +8,46 @@ const isInteger = (str: string) => {
 };
 
 export const validateProductName = (name: string, productList: Array<ProductItem>) => {
-  if (!name) return new ValidationResult(true, ERROR_MESSAGE.EMPTY_PRODUCT_NAME);
-  if (name.length > PRODUCT.NAME.MAX_LENGTH)
+  const isEmptyName = !name;
+  if (isEmptyName) return new ValidationResult(true, ERROR_MESSAGE.EMPTY_PRODUCT_NAME);
+
+  const isOverMaxNameLength = name.length > PRODUCT.NAME.MAX_LENGTH;
+  if (isOverMaxNameLength)
     return new ValidationResult(true, ERROR_MESSAGE.OVER_MAX_LENGTH_PRODUCT_NAME);
-  if (productList.some((item) => name === item.name))
-    return new ValidationResult(true, ERROR_MESSAGE.DUPLICATE_PRDUCT_NAME);
+
+  const isDuplicateName = productList.some((item) => name === item.name);
+  if (isDuplicateName) return new ValidationResult(true, ERROR_MESSAGE.DUPLICATE_PRDUCT_NAME);
+
   return new ValidationResult(false);
 };
 
 export const validateProductPrice = (price: string) => {
-  if (!price) return new ValidationResult(true, ERROR_MESSAGE.EMPTY_PRODUCT_PRICE);
+  const isEmptyPrice = !price;
+  if (isEmptyPrice) return new ValidationResult(true, ERROR_MESSAGE.EMPTY_PRODUCT_PRICE);
+
   if (!isInteger(price)) return new ValidationResult(true, ERROR_MESSAGE.NOT_NUMBER_PRODUCT_PRICE);
+
   const priceNum = toInt(price, 0);
   if (priceNum < PRODUCT.PRICE.MIN || PRODUCT.PRICE.MAX < priceNum)
     return new ValidationResult(true, ERROR_MESSAGE.NOT_IN_VALID_RANGE_PRODUCT_PRICE);
+
   if (priceNum % COIN.MIN_UNIT)
     return new ValidationResult(true, ERROR_MESSAGE.NOT_DIVIDED_BY_TEN_PRODUCT_PRICE);
+
   return new ValidationResult(false);
 };
 
 export const validateProductQuantity = (quantity: string) => {
-  if (!quantity) return new ValidationResult(true, ERROR_MESSAGE.EMPTY_PRODUCT_QUANTITY);
+  const isEmptyQuantity = !quantity;
+  if (isEmptyQuantity) return new ValidationResult(true, ERROR_MESSAGE.EMPTY_PRODUCT_QUANTITY);
+
   if (!isInteger(quantity))
     return new ValidationResult(true, ERROR_MESSAGE.NOT_IN_VALID_RANGE_PRODUCT_QUANTITY);
+
   const quantityNum = toInt(quantity, 0);
   if (quantityNum < PRODUCT.QUANTITY.MIN || PRODUCT.QUANTITY.MAX < quantityNum)
     return new ValidationResult(true, ERROR_MESSAGE.NOT_IN_VALID_RANGE_PRODUCT_QUANTITY);
+
   return new ValidationResult(false);
 };
 
@@ -49,13 +63,19 @@ export const validateProduct = (
 };
 
 export const validateChargeCoins = (money: string, chargedMoney: number) => {
-  if (!money) return new ValidationResult(true, ERROR_MESSAGE.EMPTY_CHARGE_MONEY);
+  const isEmptyMoney = !money;
+  if (isEmptyMoney) return new ValidationResult(true, ERROR_MESSAGE.EMPTY_CHARGE_MONEY);
+
   if (!isInteger(money)) return new ValidationResult(true, ERROR_MESSAGE.NOT_NUMBER_CHARGE_MONEY);
+
   const moneyNum = toInt(money, 0);
   if (moneyNum <= 0) return new ValidationResult(true, ERROR_MESSAGE.NEGATIVE_CHARGE_MONEY);
+
   if (moneyNum % COIN.MIN_UNIT)
     return new ValidationResult(true, ERROR_MESSAGE.NOT_DIVIDED_BY_TEN_CHARGE_MONEY);
+
   if (MONEY.MAX < moneyNum + chargedMoney)
     return new ValidationResult(true, ERROR_MESSAGE.OVER_MAX_CHARGE_MONEY);
+
   return new ValidationResult(false);
 };
