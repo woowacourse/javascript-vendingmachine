e176b61da9d76de70ed197fb9ef0a3aba0768d39
airman5573 <rpf5573@kakao.com>
2022-03-31 13:25:35 +0900

    :recycle: refactor(readability): reducer 가독성 향상
    if문 대신 switch문으로 대체한다

--- src/flux/reducer.ts
+++ src/flux/reducer.ts
@@ -27,23 +27,45 @@ function mergeCoins(coins: CoinRecord, newCoins: CoinRecord) {
 
 const reducer = (state: AppState, { type, payload }: Action) => {
   const newState = deepCopy(state) as AppState;
-  if (type === ACTION.ADD_PRODUCT) {
-    newState.productList = [...newState.productList, { ...payload, isEditing: false }];
-  } else if (type === ACTION.CHANGE_EDIT_MODE) {
-    const { name, isEditing } = payload;
-    const index = newState.productList.findIndex((item) => item.name === name);
-    newState.productList[index].isEditing = isEditing;
-  } else if (type === ACTION.EDIT_PRODUCT) {
-    const { originalName, name, price, quantity } = payload;
-    const index = newState.productList.findIndex((item) => item.name === originalName);
-    newState.productList[index] = { name, price, quantity, isEditing: false };
-  } else if (type === ACTION.DELETE_PRODUCT) {
-    newState.productList = newState.productList.filter((item) => item.name !== payload);
-  } else if (type === ACTION.CHARGE_COINS) {
-    newState.chargedCoins = mergeCoins(newState.chargedCoins, moneyToCoin(payload));
-    newState.chargedMoney += payload;
-  } else if (type === ACTION.CHANGE_ACTIVE_TAB) {
-    newState.activeTab = payload;
+  switch (type) {
+    case ACTION.ADD_PRODUCT: {
+      newState.productList = [...newState.productList, { ...payload, isEditing: false }];
+      break;
+    }
+    case ACTION.CHANGE_EDIT_MODE: {
+      const { name, isEditing } = payload;
+      const index = newState.productList.findIndex((item) => item.name === name);
+      newState.productList[index].isEditing = isEditing;
+      break;
+    }
+    case ACTION.EDIT_PRODUCT: {
+      const { originalName, name, price, quantity } = payload;
+      const index = newState.productList.findIndex((item) => item.name === originalName);
+      newState.productList[index] = { name, price, quantity, isEditing: false };
+      break;
+    }
+    case ACTION.DELETE_PRODUCT: {
+      newState.productList = newState.productList.filter((item) => item.name !== payload);
+      break;
+    }
+    case ACTION.DELETE_PRODUCT: {
+      newState.productList = newState.productList.filter((item) => item.name !== payload);
+      break;
+    }
+    case ACTION.CHARGE_COINS: {
+      newState.chargedCoins = mergeCoins(newState.chargedCoins, moneyToCoin(payload));
+      newState.chargedMoney += payload;
+      break;
+    }
+    case ACTION.CHARGE_COINS: {
+      newState.chargedCoins = mergeCoins(newState.chargedCoins, moneyToCoin(payload));
+      newState.chargedMoney += payload;
+      break;
+    }
+    case ACTION.CHANGE_ACTIVE_TAB: {
+      newState.activeTab = payload;
+      break;
+    }
   }
   return newState;
 };
