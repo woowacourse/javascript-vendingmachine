3f7fcc5dbfe3d18f3395ea19fb52ca8d25d7a7db
junzzi <jjh01027717502@gmail.com>
2022-04-01 02:45:11 +0900

    chore: cross-env 삭제 후 웹팩 실행 옵션으로 처리

--- webpack.config.js
+++ webpack.config.js
@@ -6,71 +6,73 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 
-module.exports = {
-  mode: process.env.NODE_ENV,
-  entry: './src/index.ts',
-  resolve: {
-    extensions: ['.js', '.css', '.ts'],
-  },
-  devServer: {
-    port: 9000,
-    historyApiFallback: true,
-    proxy: {
-      '/api/*': {
-        target: 'http://localhost:3000',
-        pathRewrite: { '^/api': '' },
+module.exports = (env, options) => {
+  return {
+    mode: options.mode,
+    entry: './src/index.ts',
+    resolve: {
+      extensions: ['.js', '.css', '.ts'],
+    },
+    devServer: {
+      port: 9000,
+      historyApiFallback: true,
+      proxy: {
+        '/api/*': {
+          target: 'http://localhost:3000',
+          pathRewrite: { '^/api': '' },
+        },
       },
     },
-  },
-  devtool: 'source-map',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
+    devtool: 'source-map',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+              },
             },
+          ],
+        },
+        {
+          test: /\.ts$/,
+          exclude: /node_module/,
+          use: {
+            loader: 'ts-loader',
           },
-        ],
-      },
-      {
-        test: /\.ts$/,
-        exclude: /node_module/,
-        use: {
-          loader: 'ts-loader',
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.png$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
         },
-      },
+        {
+          test: /\.png$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        API_URL: JSON.stringify(process.env.API_URL),
+      }),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new CopyPlugin({
+        patterns: [{ from: 'public', to: '' }],
+      }),
     ],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      API_URL: JSON.stringify(process.env.API_URL),
-    }),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new CopyPlugin({
-      patterns: [{ from: 'public', to: '' }],
-    }),
-  ],
+  };
 };
