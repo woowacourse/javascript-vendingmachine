5034a73c6a327d6f50353547b23329d58ffdabb8
uk960214 <uk960214@gmail.com>
2022-03-31 23:44:05 +0900

    feat: section에 접근성 반영한 제목 추가

--- src/js/view/template.js
+++ src/js/view/template.js
@@ -1,7 +1,8 @@
 import { MONEY_NAME_STRING } from '../constants';
 
 export const manageProductTabTemplate = `
-<section title="상품 정보">
+<section aria-labelledby="manage-tab-title">
+  <h2 id="manage-tab-title">상품 추가하기</h2>
   <form id="add-product-form">
     <fieldset>
       <legend>추가할 상품 정보를 입력해주세요.</legend>
@@ -22,58 +23,57 @@ export const manageProductTabTemplate = `
     </fieldset>
   </form>
 </section>
-<section class="table-section" title="상품 현황">
-  <table id="product-status-table">
-    <caption>
-      상품 현황
-    </caption>
-    <tr>
-      <th>상품명</th>
-      <th>가격</th>
-      <th>수량</th>
-      <th>관리</th>
-    </tr>
-  </table>
-</section>`;
+<table id="product-status-table">
+  <caption>
+    상품 현황
+  </caption>
+  <tr>
+    <th>상품명</th>
+    <th>가격</th>
+    <th>수량</th>
+    <th>관리</th>
+  </tr>
+</table>
+`;
 
 export const addChangeTabTemplate = `
-<section title="잔돈 충전">
+<section aria-labelledby="change-tab-title">
+  <h2 id="change-tab-title">잔돈 충전하기</h2>
   <form id="add-change-form">
-    <label for="">자판기가 보유할 금액을 입력해주세요</label>
+    <label for="change">자판기가 보유할 금액을 입력해주세요</label>
     <div>
-      <input type="number" id="money-input" placeholder="금액" />
+      <input type="number" id="money-input" placeholder="금액" name="change"/>
       <button type="submit" class="submit-button">충전</button>
     </div>
   </form>
   <p>현재 보유 금액: <span id="total-change">0</span>원</p>
 </section>
-<section class="table-section" title="자판기 현황">
-  <table id="coin-status-table">
-    <caption>
-      자판기가 보유한 동전
-    </caption>
-    <tr>
-      <th>동전</th>
-      <th>개수</th>
-    </tr>
-    <tr>
-      <td>500원</td>
-      <td data-coin-name='${MONEY_NAME_STRING.COIN_500_WON}'>0개</td>
-    </tr>
-    <tr>
-      <td>100원</td>
-      <td data-coin-name='${MONEY_NAME_STRING.COIN_100_WON}'>0개</td>
-    </tr>
-    <tr>
-      <td>50원</td>
-      <td data-coin-name='${MONEY_NAME_STRING.COIN_50_WON}'>0개</td>
-    </tr>
-    <tr>
-      <td>10원</td>
-      <td data-coin-name='${MONEY_NAME_STRING.COIN_10_WON}'>0개</td>
-    </tr>
-  </table>
-</section>`;
+<table id="coin-status-table">
+  <caption>
+    자판기가 보유한 동전
+  </caption>
+  <tr>
+    <th>동전</th>
+    <th>개수</th>
+  </tr>
+  <tr>
+    <td>500원</td>
+    <td data-coin-name='${MONEY_NAME_STRING.COIN_500_WON}'>0개</td>
+  </tr>
+  <tr>
+    <td>100원</td>
+    <td data-coin-name='${MONEY_NAME_STRING.COIN_100_WON}'>0개</td>
+  </tr>
+  <tr>
+    <td>50원</td>
+    <td data-coin-name='${MONEY_NAME_STRING.COIN_50_WON}'>0개</td>
+  </tr>
+  <tr>
+    <td>10원</td>
+    <td data-coin-name='${MONEY_NAME_STRING.COIN_10_WON}'>0개</td>
+  </tr>
+</table>
+`;
 
 export const productTableRowTemplate = ({ name, price, stock, id }) => `
 <tr>
