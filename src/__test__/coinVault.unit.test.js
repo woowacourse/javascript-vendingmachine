470763f3dc99a504d76f7b5e08600c12636ef7af
sangminsong <ssmin940606@gmail.com>
2022-04-01 16:10:27 +0900

    refactor: 테스트 코드 에러 핸들링 테스트 로직 수정
    - toThrowError 사용
    - beforeEach를 통해 중복 로직 제거

--- src/__test__/coinVault.unit.test.js
+++ src/__test__/coinVault.unit.test.js
@@ -1,61 +1,60 @@
 import { CoinVault } from '../domain/CoinVault.ts';
 import { ERROR_MESSAGE } from '../utils/constants.ts';
 
-test('자판기가 최초 보유한 금액은 0원이다.', () => {
-  const coinValut = new CoinVault();
-  expect(coinValut.getCoins().coin500).toStrictEqual(0);
-  expect(coinValut.getCoins().coin100).toStrictEqual(0);
-  expect(coinValut.getCoins().coin50).toStrictEqual(0);
-  expect(coinValut.getCoins().coin10).toStrictEqual(0);
-  expect(coinValut.getBalance()).toStrictEqual(0);
-});
+describe('잔돈생성 테스트', () => {
+  let coinValut;
 
-test('자판기 보유 금액은 최대 100,000원 이다.', () => {
-  const coinValut = new CoinVault();
-  try {
-    coinValut.chargeMoney(110000);
-  } catch (err) {
-    expect(err).toStrictEqual(new Error(ERROR_MESSAGE.OVER_BALANCE_LIMIT));
-  }
-});
+  beforeEach(() => {
+    coinValut = new CoinVault();
+  });
 
-test('자판기 잔돈은 10원 단위만 투입 가능하다', () => {
-  const coinValut = new CoinVault();
-  try {
-    coinValut.chargeMoney(1234);
-  } catch (err) {
-    expect(err).toStrictEqual(new Error(ERROR_MESSAGE.NOT_DIVIDED_BY_COIN_UNIT));
-  }
-});
+  test('자판기가 최초 보유한 금액은 0원이다.', () => {
+    const { coin500, coin100, coin50, coin10 } = coinValut.getCoins();
+    expect(coin500).toStrictEqual(0);
+    expect(coin100).toStrictEqual(0);
+    expect(coin50).toStrictEqual(0);
+    expect(coin10).toStrictEqual(0);
 
-test('자판기 보유 금액만큼의 동전이 무작위로 생성된다.', () => {
-  const coinValut = new CoinVault();
-  expect(coinValut.getBalance()).toStrictEqual(0);
-  coinValut.chargeMoney(5000);
-  expect(coinValut.getBalance()).toStrictEqual(5000);
-});
+    expect(coinValut.getBalance()).toStrictEqual(0);
+  });
 
-test('자판기 보유 금액을 누적하여 충전할 수 있다', () => {
-  const coinValut = new CoinVault();
-  expect(coinValut.getBalance()).toStrictEqual(0);
+  test('자판기 보유 금액은 최대 100,000원 이다.', () => {
+    expect(() => coinValut.chargeMoney(100010)).toThrowError(
+      new Error(ERROR_MESSAGE.OVER_BALANCE_LIMIT)
+    );
+  });
 
-  coinValut.chargeMoney(5000);
-  expect(coinValut.getBalance()).toStrictEqual(5000);
+  test('자판기 잔돈은 10원 단위만 투입 가능하다', () => {
+    expect(() => coinValut.chargeMoney(1234)).toThrowError(
+      new Error(ERROR_MESSAGE.NOT_DIVIDED_BY_COIN_UNIT)
+    );
+  });
 
-  coinValut.chargeMoney(5000);
-  expect(coinValut.getBalance()).toStrictEqual(10000);
-});
+  test('잔돈을 충전할 수 있다', () => {
+    expect(coinValut.getBalance()).toStrictEqual(0);
+    coinValut.chargeMoney(5000);
 
-test('추가 충전후 합산 금액도 100,000 을 넘을 수 없다', () => {
-  const coinValut = new CoinVault();
-  expect(coinValut.getBalance()).toStrictEqual(0);
+    expect(coinValut.getBalance()).toStrictEqual(5000);
+  });
 
-  coinValut.chargeMoney(50000);
-  expect(coinValut.getBalance()).toStrictEqual(50000);
+  test('자판기 보유 금액을 누적하여 충전할 수 있다', () => {
+    expect(coinValut.getBalance()).toStrictEqual(0);
 
-  try {
-    coinValut.chargeMoney(60000);
-  } catch (err) {
-    expect(err).toStrictEqual(new Error(ERROR_MESSAGE.OVER_BALANCE_LIMIT));
-  }
+    coinValut.chargeMoney(5000);
+    expect(coinValut.getBalance()).toStrictEqual(5000);
+
+    coinValut.chargeMoney(5000);
+    expect(coinValut.getBalance()).toStrictEqual(10000);
+  });
+
+  test('추가 충전후 합산 금액도 100,000 을 넘을 수 없다', () => {
+    expect(coinValut.getBalance()).toStrictEqual(0);
+
+    coinValut.chargeMoney(50000);
+    expect(coinValut.getBalance()).toStrictEqual(50000);
+
+    expect(() => coinValut.chargeMoney(50010)).toThrowError(
+      new Error(ERROR_MESSAGE.OVER_BALANCE_LIMIT)
+    );
+  });
 });
--- src/__test__/product.unit.test.js
+++ src/__test__/product.unit.test.js
@@ -2,134 +2,119 @@ import { ProductCatalog } from '../domain/ProductCatalog.ts';
 
 import { ERROR_MESSAGE } from '../utils/constants.ts';
 
-test('상품명, 가격, 수량을 입력하여 물품을 등록할 수 있다.', () => {
-  const productCatalog = new ProductCatalog();
-  expect(productCatalog.getProductList()).toHaveLength(0);
+describe('상품CRUD unit 테스트', () => {
+  let productCatalog;
 
-  productCatalog.addProduct({ name: '코카콜라', price: 1000, quantity: 20 });
-
-  expect(productCatalog.getProductList()[0].getAllProperties()).toStrictEqual({
-    name: '코카콜라',
-    price: 1000,
-    quantity: 20,
+  beforeEach(() => {
+    productCatalog = new ProductCatalog();
   });
-});
 
-test('상품명은 최대 10글자까지 가능하다.', () => {
-  const productCatalog = new ProductCatalog();
-  try {
-    productCatalog.addProduct({
-      name: '코카콜라맛있다맛있으면또먹지딩동댕동',
-      price: 1000,
-      quantity: 20,
-    });
-  } catch (err) {
-    expect(err).toStrictEqual(new Error(ERROR_MESSAGE.OVER_PRODUCT_NAME_LENGTH_LIMIT));
-  }
-});
+  test('상품명, 가격, 수량을 입력하여 물품을 등록할 수 있다.', () => {
+    expect(productCatalog.getProductList()).toHaveLength(0);
 
-test('상품가격은 100원이상 이어야 한다.', () => {
-  const productCatalog = new ProductCatalog();
-  try {
-    productCatalog.addProduct({
-      name: '코카콜라',
-      price: 10,
-      quantity: 20,
-    });
-  } catch (err) {
-    expect(err).toStrictEqual(new Error(ERROR_MESSAGE.NOT_WITHIN_PRODUCT_PRICE_RANGE));
-  }
-});
+    productCatalog.addProduct({ name: '코카콜라', price: 1000, quantity: 20 });
 
-test('상품가격은 10,000원 이하 이어야 한다.', () => {
-  const productCatalog = new ProductCatalog();
-  try {
-    productCatalog.addProduct({
-      name: '코카콜라',
-      price: 10010,
-      quantity: 20,
-    });
-  } catch (err) {
-    expect(err).toStrictEqual(new Error(ERROR_MESSAGE.NOT_WITHIN_PRODUCT_PRICE_RANGE));
-  }
-});
-
-test('상품가격은 10원 단위여야 한다', () => {
-  const productCatalog = new ProductCatalog();
-  try {
-    productCatalog.addProduct({
-      name: '코카콜라',
-      price: 105,
-      quantity: 20,
-    });
-  } catch (err) {
-    expect(err).toStrictEqual(new Error(ERROR_MESSAGE.NOT_DIVIDED_BY_PRODUCT_PRICE_UNIT));
-  }
-});
-
-test('상품수량은 최대 20개까지 가능하다', () => {
-  const productCatalog = new ProductCatalog();
-  try {
-    productCatalog.addProduct({
+    expect(productCatalog.findProduct('코카콜라').getAllProperties()).toStrictEqual({
       name: '코카콜라',
       price: 1000,
-      quantity: 21,
+      quantity: 20,
     });
-  } catch (err) {
-    expect(err).toStrictEqual(new Error(ERROR_MESSAGE.OVER_PRODUCT_QUANTITY_LIMIT));
-  }
-});
-
-test('상품을 수정할 수 있다.', () => {
-  const productCatalog = new ProductCatalog();
-  productCatalog.addProduct({
-    name: '코카콜라',
-    price: 1000,
-    quantity: 10,
-  });
-  productCatalog.editProduct('코카콜라', { name: '펩시', price: 500, quantity: 10 });
-
-  expect(productCatalog.findProduct('펩시').getAllProperties()).toStrictEqual({
-    name: '펩시',
-    price: 500,
-    quantity: 10,
-  });
-});
-
-test('상품을 삭제할 수 있다', () => {
-  const productCatalog = new ProductCatalog();
-
-  productCatalog.addProduct({
-    name: '코카콜라',
-    price: 1000,
-    quantity: 20,
-  });
-  expect(productCatalog.findProduct('코카콜라').getAllProperties()).toStrictEqual({
-    name: '코카콜라',
-    price: 1000,
-    quantity: 20,
   });
 
-  productCatalog.deleteProduct('코카콜라');
-  expect(productCatalog.findProduct('코카콜라')).toBeNull;
-});
-
-test('이미 존재하는 상품을 추가할 수 없다', () => {
-  const productCatalog = new ProductCatalog();
-
-  productCatalog.addProduct({
-    name: '코카콜라',
-    price: 1000,
-    quantity: 20,
+  test('상품명은 최대 10글자까지 가능하다.', () => {
+    expect(() =>
+      productCatalog.addProduct({
+        name: '코카콜라맛있다맛있으면또먹지딩동댕동',
+        price: 1000,
+        quantity: 20,
+      })
+    ).toThrowError(new Error(ERROR_MESSAGE.OVER_PRODUCT_NAME_LENGTH_LIMIT));
   });
 
-  try {
+  test('상품가격은 100원이상 이어야 한다.', () => {
+    expect(() =>
+      productCatalog.addProduct({
+        name: '코카콜라',
+        price: 10,
+        quantity: 20,
+      })
+    ).toThrowError(new Error(ERROR_MESSAGE.NOT_WITHIN_PRODUCT_PRICE_RANGE));
+  });
+
+  test('상품가격은 10,000원 이하 이어야 한다.', () => {
+    expect(() =>
+      productCatalog.addProduct({
+        name: '코카콜라',
+        price: 10010,
+        quantity: 20,
+      })
+    ).toThrowError(new Error(ERROR_MESSAGE.NOT_WITHIN_PRODUCT_PRICE_RANGE));
+  });
+
+  test('상품가격은 10원 단위여야 한다', () => {
+    expect(() =>
+      productCatalog.addProduct({
+        name: '코카콜라',
+        price: 105,
+        quantity: 20,
+      })
+    ).toThrowError(new Error(ERROR_MESSAGE.NOT_DIVIDED_BY_PRODUCT_PRICE_UNIT));
+  });
+
+  test('상품수량은 최대 20개까지 가능하다', () => {
+    expect(() =>
+      productCatalog.addProduct({
+        name: '코카콜라',
+        price: 1000,
+        quantity: 21,
+      })
+    ).toThrowError(new Error(ERROR_MESSAGE.OVER_PRODUCT_QUANTITY_LIMIT));
+  });
+
+  test('상품을 수정할 수 있다.', () => {
+    productCatalog.addProduct({
+      name: '코카콜라',
+      price: 1000,
+      quantity: 10,
+    });
+    productCatalog.editProduct('코카콜라', { name: '펩시', price: 500, quantity: 10 });
+
+    expect(productCatalog.findProduct('펩시').getAllProperties()).toStrictEqual({
+      name: '펩시',
+      price: 500,
+      quantity: 10,
+    });
+  });
+
+  test('상품을 삭제할 수 있다', () => {
     productCatalog.addProduct({
       name: '코카콜라',
       price: 1000,
       quantity: 20,
     });
-  } catch (err) {
-    expect(err).toStrictEqual(new Error(ERROR_MESSAGE.DUPLICATE_PRODUCT_NAME_EXIST));
-  }
+    expect(productCatalog.findProduct('코카콜라').getAllProperties()).toStrictEqual({
+      name: '코카콜라',
+      price: 1000,
+      quantity: 20,
+    });
+
+    productCatalog.deleteProduct('코카콜라');
+    expect(productCatalog.findProduct('코카콜라')).toBeNull;
+  });
+
+  test('이미 존재하는 상품을 추가할 수 없다', () => {
+    productCatalog.addProduct({
+      name: '코카콜라',
+      price: 1000,
+      quantity: 20,
+    });
+
+    expect(() =>
+      productCatalog.addProduct({
+        name: '코카콜라',
+        price: 1000,
+        quantity: 20,
+      })
+    ).toThrowError(new Error(ERROR_MESSAGE.DUPLICATE_PRODUCT_NAME_EXIST));
+  });
 });
