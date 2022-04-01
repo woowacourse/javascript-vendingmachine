1a87e62803a4862637063c0139c201f3c5f7f223
KIM YEJI <rladpwl0512@gmail.com>
2022-04-01 18:32:15 +0900

    :recycle: refactor(router): 현재 hash에 따라 화면을 렌더링하는 함수 수정

--- src/router.js
+++ src/router.js
@@ -1,43 +1,26 @@
 import { $ } from './utils/dom';
 
-const $productManageButton = $('.nav__product-manage-button');
+const $nav = document.querySelector('nav');
 const $productManageContainer = $('product-manage-container');
-const $productNameInput = $('.product-name-input');
-
-const $coinChargeButton = $('.nav__coin-charge-button');
 const $coinChargeContainer = $('coin-charge-container');
-const $coinInput = $('#coin-input');
 
-const menuContainers = [$productManageContainer, $coinChargeContainer];
-const menuButtons = [$productManageButton, $coinChargeButton];
-
-const renderFocusContainer = ($currentContainer, $currentButton, $currentInput) => {
-  menuContainers.forEach((container) => {
-    if (container === $currentContainer) {
-      container.show();
-      return;
-    }
-    container.hide();
-  });
-
-  menuButtons.forEach((button) => {
-    if (button === $currentButton) {
-      button.classList.add('clicked');
-      return;
-    }
-    button.classList.remove('clicked');
-  });
-
-  $currentInput.focus();
-};
+const routes = [
+  { hash: '#!product-manage', target: $productManageContainer },
+  { hash: '#!coin-charge', target: $coinChargeContainer },
+];
 
 const renderTargetContainer = (currentHash) => {
-  if (currentHash === '#!product-manage') {
-    renderFocusContainer($productManageContainer, $productManageButton, $productNameInput);
-  }
-  if (currentHash === '#!coin-charge') {
-    renderFocusContainer($coinChargeContainer, $coinChargeButton, $coinInput);
-  }
+  routes.forEach(({ hash, target }) => {
+    const $menu = $nav.querySelector(`[href='${hash}']`);
+    if (currentHash === hash) {
+      target.show();
+      $menu.querySelector('button').classList.add('clicked');
+      target.querySelector('input').focus();
+      return;
+    }
+    target.hide();
+    $menu.querySelector('button').classList.remove('clicked');
+  });
 };
 
 const renderInitContainer = (currentHash) => {
