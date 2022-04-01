116f94c7e961eac633881070cfbd91404869681c
DomMorello <zuzudnf@gmail.com>
2022-04-01 17:07:43 +0900

    style: util.js prettier 적용

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,8 +1,12 @@
-export const $ = <E extends Element, P extends Element>(selector: string, baseElement: P | Document = document): E | null =>
-  baseElement.querySelector(selector);
+export const $ = <E extends Element, P extends Element>(
+  selector: string,
+  baseElement: P | Document = document,
+): E | null => baseElement.querySelector(selector);
 
-export const $$ = <E extends Element, P extends Element>(selector: string, baseElement: P | Document = document): NodeListOf<E> =>
-  baseElement.querySelectorAll(selector);
+export const $$ = <E extends Element, P extends Element>(
+  selector: string,
+  baseElement: P | Document = document,
+): NodeListOf<E> => baseElement.querySelectorAll(selector);
 
 export const getRandomNumber = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
