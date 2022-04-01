c16bb46054acf3c49777f44aac05a46df7289ce9
sangminsong <ssmin940606@gmail.com>
2022-04-01 22:22:09 +0900

    feat: typescript 상수화 적용
    - as const를 활용해서 상수 객체의 프로퍼티 값을 바꿀 수 없도록 적용

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,25 +2,25 @@ export const CHEAPEST_COIN = 10;
 
 export const COIN_CONDITION = {
   UNIT_PRICE: 10,
-};
+} as const;
 
 export const COINS_INIT_QUANTITY = {
   coin500: 0,
   coin100: 0,
   coin50: 0,
   coin10: 0,
-};
+} as const;
 
-export const COINS_PRICE_TABLE = {
+export const COINS_UNIT_TABLE = {
   coin500: 500,
   coin100: 100,
   coin50: 50,
   coin10: 10,
-};
+} as const;
 
 export const COIN_VAULT_CONDITION = {
   MAX_BALANCE: 100000,
-};
+} as const;
 
 export const ERROR_MESSAGE = {
   NOT_DIVIDED_BY_COIN_UNIT: '상평통보는 안 받습니다. 10원단위로 넣어주세요!',
@@ -32,13 +32,13 @@ export const ERROR_MESSAGE = {
   OVER_PRODUCT_QUANTITY_LIMIT: '수량은 최대 20개까지만 가능합니다~',
   OVER_USER_MONEY_LIMIT: '10,000원까지만 투입 가능합니다',
   DUPLICATE_PRODUCT_NAME_EXIST: '중복된 상품명입니다',
-};
+} as const;
 
 export const URL_PATH = {
   HOME: '/',
   PRODUCT_MANAGE: '/productManage',
   BALANCE_CHAREGE: '/balanceCharge',
-};
+} as const;
 
 export const PRODUCT_CONDITION = {
   MAX_NAME_LENGTH: 10,
@@ -46,4 +46,4 @@ export const PRODUCT_CONDITION = {
   MAX_PRICE: 10000,
   UNIT_PRICE: 10,
   MAX_QUANTITY: 20,
-};
+} as const;
