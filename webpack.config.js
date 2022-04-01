960db3737bda6946561d2d2e90b00be2bcfe9289
airman5573 <68623798+airman5573@users.noreply.github.com>
2022-04-01 18:50:11 +0900

    [1단계 - 자판기] 병민(윤병인) 미션 제출합니다. (#40)

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.ts",
+  mode: 'development',
+  entry: './src/index.ts',
   resolve: {
-    extensions: [".js", ".css", ".ts"],
+    extensions: ['.js', '.css', '.ts'],
   },
   devServer: {
     port: 9000,
+    historyApiFallback: true,
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -23,9 +25,9 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
+              presets: ['@babel/preset-env'],
             },
           },
         ],
@@ -34,19 +36,29 @@ module.exports = {
         test: /\.ts$/,
         exclude: /node_module/,
         use: {
-          loader: "ts-loader",
+          loader: 'ts-loader',
         },
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /.(png|jpe?g|gif|json)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[contenthash].[ext]',
+        },
       },
     ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
     }),
   ],
 };
