91bbe625063157aa8789e031cb700bf9d105d21c
Soyi Jeon <soyi.dev@gmail.com>
2022-04-03 04:22:13 +0900

    feat: 로그인, 회원가입 submit 이벤트 추가

--- src/es/View/LoginPageView.js
+++ src/es/View/LoginPageView.js
@@ -1,13 +1,13 @@
-import { $ } from '../utils';
+import { $, getInnerInputValues } from '../utils';
 
 const pageTemplate = `
   <section class="user-information-form-section">
       <form id="login-form" >
           <label>이메일<br>
-              <input type="email" placeholder="이메일 주소를 입력해주세요">
+              <input type="email" name="email" placeholder="이메일 주소를 입력해주세요">
           </label>
           <label>비밀번호<br>
-              <input type="password" placeholder="비밀번호를 입력해주세요">
+              <input type="password" name="password" placeholder="비밀번호를 입력해주세요">
           </label>
           <button class="button accent">확인</button>
       </form>
@@ -18,5 +18,11 @@ const pageTemplate = `
 export default class LoginPageView {
   loadPage = () => {
     $('.main').innerHTML = pageTemplate;
+    $('#login-form').addEventListener('submit', this.onSubmitLoginForm);
+  };
+
+  onSubmitLoginForm = (event) => {
+    event.preventDefault();
+    console.log(getInnerInputValues(event.target));
   };
 }
--- src/es/View/SignUpPageView.js
+++ src/es/View/SignUpPageView.js
@@ -1,19 +1,19 @@
-import { $ } from '../utils';
+import { $, getInnerInputValues } from '../utils';
 
 const pageTemplate = `
   <section class="user-information-form-section">
       <form id="signup-form" >
           <label>이메일<br>
-              <input type="email" placeholder="이메일 주소를 입력해주세요">
+              <input type="email" name="email" placeholder="이메일 주소를 입력해주세요">
           </label>
           <label>이름<br>
-              <input type="text" placeholder="이름을 입력해주세요">
+              <input type="text" name="name" placeholder="이름을 입력해주세요">
           </label>
           <label>비밀번호<br>
-              <input type="password" placeholder="비밀번호를 입력해주세요">
+              <input type="password" name="password" placeholder="비밀번호를 입력해주세요">
           </label>
           <label>비밀번호 확인<br>
-              <input type="password" placeholder="비밀번호를 입력해주세요">
+              <input type="password" name="passwordConfirm" placeholder="비밀번호를 입력해주세요">
           </label>
           <button class="button accent">확인</button>
       </form>
@@ -23,5 +23,11 @@ const pageTemplate = `
 export default class SignUpPageView {
   loadPage = () => {
     $('.main').innerHTML = pageTemplate;
+    $('#signup-form').addEventListener('submit', this.onSubmitSignUpForm);
+  };
+
+  onSubmitSignUpForm = (event) => {
+    event.preventDefault();
+    console.log(getInnerInputValues(event.target));
   };
 }
