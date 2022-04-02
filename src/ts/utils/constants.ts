df474162a492a909612d2701c5a466414fa109b5
InKyoJeong <viapolar@naver.com>
2022-04-02 22:13:19 +0900

    fix: 잘못된 검증 범위 수정 - 제품 수량 최소,최대값 수정

--- src/ts/utils/constants.ts
+++ src/ts/utils/constants.ts
@@ -21,7 +21,7 @@ const ERROR_MESSAGES = {
   EXCEED_PRODUCT_LENGTH: `상품명은 최대 ${PRODUCT.MAX_LENGTH}글자까지 입력 가능합니다.`,
   INVALID_PRODUCT_PRICE_RANGE: `상품 가격은 ${PRODUCT.MIN_PRICE}원 ~ ${PRODUCT.MAX_PRICE}원 사이로 입력해주세요.`,
   INVALID_PRODUCT_PRICE_UNIT: `상품 가격은 ${PRODUCT.UNIT}원으로 나누어 떨어져야합니다.`,
-  INVALID_PRODUCT_QUANTITY_RANGE: `제품당 수량은 최소 ${PRODUCT.MAX_QUANTITY}개부터 최대 ${PRODUCT.MIN_QUANTITY}개까지 가능합니다.`,
+  INVALID_PRODUCT_QUANTITY_RANGE: `제품당 수량은 최소 ${PRODUCT.MIN_QUANTITY}개부터 최대 ${PRODUCT.MAX_QUANTITY}개까지 가능합니다.`,
   INVALID_PRODUCT_QUANTITY_UNIT: "제품의 수량은 소수점으로 입력할 수 없습니다.",
   INVALID_CHARGE_RANGE: `최소 ${CHARGE.MIN_PRICE}원, 최대 ${CHARGE.MAX_PRICE}원까지 충전할 수 있습니다.`,
   INVALID_CHARGE_UNIT: `잔돈은 ${CHARGE.UNIT}원으로 나누어 떨어지는 금액만 투입할 수 있습니다.`,
