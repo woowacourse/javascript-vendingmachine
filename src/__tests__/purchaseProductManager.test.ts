a8c65af74d585d3df4ae8817f5e8238f306178a2
kkojae <jaejeung3210@gmail.com>
2022-04-02 16:28:21 +0900

    test: VendingMachinePurchaseManager test 케이스 수정 및 추가
    - 누적 금액에서 상품의 가격만큼 차감된 금액이 저장되는지 확인한다.
    - 누적 금액보다 상품의 가격이 비쌀 경우 에러가 발생한다.
    - 보유할 수 있는 최대 누적 금액인 10000원을 초과할 경우 에러가 발생한다.

--- src/__tests__/purchaseProductManager.test.ts
+++ src/__tests__/purchaseProductManager.test.ts
@@ -4,7 +4,10 @@
 //   - [ ] 최대 투입 금액은 10,000원이다. // utils
 // - [ ] 금액은 누적으로 투입할 수 있다. // domain 누적 되는지 확인
 // - [ ] 사용자가 버튼을 클릭했을 때 해당 행위가 정상적으로 동작하거나, 실패하였음을 snackbar를 통해 보여준다.
-import VendingMachinePurchaseManager from '../ts/domains/VendingMachinePurchaseManager';
+import VendingMachinePurchaseManager, {
+  checkValidPurchaseMoney,
+  checkConsumerChargeMoneyLessThenPurchaseMoney,
+} from '../ts/domains/VendingMachinePurchaseManager';
 
 test('최초 상품 구매를 위한 금액은 0원이며, 각 동전의 개수가 0개인지 확인한다.', () => {
   const vendingMachinePurchaseManager = new VendingMachinePurchaseManager();
@@ -28,21 +31,39 @@ test('상품 구매를 위해 투입한 금액이 누적되는지 확인한다.'
   expect(vendingMachinePurchaseManager.getPurchaseMoney()).toBe(2000);
 });
 
-test('보유할 수 있는 최대 누적 금액인 10000원을 초과할 경우 에러가 발생한다.', () => {
-  const vendingMachinePurchaseManager = new VendingMachinePurchaseManager();
-
-  vendingMachinePurchaseManager.addPurchaseMoney(10010);
-
-  expect(() => {
-    vendingMachinePurchaseManager.getPurchaseMoney();
-  }).toThrowError(
-    '현재 투입된 금액은 0000원 입니다. 누적으로 투입할 수 있는 최대 금액은 10000원 이니 투입된 금액을 확인 후 다시 시도해주세요.'
-  );
-});
-
 test('보유할 수 있는 최대 누적 금액인 10000원 까지 투입되는지 확인한다.', () => {
   const vendingMachinePurchaseManager = new VendingMachinePurchaseManager();
 
   vendingMachinePurchaseManager.addPurchaseMoney(10000);
+
   expect(vendingMachinePurchaseManager.getPurchaseMoney()).toBe(10000);
+});
+
+test('누적 금액에서 상품의 가격만큼 차감된 금액이 저장되는지 확인한다.', () => {
+  const vendingMachinePurchaseManager = new VendingMachinePurchaseManager();
+
+  vendingMachinePurchaseManager.addPurchaseMoney(10000);
+  vendingMachinePurchaseManager.subtractPurchaseMoney(1000);
+
+  expect(vendingMachinePurchaseManager.getPurchaseMoney()).toBe(9000);
+});
+
+test('누적 금액보다 상품의 가격이 비쌀 경우 에러가 발생한다.', () => {
+  const consumerChargeMoney = 1000;
+  const productPrice = 1100;
+
+  expect(() => {
+    checkConsumerChargeMoneyLessThenPurchaseMoney(
+      consumerChargeMoney,
+      productPrice
+    );
+  }).toThrowError();
+});
+
+test('보유할 수 있는 최대 누적 금액인 10000원을 초과할 경우 에러가 발생한다.', () => {
+  expect(() => {
+    checkValidPurchaseMoney(10010);
+  }).toThrowError(
+    '충전 금액을 잘못 입력하셨습니다. 충전 금액은 최소 10원 이상 10000원 이하로 입력해주세요.'
+  );
 });
