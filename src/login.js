dbdb7ebf0a723937d1c487584e4158fc6650f26e
Hwang Jung Min <hwangstar156@gmail.com>
2022-04-02 14:52:33 +0900

    feat: 로그인 기능 추가

--- src/login.js
+++ src/login.js
@@ -1,1 +1,37 @@
 import "./css/login";
+
+class Login {
+  constructor() {
+    this.$loginEmailInput = document.querySelector("#login-email-input");
+    this.$loginPasswordInput = document.querySelector("#login-password-input");
+    this.$loginForm = document.querySelector("#login-form");
+    this.$loginForm.addEventListener("submit", this.onSubmit);
+  }
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      body: new URLSearchParams(new FormData(this.$loginForm)),
+    });
+
+    const body = await response.json();
+
+    if (!response.ok) {
+      alert(body);
+      return;
+    }
+
+    const user = {
+      key: body.accessToken,
+      name: body.user.name,
+      id: body.user.id,
+      email: body.user.email,
+    };
+    localStorage.setItem("user-info", JSON.stringify(user));
+    location.href = "./";
+  };
+}
+
+new Login();
