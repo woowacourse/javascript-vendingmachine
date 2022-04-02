fb499aa4c1486ce9043cad43f9dede4dd85d3c47
2019015414 <2019015414@hanyang.ac.kr>
2022-04-03 03:49:53 +0900

    test: cypress test 추가

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,48 @@
+describe('관리자 인증 테스트', () => {
+  beforeEach(() => {
+    cy.visit('dist/index.html');
+  });
+
+  it('회원가입 및 로그인 테스트', () => {
+    const email = 'test1@test1';
+    const name = 'test';
+    const password = 'Password1234';
+    cy.signupAndLogin(email, name, password);
+
+    cy.get('nav').should('be.visible');
+  });
+
+  it('로그아웃 테스트', () => {
+    const email = 'test1@test1';
+    const password = 'Password1234';
+    cy.get('#login-button').click();
+    cy.login(email, password);
+
+    cy.get('#manager-name-button').click();
+    cy.get('#logout').click();
+    cy.get('#login-button').should('be.visible');
+  });
+
+  it('회원 정보 수정 테스트', () => {
+    const email = 'test1@test1';
+    const password = 'Password1234';
+    cy.get('#login-button').click();
+    cy.login(email, password);
+
+    cy.get('#manager-name-button').click();
+    cy.get('#modify-info').click();
+
+    // 회원 정보 수정
+    const newName = 'newName';
+    cy.get('#name-input').type(newName);
+    cy.get('#password-input').type(password);
+    cy.get('#password-check').type(password);
+    cy.get('#sign-up-info-form button').click();
+
+    cy.wait(1000);
+
+    cy.login(email, password);
+
+    cy.get('#manager-name-button').contains('n');
+  });
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,24 @@
+Cypress.Commands.add('login', (email, password) => {
+  cy.get('#email-input').type(email);
+  cy.get('#password-input').type(password);
+  cy.get('button').click();
+
+  cy.wait(1000);
+});
+
+Cypress.Commands.add('signupAndLogin', (email, name, password) => {
+  // 회원가입 페이지 이동
+  cy.get('.sign-up-button').click();
+  cy.get('.sign-up-link').click();
+
+  // 회원가입
+  cy.get('#email-input').type(email);
+  cy.get('#name-input').type(name);
+  cy.get('#password-input').type(password);
+  cy.get('#password-check').type(password);
+  cy.get('button').click();
+
+  cy.wait(1000);
+
+  cy.login(email, password);
+});
