63b8a96f10c66b30110dd2d00adc32f0b42579c1
intae92 <kangit2013@gmail.com>
2022-04-03 19:17:51 +0900

    test: cypress 설치, 환경설정

---
+++ cypress/integration/vendingmachine.spec.js
@@ -0,0 +1,12 @@
+import { DELETE_CONFIRM_MESSAGE } from '../../src/js/constants';
+
+describe('구현 결과가 요구사항과 일치해야 한다.', () => {
+  const baseUrl = './index.html';
+
+  before(() => {
+    cy.visit(baseUrl);
+  });
+
+    it('test', () => {
+  });
+});
