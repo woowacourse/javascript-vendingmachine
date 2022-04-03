ec3e1747e0a50bf9c67834153e77fccd65917b0d
intae92 <kangit2013@gmail.com>
2022-04-04 08:38:18 +0900

    make it better

---
+++ auth-middleware.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+    // res.header('X-Hello', 'World')
+    next()
+    // if(req.path === '/users'){
+    //     next()
+    // } else{
+    //     res.status(400).json({err: "cokecoke"})
+    // }
+}
---
+++ server.js
@@ -0,0 +1,31 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+
+const path = require('path')
+
+const router = jsonServer.router(path.join(__dirname,'db.json'))
+
+const middlewares = jsonServer.defaults()
+
+const customMiddlewares = require(path.join(__dirname, 'auth-middleware.js'))
+
+const port = process.env.PORT || 3000;
+
+server.use(middlewares)
+server.use(customMiddlewares)
+
+const customRouter = jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/:resource/:id/show": "/:resource/:id",
+    "/posts/:category": "/posts?category=:category",
+    "/articles\\?id=:id": "/posts/:id",
+    "/findUser\\?name=:name": "/users/?name=:name"
+})
+
+server.use(customRouter)
+
+server.use(router)
+
+server.listen(port, () => {
+  console.log('JSON Server is running')
+})
