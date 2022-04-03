52c7a0c370947d51b53b20337e5299f9f1ac4747
DomMorello <zuzudnf@gmail.com>
2022-04-03 12:08:43 +0900

    feat: 구매 가능 상품 현황을 화면에 보여준다

--- src/ts/view/PurchaseProductView.ts
+++ src/ts/view/PurchaseProductView.ts
@@ -5,17 +5,19 @@ export interface PurchaseProductViewInterface {
   $purchaseForm: HTMLFormElement;
   $purchaseInput: HTMLInputElement;
   $purchaseMoney: HTMLSpanElement;
+  $availableProducts: HTMLTableSectionElement;
 
   vendingMachine: VendingMachineInterface;
 
-  renderPurchaseProduct(): void;
   handleSubmit(event: SubmitEvent): void;
+  renderPurchaseProduct(): void;
 }
 
 class PurchaseProductView implements PurchaseProductViewInterface {
   $purchaseForm: HTMLFormElement;
   $purchaseInput: HTMLInputElement;
   $purchaseMoney: HTMLSpanElement;
+  $availableProducts: HTMLTableSectionElement;
 
   vendingMachine: VendingMachineInterface;
 
@@ -23,15 +25,12 @@ class PurchaseProductView implements PurchaseProductViewInterface {
     this.$purchaseForm = $('.purchase-form');
     this.$purchaseInput = $('.purchase-input', this.$purchaseForm);
     this.$purchaseMoney = $('.purchase-money', this.$purchaseForm);
+    this.$availableProducts = $('.available-products-body');
 
     this.vendingMachine = vendingMachine;
 
     this.$purchaseForm.addEventListener('submit', this.handleSubmit);
   }
-
-  renderPurchaseProduct = () => {
-    this.$purchaseMoney.textContent = String(this.vendingMachine.purchaseMoney.money);
-  };
 
   handleSubmit = (event: SubmitEvent) => {
     event.preventDefault();
@@ -42,6 +41,30 @@ class PurchaseProductView implements PurchaseProductViewInterface {
     } catch (error) {
       alert(error.message);
     }
+  };
+
+  renderPurchaseProduct = () => {
+    this.$purchaseMoney.textContent = String(this.vendingMachine.purchaseMoney.money);
+    this.renderAvailableProduct();
+  };
+
+  renderAvailableProduct = () => {
+    const template = this.vendingMachine.products
+      .map(({ name, price, quantity }) => {
+        return `
+          <tr class="product-row" data-name="${name}">
+            <td class="product-row-name">${name}</td>
+            <td class="product-row-price">${price}</td>
+            <td class="product-row-quantity">${quantity}</td>
+            <td>
+              <button class="purchase-button small-button" data-name="${name}">구매</button>
+            </td>
+          </tr>
+        `;
+      })
+      .join('');
+
+    this.$availableProducts.innerHTML = template;
   };
 }
 
