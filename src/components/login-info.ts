97d626077d6a925b77f1873440bb9f168e07862d
jhy979 <jhy979@gmail.com>
2022-04-03 21:04:26 +0900

    feat(ui): 유저 정보 썸네일 생성

--- src/components/login-info.ts
+++ src/components/login-info.ts
@@ -1,12 +1,27 @@
 import Component from '../abstract/component';
 import { customElement } from '../decorators/decortators';
+import { getUserInfo } from '../member';
 
 @customElement('login-info')
 class LoginInfo extends Component {
-  template(): string {
+  template(thumbnail = 'A'): string {
     return `
-    <button type="button" class="btn btn-primary">우</button>
+    <button type="button" class="btn btn-primary">${thumbnail}</button>
     `;
+  }
+
+  async getFirstSpelling() {
+    const userInfo = await getUserInfo();
+    return userInfo.name[0];
+  }
+
+  async mount() {
+    this.render();
+  }
+
+  async render() {
+    const thumbnail = await this.getFirstSpelling();
+    this.innerHTML = this.template(thumbnail);
   }
 }
 export default LoginInfo;
