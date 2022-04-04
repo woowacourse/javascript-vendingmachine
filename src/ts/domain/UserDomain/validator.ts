cb5c9786ff4fdc74d955c4652b6d92c00892d017
이아현 <lah1203@naver.com>
2022-04-04 17:19:43 +0900

    refactor: user info validator 분리

--- src/ts/domain/UserDomain/validator.ts
+++ src/ts/domain/UserDomain/validator.ts
@@ -2,23 +2,60 @@ import { USER_INFO_RULE } from '../../constants';
 import { MESSAGE } from '../../constants/message';
 import { UserInfo } from '../types';
 
-const validateUserInfo = (user: UserInfo, confirmPassword: string) => {
+const isEmptyValue = (user: UserInfo, confirmPassword: string) => {
   const { email, name, password } = user;
-  const { MIN_NAME_LENGTH, MAX_NAME_LENGTH, MIN_PASSWORD_LENGTH } =
-    USER_INFO_RULE;
+  return !email || !name || !password || !confirmPassword;
+};
 
-  if (!email || !name || !password || !confirmPassword) {
-    throw new Error(MESSAGE.ERROR_USER_INFO_EMPTY);
-  }
-  if (name.length < MIN_NAME_LENGTH || name.length > MAX_NAME_LENGTH) {
-    throw new Error(MESSAGE.ERROR_USER_NAME_LENGTH);
-  }
-  if (password.length < MIN_PASSWORD_LENGTH) {
-    throw new Error(MESSAGE.ERROR_USER_PASSWORD_LENGTH);
-  }
-  if (password !== confirmPassword) {
-    throw new Error(MESSAGE.ERROR_USER_CONFIRM_PASSWORD);
-  }
+const isNameLengthRanged = (name: string) => {
+  const { MIN_NAME_LENGTH, MAX_NAME_LENGTH } = USER_INFO_RULE;
+  return name.length < MIN_NAME_LENGTH || name.length > MAX_NAME_LENGTH;
+};
+
+const isPasswordLengthRanged = (password: string) => {
+  return password.length < USER_INFO_RULE.MIN_PASSWORD_LENGTH;
+};
+
+const isDifferentPassword = (password: string, confirmPassword: string) => {
+  return password !== confirmPassword;
+};
+
+interface Validator {
+  test: boolean;
+  errorMsg: string;
+}
+
+const generateUserInfoValidators = (
+  user: UserInfo,
+  confirmPassword: string,
+): Validator[] => [
+  {
+    test: isEmptyValue(user, confirmPassword),
+    errorMsg: MESSAGE.ERROR_USER_INFO_EMPTY,
+  },
+  {
+    test: isNameLengthRanged(user.name),
+    errorMsg: MESSAGE.ERROR_USER_NAME_LENGTH,
+  },
+  {
+    test: isPasswordLengthRanged(user.password),
+    errorMsg: MESSAGE.ERROR_USER_PASSWORD_LENGTH,
+  },
+  {
+    test: isDifferentPassword(user.password, confirmPassword),
+    errorMsg: MESSAGE.ERROR_USER_CONFIRM_PASSWORD,
+  },
+];
+
+const validateUserInfo = (user: UserInfo, confirmPassword: string) => {
+  const validator = generateUserInfoValidators(user, confirmPassword);
+
+  return validator.every(({ test, errorMsg }) => {
+    if (test) {
+      throw new Error(errorMsg);
+    }
+    return true;
+  });
 };
 
 export { validateUserInfo };
