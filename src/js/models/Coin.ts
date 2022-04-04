a4d123966e5bc27c414ede4d4cfb4c2b53c851fc
Ahn <jin7969@gmail.com>
2022-04-04 17:07:56 +0900

    refactor: 범용적으로 쓰이는 type을 파일로 분리

--- src/js/models/Coin.ts
+++ src/js/models/Coin.ts
@@ -1,13 +1,7 @@
 import { COIN } from '../constants/constants.js';
 import { getRandomNumber } from '../utils/common.js';
 import { validChargeAmount } from '../utils/validation.js';
-
-interface Coins {
-  500: number;
-  100: number;
-  50: number;
-  10: number;
-}
+import { Coins } from './types';
 
 interface CoinInterface {
   setAmount: (chargedAmount: number) => void;
