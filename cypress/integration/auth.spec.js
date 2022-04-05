91d2a91b17f77fef5b707309935eff828b1f8ed4
2019015414 <2019015414@hanyang.ac.kr>
2022-04-05 23:17:18 +0900

    test: sessionStorage에 따른 test 수정

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -3,21 +3,20 @@ describe('관리자 인증 테스트', () => {
     cy.visit('dist/index.html');
   });
 
-  it('회원가입을 하고 로그인을 할 수 있어야 한다.', () => {
-    const email = 'test1@test1';
-    const name = 'test';
-    const password = 'Password1234';
-    cy.signupAndLogin(email, name, password);
-
-    cy.get('nav').should('be.visible');
+  after(() => {
+    cy.logout();
   });
 
-  it('로그인 상태에서 로그아웃 버튼을 눌렀을 때 로그아웃이 되어야한다.', () => {
+  it('로그인했을 때 관리자 화면을 볼 수 있어야 한다.', () => {
     const email = 'test1@test1';
     const password = 'Password1234';
     cy.get('#login-button').click();
     cy.login(email, password);
 
+    cy.get('nav').should('be.visible');
+  });
+
+  it('로그인 상태에서 로그아웃 버튼을 눌렀을 때 로그아웃이 되어야한다.', () => {
     cy.logout();
     cy.get('#login-button').should('be.visible');
   });
