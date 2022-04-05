f9aac8ed48c72a98b9a086f1126d8cd7c323bfa0
kimsanglok <evertree6031@gmail.com>
2022-04-05 15:27:55 +0900

    refactor: 클래스 멤버변수 및 메소드에 # private field 적용

--- src/js/domain/VendingMachine.ts
+++ src/js/domain/VendingMachine.ts
@@ -18,58 +18,58 @@ import {
 } from './validator';
 
 export default class VendingMachine {
-  private _productList: VendingMachineProductDictionary;
-  private _moneyBox: MoneyBox;
+  #productList: VendingMachineProductDictionary;
+  #moneyBox: MoneyBox;
 
   constructor() {
-    this._productList = {};
-    this._moneyBox = new MoneyBox();
+    this.#productList = {};
+    this.#moneyBox = new MoneyBox();
   }
 
   get productList(): VendingMachineProductDictionary {
-    return this._productList;
+    return this.#productList;
   }
 
   get totalChange(): number {
-    return this._moneyBox.totalChange;
+    return this.#moneyBox.totalChange;
   }
 
   get coinStatus(): CoinStatus {
-    return this._moneyBox.coinStatus;
+    return this.#moneyBox.coinStatus;
   }
 
   addChange(money: number): never | Coin[] {
-    this.validateChange(money);
+    this.#validateChange(money);
 
-    this._moneyBox.addChange(money);
+    this.#moneyBox.addChange(money);
 
-    return this._moneyBox.coinStatusList;
+    return this.#moneyBox.coinStatusList;
   }
 
   addProduct(data: ProductData): never | string {
-    this.validateUniqueProductName(data.name);
+    this.#validateUniqueProductName(data.name);
 
-    const newId = generateUniqueId(Object.keys(this._productList));
-    this._productList[newId] = new VendingMachineProduct(data);
+    const newId = generateUniqueId(Object.keys(this.#productList));
+    this.#productList[newId] = new VendingMachineProduct(data);
 
     return newId;
   }
 
   updateProduct(productId: string, data: ProductData): void {
-    this.validateProductIdInList(productId);
-    if (data.name !== this._productList[productId].name) {
-      this.validateUniqueProductName(data.name);
+    this.#validateProductIdInList(productId);
+    if (data.name !== this.#productList[productId].name) {
+      this.#validateUniqueProductName(data.name);
     }
 
-    this._productList[productId].modify(data);
+    this.#productList[productId].modify(data);
   }
 
   removeProduct(productId: string): void {
-    this.validateProductIdInList(productId);
-    delete this._productList[productId];
+    this.#validateProductIdInList(productId);
+    delete this.#productList[productId];
   }
 
-  private validateChange(money: number): never | void {
+  #validateChange(money: number): never | void {
     const changeValidator = [
       { testFunc: isBelowMinCharge, errorMsg: ERROR_MESSAGE.BELOW_MIN_CHANGE },
       { testFunc: inValidUnitChange, errorMsg: ERROR_MESSAGE.INVALID_UNIT_CHANGE },
@@ -82,14 +82,14 @@ export default class VendingMachine {
     validateData({ money, totalChange: this.totalChange }, changeValidator);
   }
 
-  private validateUniqueProductName(name): never | void {
-    if (Object.values(this._productList).some((product) => product.name === name)) {
+  #validateUniqueProductName(name): never | void {
+    if (Object.values(this.#productList).some((product) => product.name === name)) {
       throw new Error(ERROR_MESSAGE.DUPLICATE_PRODUCT_NAME);
     }
   }
 
-  private validateProductIdInList(productId: string): never | void {
-    if (this._productList[productId] === undefined) {
+  #validateProductIdInList(productId: string): never | void {
+    if (this.#productList[productId] === undefined) {
       throw new Error(ERROR_MESSAGE.NOT_FOUND_PRODUCT_ID);
     }
   }
--- src/js/domain/VendingMachineProduct.ts
+++ src/js/domain/VendingMachineProduct.ts
@@ -11,39 +11,39 @@ import {
 import { ERROR_MESSAGE } from '../constants';
 
 export default class VendingMachineProduct {
-  private _name: string;
-  private _price: number;
-  private _stock: number;
+  #name: string;
+  #price: number;
+  #stock: number;
 
   constructor({ name, price, stock }: ProductData) {
-    this.validateProduct({ name, price, stock });
+    this.#validateProduct({ name, price, stock });
 
-    this._name = name;
-    this._price = price;
-    this._stock = stock;
+    this.#name = name;
+    this.#price = price;
+    this.#stock = stock;
   }
 
   get name(): string {
-    return this._name;
+    return this.#name;
   }
 
   get price(): number {
-    return this._price;
+    return this.#price;
   }
 
   get stock(): number {
-    return this._stock;
+    return this.#stock;
   }
 
   modify({ name, price, stock }: ProductData): void {
-    this.validateProduct({ name, price, stock });
+    this.#validateProduct({ name, price, stock });
 
-    this._name = name;
-    this._price = price;
-    this._stock = stock;
+    this.#name = name;
+    this.#price = price;
+    this.#stock = stock;
   }
 
-  private validateProduct(data: ProductData) {
+  #validateProduct(data: ProductData) {
     const productValidator = [
       { testFunc: hasEmptyInput, errorMsg: ERROR_MESSAGE.CONTAIN_EMPTY_FIELD_IN_FORM },
       {
