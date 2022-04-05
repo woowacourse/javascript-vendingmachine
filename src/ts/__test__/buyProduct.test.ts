63f1cb596f99b5bc9139feb492f32942fcdc63a1
sanaandmomo <qajjang46@naver.com>
2022-04-05 17:35:16 +0900

    test: 유저 투입 금액 성공 케이스 추가

--- src/ts/__test__/buyProduct.test.ts
+++ src/ts/__test__/buyProduct.test.ts
@@ -9,14 +9,14 @@ describe('유저 투입 금액 테스트', () => {
     vendingMachine = new VendingMachineImpl();
   });
 
-  it(`유저 투입 금액이 ${USER_INPUT_MONEY_RULES.MOD_UNIT}으로 나누어 떨어지지 않을 때, 동전이 충전되면 안된다.`, () => {
+  it(`유저 투입 금액이 ${USER_INPUT_MONEY_RULES.MOD_UNIT}으로 나누어 떨어지지 않을 때, 금액이 충전되면 안된다.`, () => {
     const userInputMoney = 1555;
     const test = () => vendingMachine.chargeUserMoney(userInputMoney);
 
     expect(test).toThrow(ERROR_MESSAGE.INDIVISIBLE_USER_INPUT_MONEY_MOD_UNIT);
   });
 
-  it(`유저 투입 금액이 ${USER_INPUT_MONEY_RULES.MAX_HAVE.toLocaleString()}원을 넘을 때, 동전이 충전되면 안된다.`, () => {
+  it(`유저 투입 금액이 ${USER_INPUT_MONEY_RULES.MAX_HAVE.toLocaleString()}원을 넘을 때, 금액이 충전되면 안된다.`, () => {
     const userInputMoney = 2000;
     const holdingMoney = 9000;
     const test = () => vendingMachine.chargeUserMoney(userInputMoney);
@@ -24,5 +24,13 @@ describe('유저 투입 금액 테스트', () => {
     vendingMachine.chargeUserMoney(holdingMoney);
 
     expect(test).toThrow(ERROR_MESSAGE.EXCEED_MAX_USER_INPUT_MONEY);
+  });
+
+  it(`유저 투입 금액이 ${USER_INPUT_MONEY_RULES.MAX_HAVE.toLocaleString()}일 때, 금액이 충전된다.`, () => {
+    const userInputMoney = USER_INPUT_MONEY_RULES.MAX_HAVE;
+
+    vendingMachine.chargeUserMoney(userInputMoney);
+
+    expect(vendingMachine.totalUserInputMoney).toBe(userInputMoney);
   });
 });
