e75154d9e3e283ba3f2bf98e1499373180c8369d
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-04-05 20:58:51 +0900

    test: 회원가입 유효성 테스트 케이스 추가
    공백일 때 확인

--- src/__test__/registerValidation.test.ts
+++ src/__test__/registerValidation.test.ts
@@ -5,6 +5,18 @@ import RegisterUser from '../ts/vendingMachine/RegisterUser';
 describe('회원가입 입력값 유효성 테스트', () => {
   const registerUser = new RegisterUser();
 
+  test('어떤 입력값이라도 공백이면 에러가 발생한다.', () => {
+    const userInfo: UserInfoType = {
+      email: 'a@woowahan.com',
+      name: '',
+      password: '1234!@#$abcd',
+      confirmPassword: '1234!@#$abcd',
+    };
+    expect(() => registerUser.validateRegisterBehavior(userInfo)).toThrow(
+      REGISTER_ERROR_MESSAGE.BLANK_NOT_ALLOWED
+    );
+  });
+
   test('이름이 2 ~ 6 글자 사이가 아니면 에러가 발생한다.', () => {
     const userInfo: UserInfoType = {
       email: 'a@woowahan.com',
