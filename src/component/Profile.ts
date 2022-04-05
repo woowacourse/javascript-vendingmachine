e0dbb10a10ece3f392a1fdb2acdc7145ae251a3c
sangminsong <ssmin940606@gmail.com>
2022-04-06 00:28:03 +0900

    feat: 프로필 썸네일 UI 구현 및 도메인과 연동

---
+++ src/component/Profile.ts
@@ -0,0 +1,96 @@
+import { Auth } from '../domain/Auth';
+import { ProductProps } from '../utils/interface';
+
+interface ProfileInterface {
+  getIsRendered();
+  render();
+}
+
+export class Profile implements ProfileInterface {
+  #isRendered: boolean;
+  #target: HTMLDivElement;
+  #selectContainer: HTMLDivElement;
+  #editUserInfoBtn: HTMLButtonElement;
+  #logoutBtn: HTMLButtonElement;
+  #thumbnailName: HTMLSpanElement;
+  #auth: Auth;
+
+  constructor({ target, auth }) {
+    this.#target = target;
+    this.#auth = auth;
+    this.#isRendered = false;
+  }
+
+  getIsRendered() {
+    return this.#isRendered;
+  }
+
+  #setIsRendered(status: boolean) {
+    this.#isRendered = status;
+  }
+
+  #template(name: ProductProps['name']) {
+    return `
+      <span class="name">${name.slice(0, 1)}</span>
+      <div class="select-container hide">
+        <button type="button" class="edit-user-info button">회원정보 수정</button>
+        <button type="button" class="logout button">로그아웃</button>
+      </div>
+    `;
+  }
+
+  render() {
+    if (this.#isRendered) {
+      this.#updateThumbnail();
+
+      return;
+    }
+
+    const { name } = JSON.parse(localStorage.getItem('user'));
+    console.log(name);
+
+    this.#target.insertAdjacentHTML('beforeend', this.#template(name));
+
+    this.#setIsRendered(true);
+
+    this.#selectDOM();
+    this.#bindEvent();
+  }
+
+  #updateThumbnail() {
+    const { name } = JSON.parse(localStorage.getItem('user'));
+
+    this.#thumbnailName.textContent = `${name.slice(0, 1)}`;
+  }
+
+  #selectDOM() {
+    this.#thumbnailName = document.querySelector('.name');
+    this.#selectContainer = document.querySelector('.select-container');
+    this.#editUserInfoBtn = document.querySelector('.edit-user-info');
+    this.#logoutBtn = document.querySelector('.logout');
+  }
+
+  #bindEvent() {
+    this.#thumbnailName.addEventListener('click', this.#handleToggleSelectContainer);
+    this.#editUserInfoBtn.addEventListener('click', this.#handleRequestEditUserInfoPage);
+    this.#logoutBtn.addEventListener('click', this.#handleLogout);
+  }
+
+  #handleToggleSelectContainer = () => {
+    this.#selectContainer.classList.toggle('hide');
+  };
+
+  #handleRequestEditUserInfoPage = () => {
+    this.#target.dispatchEvent(new CustomEvent('showEditUserInfoRequested'));
+
+    this.#selectContainer.classList.toggle('hide');
+  };
+
+  #handleLogout = () => {
+    this.#auth.logout();
+
+    this.#target.dispatchEvent(new CustomEvent('logoutCompleted'));
+
+    this.#selectContainer.classList.toggle('hide');
+  };
+}
