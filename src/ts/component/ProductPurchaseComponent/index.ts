67c3e575255ee2ce8e2d5f41b99f25b0f3b05c4b
gytks4 <gytks4@naver.com>
2022-04-06 00:09:10 +0900

    chore: 상품 구매 label 수정

--- src/ts/component/ProductPurchaseComponent/index.ts
+++ src/ts/component/ProductPurchaseComponent/index.ts
@@ -30,13 +30,13 @@ export default class ProductPurchaseComponent {
     return `
       <div>
         <section class="money-charge input-section">
-          <h2 hidden>충전할 금액 입력</h2>
+          <h2 hidden>투입할 금액 입력</h2>
           <form class="money-charge__form">
             <label
               class="money-charge__description input-description"
               for="cash-amount"
             >
-              자판기가 보유할 금액을 입력해주세요.
+              상품을 구매할 금액을 투입해주세요
             </label>
             <input
               type="number"
@@ -45,9 +45,9 @@ export default class ProductPurchaseComponent {
               placeholder="금액"
               name="moneyInput"
             />
-            <button class="money-charge__button submit-button">충전</button>
+            <button class="money-charge__button submit-button">투입</button>
           </form>
-          <p class="money-charge__total-cash-description">현재 보유 금액: <span class="money-charge__total-money">${
+          <p class="money-charge__total-cash-description">투입한 금액: <span class="money-charge__total-money">${
             this.#moneyManagement.money
           }</span>원</p>
         </section>
