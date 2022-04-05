286100fd5cb8e3d00cb588cac4ab1f1196998f4a
woose28 <dldntmdsla@naver.com>
2022-04-06 00:13:54 +0900

    refactor: 인증 입력 값 유효성 검사 테스트  수정

--- src/__test__/authentication.test.ts
+++ src/__test__/authentication.test.ts
@@ -1,139 +1,142 @@
-const isNotEmailFormat = (email: string): boolean | never => {
-  const emailRegex = /[0-9a-zA-Z]+@([0-9a-zA-Z]+)(.[0-9a-zA-Z]+){1,2}$/;
-
-  if (emailRegex.test(email)) {
-    return true;
-  }
-
-  throw new Error('이메일 형식에 맞지 않습니다.');
-};
-
-const isValidNameRange = (name: string): boolean | never => {
-  if (name.length >= 2 && name.length <= 6) {
-    return true;
-  }
-
-  throw new Error('이름은 2글자 이상 6글자 이하로 작성해 주세요.');
-};
-
-const isValidPasswordRange = (password: string): boolean | never => {
-  if (password.length >= 8 && password.length <= 16) {
-    return true;
-  }
-
-  throw new Error('비밀번호는 8글자 이상 16글자 이하로 작성해 주세요.');
-};
-
-const isValidPasswordFormat = (password: string): boolean | never => {
-  const numberRegex = /[0-9]/;
-  const alphabetRegex = /[a-zA-Z]/;
-  const specialCharactersRegex = /[!#$%&()*+,-./:;<=>?@]/;
-  const otherCharacterRegex = /[^0-9a-zA-Z!#$%&()*+,-./:;<=>?@]/;
-
-  if (
-    numberRegex.test(password) &&
-    alphabetRegex.test(password) &&
-    specialCharactersRegex.test(password) &&
-    !otherCharacterRegex.test(password)
-  ) {
-    return true;
-  }
-
-  throw new Error(
-    `비밀번호 형식에 맞지 않습니다.\n
-    비밀번호는 비밀번호는 영문, 숫자, 특수문자(! # $ % & ( ) * + , - . / : ; < = > ? @)의 조합으로 작성해주세요`
-  );
-};
-
-const isSameVerificationPassword = (
-  password: string,
-  verificationPassword: string
-): boolean | never => {
-  if (password === verificationPassword) {
-    return true;
-  }
-
-  throw new Error(
-    '비밀번호와 비밀번호 확인이 일치하지 않습니다. 비밀번호 확인을 다시 입력해주세요.'
-  );
-};
+import UserStore from '../ts/domain/UserStore';
+import { AUTHENTICATION_MESSAGE } from '../ts/constant/errorMessage';
 
 describe('인증 입력 값 유효성 검사', () => {
+  const userStore = new UserStore();
+
+  const validEmail = 'woowa@test.email.com';
+  const validName = 'woowa';
+  const validPassword = 'abcd1234!';
+  const validVerificationPassword = 'abcd1234!';
+
   test('입력한 이메일이 이메일 형식에 맞지 않으면 에러가 발생한다.', () => {
     const email = 'woowa!test.email.com';
 
-    expect(() => isNotEmailFormat(email)).toThrow('이메일 형식에 맞지 않습니다.');
+    expect(() =>
+      userStore.validateRegisterInput({
+        email,
+        name: validName,
+        password: validPassword,
+        verificationPassword: validVerificationPassword,
+      })
+    ).toThrow(AUTHENTICATION_MESSAGE.NOT_EMAIL_FORMAT);
   });
 
   test('입력한 이름의 길이가 2자보다 적으면 에러가 발생한다.', () => {
     const name = 'a';
 
-    expect(() => isValidNameRange(name)).toThrow('이름은 2글자 이상 6글자 이하로 작성해 주세요.');
+    expect(() =>
+      userStore.validateRegisterInput({
+        email: validEmail,
+        name: name,
+        password: validPassword,
+        verificationPassword: validVerificationPassword,
+      })
+    ).toThrow(AUTHENTICATION_MESSAGE.EXCEED_NAME_LENGTH_RANGE);
   });
 
   test('입력한 이름의 길이가 6자보다 많으면 에러가 발생한다.', () => {
-    const name = 'a';
+    const name = 'abcdefg';
 
-    expect(() => isValidNameRange(name)).toThrow('이름은 2글자 이상 6글자 이하로 작성해 주세요.');
+    expect(() =>
+      userStore.validateRegisterInput({
+        email: validEmail,
+        name: name,
+        password: validPassword,
+        verificationPassword: validVerificationPassword,
+      })
+    ).toThrow(AUTHENTICATION_MESSAGE.EXCEED_NAME_LENGTH_RANGE);
   });
 
-  test('입력한 비밀번호의 길이가 8자보다 많으면 에러가 발생한다.', () => {
+  test('입력한 비밀번호의 길이가 8자보다 적으면 에러가 발생한다.', () => {
     const password = 'a!12345';
 
-    expect(() => isValidPasswordRange(password)).toThrow(
-      '비밀번호는 8글자 이상 16글자 이하로 작성해 주세요.'
-    );
+    expect(() =>
+      userStore.validateRegisterInput({
+        email: validEmail,
+        name: validName,
+        password: password,
+        verificationPassword: password,
+      })
+    ).toThrow(AUTHENTICATION_MESSAGE.EXCEED_PASSWORD_LENGTH_RANGE);
   });
 
-  test('입력한 비밀번호의 길이가 16자보다 적으면 에러가 발생한다.', () => {
+  test('입력한 비밀번호의 길이가 16자보다 많으면 에러가 발생한다.', () => {
     const password = 'a!123456789123456';
 
-    expect(() => isValidPasswordRange(password)).toThrow(
-      '비밀번호는 8글자 이상 16글자 이하로 작성해 주세요.'
-    );
+    expect(() =>
+      userStore.validateRegisterInput({
+        email: validEmail,
+        name: validName,
+        password: password,
+        verificationPassword: password,
+      })
+    ).toThrow(AUTHENTICATION_MESSAGE.EXCEED_PASSWORD_LENGTH_RANGE);
   });
 
   test('입력한 비밀번호에 영어가 들어가지 않으면 에러가 발생한다.', () => {
-    const password = '123456!';
+    const password = '1234567!';
 
-    expect(() => isValidPasswordFormat(password)).toThrow(`비밀번호 형식에 맞지 않습니다.\n
-    비밀번호는 비밀번호는 영문, 숫자, 특수문자(! # $ % & ( ) * + , - . / : ; < = > ? @)의 조합으로 작성해주세요`);
+    expect(() =>
+      userStore.validateRegisterInput({
+        email: validEmail,
+        name: validName,
+        password: password,
+        verificationPassword: password,
+      })
+    ).toThrow(AUTHENTICATION_MESSAGE.NOT_PASSWORD_FORMAT);
   });
 
   test('입력한 비밀번호에 숫자가 들어가지 않으면 에러가 발생한다.', () => {
-    const password = 'abcdef!';
+    const password = 'abcdefg!';
 
-    expect(() => isValidPasswordFormat(password)).toThrow(`비밀번호 형식에 맞지 않습니다.\n
-    비밀번호는 비밀번호는 영문, 숫자, 특수문자(! # $ % & ( ) * + , - . / : ; < = > ? @)의 조합으로 작성해주세요`);
+    expect(() =>
+      userStore.validateRegisterInput({
+        email: validEmail,
+        name: validName,
+        password: password,
+        verificationPassword: password,
+      })
+    ).toThrow(AUTHENTICATION_MESSAGE.NOT_PASSWORD_FORMAT);
   });
 
   test('입력한 비밀번호에 특수문자(! # $ % & ( ) * + , - . / : ; < = > ? @)가 들어가지 않으면 에러가 발생한다.', () => {
-    const password = 'abc123';
+    const password = 'abcd1234';
 
-    expect(() => isValidPasswordFormat(password)).toThrow(`비밀번호 형식에 맞지 않습니다.\n
-    비밀번호는 비밀번호는 영문, 숫자, 특수문자(! # $ % & ( ) * + , - . / : ; < = > ? @)의 조합으로 작성해주세요`);
+    expect(() =>
+      userStore.validateRegisterInput({
+        email: validEmail,
+        name: validName,
+        password: password,
+        verificationPassword: password,
+      })
+    ).toThrow(AUTHENTICATION_MESSAGE.NOT_PASSWORD_FORMAT);
   });
 
   test('입력한 비밀번호에 영어, 숫자, 특수문자(! # $ % & ( ) * + , - . / : ; < = > ? @) 이외의 문자가 들어가 있으면 에러가 발생한다.', () => {
     const password = 'abc123!!~';
 
-    expect(() => isValidPasswordFormat(password)).toThrow(`비밀번호 형식에 맞지 않습니다.\n
-    비밀번호는 비밀번호는 영문, 숫자, 특수문자(! # $ % & ( ) * + , - . / : ; < = > ? @)의 조합으로 작성해주세요`);
-  });
-
-  test('입력한 비밀번호에 영어, 숫자, 특수문자가 모두 있으면 에러가 발생하지 않는다.', () => {
-    const password = 'abc123!';
-
-    expect(() => isValidPasswordFormat(password)).not.toThrow(`비밀번호 형식에 맞지 않습니다.\n
-    비밀번호는 비밀번호는 영문, 숫자, 특수문자(! # $ % & ( ) * + , - . / : ; < = > ? @)의 조합으로 작성해주세요`);
+    expect(() =>
+      userStore.validateRegisterInput({
+        email: validEmail,
+        name: validName,
+        password: password,
+        verificationPassword: password,
+      })
+    ).toThrow(AUTHENTICATION_MESSAGE.NOT_PASSWORD_FORMAT);
   });
 
   test('처음 입력한 비밀번호와 재입력한 비밀번호가 일치하지 않으면 에러가 발생한다.', () => {
-    const password = 'a!123456789123456';
-    const verificationPassword = 'a!123456789123455';
+    const password = 'a!123456';
+    const verificationPassword = 'a!123457';
 
-    expect(() => isSameVerificationPassword(password, verificationPassword)).toThrow(
-      '비밀번호와 비밀번호 확인이 일치하지 않습니다. 비밀번호 확인을 다시 입력해주세요.'
-    );
+    expect(() =>
+      userStore.validateRegisterInput({
+        email: validEmail,
+        name: validName,
+        password: password,
+        verificationPassword: verificationPassword,
+      })
+    ).toThrow(AUTHENTICATION_MESSAGE.DIFFERENT_VERIFICATION_PASSWORD);
   });
 });
