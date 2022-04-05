223bb5cee888d6cd899d131ae0dbaf8b5cb441cc
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-04-05 18:04:17 +0900

    ✨ : 템플릿 처리를 위한 유틸리티 추가 - convertStringToElement : HTML 문자열을 엘리먼트로 변환합니다. - setContentOfChildElement : 엘리먼트의 자식 요소의 텍스트를 한번에 변경하여 줍니다. - setElementProperty : 엘리먼트의 속성을 한번에 변경하여 줍니다. - createTemplate : 상위 3 유틸을 한번에 실행시켜줍니다.

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -1,5 +1,7 @@
-export const $ = (selector, node = document) => node.querySelector(selector);
-export const $$ = (selector, node = document) => node.querySelectorAll(selector);
+export const $ = (selector, node: HTMLElement | DocumentFragment = document.body) =>
+  node.querySelector(selector);
+export const $$ = (selector, node: HTMLElement | DocumentFragment = document.body) =>
+  node.querySelectorAll(selector);
 
 export const getRandomNumber = (min: number, max: number): number =>
   Math.floor(Math.random() * (max - min + 1) + min);
@@ -16,3 +18,59 @@ export const getSearchParamsParse = (searchUrl = ''): string => `?${searchUrl.sp
 
 export const getSearchParamsObject = (searchUrl = ''): object =>
   Object.fromEntries(new URLSearchParams(searchUrl));
+
+export const convertStringToElement = (htmlString = ''): HTMLElement => {
+  const element: HTMLElement = document.createElement('template');
+  element.insertAdjacentHTML('afterbegin', htmlString);
+
+  return <HTMLElement>element.children[0];
+};
+
+export const setContentOfChildElement = (
+  element: HTMLElement,
+  target: TemplateSetting['childTextContent'],
+): void => {
+  Object.entries(target).forEach(([selector, innerText]) => {
+    const $target: HTMLInputElement = element.querySelector(selector);
+    $target.value !== undefined
+      ? ($target.value = <string>innerText)
+      : ($target.textContent = <string>innerText);
+  });
+};
+
+export const setElementProperty = (
+  element: HTMLElement,
+  property: TemplateSetting['elementProperty'],
+): void => {
+  Object.entries(property).forEach(([key, value]) => {
+    if (key === 'dataset') {
+      Object.entries(value).forEach(([datasetId, datasetValue]) =>
+        element.setAttribute(`data-${datasetId}`, datasetValue),
+      );
+      return;
+    }
+
+    switch (typeof element[key]) {
+      case 'string':
+        element[key] = value;
+        break;
+      case 'function':
+        element[key](...(<[]>value));
+        break;
+
+      // no default
+    }
+  });
+};
+
+export const createTemplate = (
+  template: HTMLElement | string,
+  setting: TemplateSetting,
+): HTMLElement => {
+  const element = typeof template === 'string' ? convertStringToElement(template) : template;
+
+  setContentOfChildElement(element, setting.childTextContent);
+  setElementProperty(element, setting.elementProperty);
+
+  return element;
+};
