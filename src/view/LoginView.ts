150399575051ad29bd13afbaf85f38da0ae6a27d
sangminsong <ssmin940606@gmail.com>
2022-04-06 00:29:06 +0900

    feat: 로그인 UI 구현 및 도메인 연동

--- src/view/LoginView.ts
+++ src/view/LoginView.ts
@@ -1,12 +1,18 @@
+import { Auth } from '../domain/Auth';
+
 interface LoginViewInterface {
   render();
 }
 
 export class LoginView implements LoginViewInterface {
   #target: HTMLDivElement;
+  #loginForm: HTMLFormElement;
+  #signupLink: HTMLSpanElement;
+  #auth: Auth;
 
-  constructor(target) {
+  constructor({ target, auth }) {
     this.#target = target;
+    this.#auth = auth;
   }
 
   #template() {
@@ -14,15 +20,55 @@ export class LoginView implements LoginViewInterface {
       <h1>로그인</h1>
       <form id="login-form" class="auth-form">
         <label for="email">이메일</label>
-        <input id="email" class="input" type="email" placeholder="wooteco@gmail.com" />
+        <input id="email" class="input" name="email" type="email" placeholder="wooteco@gmail.com" />
         <label for="password">비밀번호</label>
-        <input id="password" class="input" type="password" placeholder="비밀번호를 입력해주세요" />
+        <input id="password" class="input" name="password" type="password" placeholder="비밀번호를 입력해주세요" />
         <button class="submit-button button">확인</button>
       </form>
+      <p>아직 회원이 아니신가요? <span class="signup-link">회원가입</spanc></p>
     `;
   }
 
   render() {
     this.#target.insertAdjacentHTML('beforeend', this.#template());
+    this.#selectDOM();
+    this.#bindEvent();
+  }
+
+  #selectDOM() {
+    this.#loginForm = document.querySelector('#login-form');
+    this.#signupLink = document.querySelector('.signup-link');
+  }
+
+  #bindEvent() {
+    this.#loginForm.addEventListener('submit', this.#handleLogin);
+    this.#signupLink.addEventListener('click', this.#handleRequestSignupPage);
+  }
+
+  #handleRequestSignupPage = () => {
+    this.#target.dispatchEvent(new CustomEvent('signupPageRequested'));
+  };
+
+  #handleLogin = async (e: SubmitEvent) => {
+    e.preventDefault();
+
+    const email = e.target['email'].value;
+    const password = e.target['password'].value;
+
+    try {
+      const userInfo = await this.#auth.login({ email, password });
+      this.#setUserInfo(userInfo);
+
+      this.#target.dispatchEvent(new CustomEvent('loginCompleted'));
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  #setUserInfo(userInfo) {
+    const { accessToken, user } = userInfo;
+
+    localStorage.setItem('accessToken', JSON.stringify(accessToken));
+    localStorage.setItem('user', JSON.stringify(user));
   }
 }
