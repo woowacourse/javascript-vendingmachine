05ff08ed36e92f4f6d66642d822e919b26f6250e
kkojae <jaejeung3210@gmail.com>
2022-04-06 22:48:58 +0900

    api 요청 url 배포된 주소로 변경

--- src/ts/api/api.ts
+++ src/ts/api/api.ts
@@ -8,56 +8,68 @@ const request = async (url, option) => {
 };
 
 export const requestRegister = async (email, name, password) => {
-  const response = await request('http://localhost:3000/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      email,
-      name,
-      password,
-    }),
-  });
+  const response = await request(
+    'https://vendingmachine-json-server.herokuapp.com/register',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        name,
+        password,
+      }),
+    }
+  );
 
   return response;
 };
 
 export const requestSignIn = async (email, password) => {
-  const response = await request('http://localhost:3000/signin', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      email,
-      password,
-    }),
-  });
+  const response = await request(
+    'https://vendingmachine-json-server.herokuapp.com/signin',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    }
+  );
 
   return response;
 };
 
 export const requestUserInfo = async (accessToken, userId) => {
-  const response = await request(`http://localhost:3000/600/users/${userId}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+  const response = await request(
+    `https://vendingmachine-json-server.herokuapp.com/600/users/${userId}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
 
   return response;
 };
 
 export const requestEditInformation = async (accessToken, user) => {
-  const response = await request(`http://localhost:3000/users/${user.id}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${accessToken}`,
-    },
-    body: JSON.stringify(user),
-  });
+  const response = await request(
+    `https://vendingmachine-json-server.herokuapp.com/users/${user.id}`,
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(user),
+    }
+  );
 
   return response;
 };
