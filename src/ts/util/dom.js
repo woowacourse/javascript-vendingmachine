d18d1e9380f2be687f548ee68f4f7f9066e1204a
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-04-07 07:18:17 +0900

    chore: 잘못 pull 된 파일 삭제

--- src/ts/util/dom.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.$ = void 0;
-var $ = function (selector, parentNode) {
-    if (parentNode === void 0) { parentNode = document; }
-    return parentNode.querySelector(selector);
-};
-exports.$ = $;
--- src/ts/util/dom.ts
+++
@@ -1,2 +0,0 @@
-export const $ = (selector, parentNode = document) =>
-  parentNode.querySelector(selector);
