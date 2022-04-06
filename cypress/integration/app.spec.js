8a8163fc369ed0f693657a1b6757302dba2c9688
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-04-07 04:04:37 +0900

    test: 관리자 기능 e2e 테스트 작성

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,4 +1,7 @@
 import { ALERT_MESSAGE } from '../../src/js/constants';
+const userEmail = `${Date.now()}@test.com`;
+const userName = 'test';
+const userPassword = '1q2w3e!!';
 
 describe('기본 시나리오 동작 확인', () => {
   before(() => {
@@ -6,10 +9,6 @@ describe('기본 시나리오 동작 확인', () => {
   });
 
   describe('로그인 / 회원가입 테스트', () => {
-    const userEmail = `${Date.now()}@test.com`;
-    const userName = 'test';
-    const userPassword = '1q2w3e!!';
-
     it('로그인 버튼을 누르면 로그인 화면으로 이동해야한다.', () => {
       cy.get('#login-page-button')
         .click()
@@ -47,6 +46,69 @@ describe('기본 시나리오 동작 확인', () => {
         .click()
         .then(() => {
           cy.get('.snackbar').should('contain', ALERT_MESSAGE.LOGIN_SUCCESS(userName));
+        });
+    });
+  });
+
+  describe('관리자 기능 테스트', () => {
+    const productName = '사이다';
+    const productPrice = '1000';
+    const productAmount = '10';
+    const productModifyAmount = '20';
+    const addChangeMoney = 10000;
+
+    it('관리자는 자판기에 상품을 추가할 수 있어야 한다.', () => {
+      cy.get('#product-name-input').type(productName);
+      cy.get('#product-price-input').type(productPrice);
+      cy.get('#product-amount-input').type(productAmount);
+
+      cy.get('#product-add-button')
+        .click()
+        .then(() => {
+          cy.get('.snackbar').should('contain', ALERT_MESSAGE.ADD_PRODUCT_SUCCESS(productName));
+        });
+    });
+
+    it('관리자는 자판기의 상품을 수정할 수 있어야 한다.', () => {
+      cy.get('.product-modify-button').click();
+      cy.get('.product-amount-modify-input').clear().type(productModifyAmount);
+      cy.get('.product-modify-submit-button')
+        .click()
+        .then(() => {
+          cy.get('.snackbar').should('contain', ALERT_MESSAGE.MODIFY_PRODUCT_SUCCESS(productName));
+        });
+    });
+
+    it('관리자는 자판기의 상품을 삭제할 수 있어야 한다.', () => {
+      cy.get('.product-remove-button')
+        .click()
+        .then(() => {
+          cy.on('window:confirm', text => {
+            expect(text).to.contains(REMOVE_CONFIRM_MESSAGE);
+            return true;
+          });
+          cy.get('.snackbar').should('contain', ALERT_MESSAGE.DELETE_PRODUCT_SUCCESS(productName));
+        });
+    });
+
+    it('관리자는 자판기의 잔돈을 채울 수 있어야 한다.', () => {
+      cy.get('nav > #change-add-button').click();
+      cy.get('#change-add-input').type(addChangeMoney);
+      cy.get('#change-add-form > #change-add-button')
+        .click()
+        .then(() => {
+          cy.get('.snackbar').should('contain', ALERT_MESSAGE.ADD_CHARGE_SUCCESS(addChangeMoney));
+        });
+    });
+
+    it('관리자는 로그아웃 버튼을 눌러 로그아웃 할 수 있어야 한다.', () => {
+      cy.login(userEmail, userPassword);
+
+      cy.get('.user-menu-symbol').click();
+      cy.get('#logout')
+        .click()
+        .then(() => {
+          cy.get('.snackbar').should('contain', ALERT_MESSAGE.LOGOUT_SUCCESS);
         });
     });
   });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (userEmail, userPassword) => {
+  cy.get('#login-page-button').click();
+  cy.get('#email-input').type(userEmail);
+  cy.get('#password-input').type(userPassword);
+  cy.get('#login-button').click();
+});
