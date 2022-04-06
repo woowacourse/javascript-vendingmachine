a19fcb7fb5f4f000bbaea2e3a1c0cb3d1e3455d6
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-04-06 23:26:21 +0900

    refactor: 스낵바 애니메이션 수정
    setTimeout -> requestAnimationFrame

--- src/ts/utils/index.js
+++ src/ts/utils/index.js
@@ -1,11 +1,22 @@
+import { SNACKBAR_MS_TIME } from '../constant/rule';
+import { SELECTOR_NAME } from '../constant/selector';
+
 export const selectDom = (selector, parent = document) => parent.querySelector(selector);
 
 export const selectDoms = (selector, parent = document) => parent.querySelectorAll(selector);
 
 export const showSnackbar = (snackbarElement, messsage) => {
-  snackbarElement.classList.add('show');
+  snackbarElement.classList.add(SELECTOR_NAME.SHOW);
   snackbarElement.textContent = messsage;
-  setTimeout(() => {
-    snackbarElement.classList.remove('show');
-  }, 2700);
+
+  const snackbarStartTime = new Date().getTime();
+  const snackbarAnimation = () => {
+    const snackbarCurrentTime = new Date().getTime();
+    if (snackbarCurrentTime < snackbarStartTime + SNACKBAR_MS_TIME) {
+      requestAnimationFrame(snackbarAnimation);
+      return;
+    }
+    snackbarElement.classList.remove(SELECTOR_NAME.SHOW);
+  };
+  requestAnimationFrame(snackbarAnimation);
 };
