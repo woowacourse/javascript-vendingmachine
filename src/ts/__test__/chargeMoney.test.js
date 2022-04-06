8a062d905b87af103b87e46f44e4692c2feae094
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-04-07 03:50:48 +0900

    test: 자판기 금액 투입 유닛 테스트 수정

--- src/ts/__test__/chargeMoney.test.js
+++ src/ts/__test__/chargeMoney.test.js
@@ -1,47 +1,45 @@
-import ChargeMoneyImpl from '../core/ChargeMoneyImpl';
-import { isValidMoney, totalAmount } from '../validation/isValidMoney';
-
-jest.spyOn(window, 'alert').mockImplementation(() => {});
+import ChargeMoneyTab from '../core/ChargeMoneyTab';
+import VerifyValueValidation from '../validations/verifyValueValidation';
+import { generateRandomCoins } from '../utils/productUtil';
 
 describe('chargeMoney', () => {
-  let chargeMoney;
-
-  beforeEach(
-    () =>
-      (chargeMoney = new ChargeMoneyImpl([
-        { amount: 10, count: 0 },
-        { amount: 50, count: 0 },
-        { amount: 100, count: 0 },
-        { amount: 500, count: 0 },
-      ])),
-  );
+  const coins = [
+    { amount: 10, count: 0 },
+    { amount: 50, count: 0 },
+    { amount: 100, count: 0 },
+    { amount: 500, count: 0 },
+  ];
+  const verifyValue = new VerifyValueValidation([], coins);
+  const chargeMoney = new ChargeMoneyTab(coins);
 
   test('투입 금액이 10으로 나누어 떨어지지 않을 때, 동전이 충전되지 않는지 확인', () => {
     const inputMoney = 1555;
-
-    expect(isValidMoney(inputMoney, chargeMoney.coins)).toBe(false);
+    expect(verifyValue.isValidChargeMoney(inputMoney)).toBe(false);
   });
 
   test('투입 금액이 1000원보다 작을 때, 동전이 충전되지 않는지 확인', () => {
     const inputMoney = 900;
-
-    expect(isValidMoney(inputMoney, chargeMoney.coins)).toBe(false);
+    expect(verifyValue.isValidChargeMoney(inputMoney)).toBe(false);
   });
 
   test('보유 금액과 투입 금액의 합이 100,000을 넘을 때, 동전이 충전되지 않는지 확인', () => {
     const inputMoney = 11000;
     const holdingMoney = 90000;
 
-    chargeMoney.chargeMoney(chargeMoney.generateRandomCoins(holdingMoney));
-
-    expect(isValidMoney(inputMoney, chargeMoney.coins)).toBe(false);
+    chargeMoney.chargeMoney(generateRandomCoins.call(chargeMoney, holdingMoney));
+    expect(verifyValue.isValidChargeMoneyOver(inputMoney)).toBe(false);
   });
 
   test('투입 금액만큼 동전이 만들어지는지 확인', () => {
+    chargeMoney.coins = [
+      { amount: 10, count: 0 },
+      { amount: 50, count: 0 },
+      { amount: 100, count: 0 },
+      { amount: 500, count: 0 },
+    ];
     const inputMoney = 89500;
 
-    chargeMoney.chargeMoney(chargeMoney.generateRandomCoins(inputMoney));
-
-    expect(totalAmount(chargeMoney.coins)).toBe(inputMoney);
+    chargeMoney.chargeMoney(generateRandomCoins.call(chargeMoney, inputMoney));
+    expect(verifyValue.totalAmount.call(chargeMoney, chargeMoney.coins)).toBe(inputMoney);
   });
 });
