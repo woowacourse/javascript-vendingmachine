5e40c3e90e023b797bac741ab495c7d2d7168db4
kimsanglok <evertree6031@gmail.com>
2022-04-06 22:58:24 +0900

    feat: 회원가입 기능 도메인 구현

---
+++ src/js/domain/Auth.ts
@@ -0,0 +1,139 @@
+import { validateData } from './validator';
+
+const emailValidator = [
+  { testFunc: isEmpty, errorMsg: '이메일을 입력해주세요.' },
+  { testFunc: hasBlank, errorMsg: '이메일은 공백을 포함할 수 없습니다.' },
+];
+
+const nameValidator = [
+  { testFunc: isEmpty, errorMsg: '이름을 입력해주세요.' },
+  { testFunc: hasBlank, errorMsg: '이름은 공백을 포함할 수 없습니다.' },
+  {
+    testFunc: isNameLengthOutOfRange,
+    errorMsg: '이름은 2자 미만이거나 6자를 초과할 수 없습니다.',
+  },
+];
+
+const passwordValidator = [
+  { testFunc: isEmpty, errorMsg: '비밀번호를 입력해주세요.' },
+  { testFunc: hasBlank, errorMsg: '비밀번호는 공백을 포함할 수 없습니다.' },
+  {
+    testFunc: isUnderMinLengthPassword,
+    errorMsg: '비밀번호는 10자 미만일 수 없습니다.',
+  },
+  {
+    testFunc: isInValidPassword,
+    errorMsg: '비밀번호는 소문자, 대문자, 숫자, 특수문자 중 2가지를 혼합해야 합니다.',
+  },
+];
+
+const passwordConfirmValidator = [
+  { testFunc: isEmpty, errorMsg: '비밀번호 확인을 입력해주세요.' },
+  { testFunc: hasBlank, errorMsg: '비밀번호 확인은 공백을 포함할 수 없습니다.' },
+];
+
+function validateUserInfo(userInfo) {
+  const { email, name, password, passwordConfirm } = userInfo;
+
+  validateData(email, emailValidator);
+  validateData(name, nameValidator);
+  validateData(password, passwordValidator);
+  validateData(passwordConfirm, passwordConfirmValidator);
+
+  if (isDifferent(password, passwordConfirm)) {
+    throw Error('비밀번호와 비밀번호 확인이 일치해야 합니다.');
+  }
+}
+
+const Auth = {
+  register(userInfo) {
+    validateUserInfo(userInfo);
+
+    const response = this.loadRegisterAPI(userInfo);
+    const {
+      accessToken,
+      user: { userId },
+    } = response;
+
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('userId', userId);
+  },
+  async loadRegisterAPI(userInfo) {
+    const { email, name, password } = userInfo;
+    const payload = JSON.stringify({ email, name, password });
+
+    try {
+      const response = await fetch('http://localhost:3000/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: payload,
+      }).then((res) => res.json());
+
+      return response;
+    } catch ({ message }) {
+      alert(message);
+    }
+  },
+};
+
+function isUnderMinLengthPassword(password) {
+  return password.length < 10;
+}
+
+function isNameLengthOutOfRange(value) {
+  return value.length < 2 || value.length > 6;
+}
+
+function isDifferent(valueA, valueB) {
+  return valueA !== valueB;
+}
+
+function isEmpty(value) {
+  return value === '';
+}
+
+function hasBlank(value) {
+  return value.includes(' ');
+}
+
+function isInValidPassword(password) {
+  const testFuncs = [isSpecialCase, isUpperCase, isLowerCase, isNumber];
+
+  for (let value of password.split('')) {
+    for (let index in testFuncs) {
+      if (testFuncs[index](value)) {
+        testFuncs.splice(Number(index), 1);
+        break;
+      }
+    }
+    if (testFuncs.length <= 2) return false;
+  }
+
+  return true;
+}
+
+function isSpecialCase(value) {
+  const special = ['!', '@', '#', '$', '%', '^', '&', '* '];
+
+  return special.includes(value);
+}
+
+function isUpperCase(value) {
+  const code = value.charCodeAt(0);
+
+  return code >= 65 && code <= 90;
+}
+
+function isLowerCase(value) {
+  const code = value.charCodeAt(0);
+
+  return code >= 97 && code <= 122;
+}
+
+function isNumber(value) {
+  const code = value.charCodeAt(0);
+
+  return code >= 48 && code <= 57;
+}
+
+export default Auth;
