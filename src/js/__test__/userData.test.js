df3720b82b6d62db09e3138df31ef7e65048eb19
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-04-06 23:25:50 +0900

    test: 이름의 길이가 2~6자 사이가 아니면, 오류를 발생시킨다.

--- src/js/__test__/userData.test.js
+++ src/js/__test__/userData.test.js
@@ -13,4 +13,19 @@ describe('회원가입 유효성 테스트', () => {
 
     expect(() => checkUserDataValidate(newUserData)).toThrowError(ERROR_MESSAGE.EMAIL_IS_INVALID);
   });
+
+  it('이름의 길이가 2~6자 사이가 아니면, 오류를 발생시킨다.', () => {
+    const newUserData = {
+      email: 'test@test.com',
+      name: 't',
+      password: '1q2w3e!!',
+      passwordCheck: '1q2w3e!!',
+      id: null,
+    };
+
+    expect(() => checkUserDataValidate(newUserData)).toThrowError(ERROR_MESSAGE.NAME_LENGTH_IS_INVALID);
+
+    newUserData.name = 'testest';
+    expect(() => checkUserDataValidate(newUserData)).toThrowError(ERROR_MESSAGE.NAME_LENGTH_IS_INVALID);
+  });
 });
