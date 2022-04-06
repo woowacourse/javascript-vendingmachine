a7a2b4e9bc9e93de15e55648af1d5b41f9368595
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-04-06 23:34:23 +0900

    test: 비밀번호가 6~20자 사이의 영문과 숫자, 특수문자 조합이 아니면 오류를 발생시킨다.

--- src/js/__test__/userData.test.js
+++ src/js/__test__/userData.test.js
@@ -28,4 +28,31 @@ describe('회원가입 유효성 테스트', () => {
     newUserData.name = 'testest';
     expect(() => checkUserDataValidate(newUserData)).toThrowError(ERROR_MESSAGE.NAME_LENGTH_IS_INVALID);
   });
+
+  it('비밀번호가 6~20자 사이의 영문과 숫자, 특수문자 조합이 아니면 오류를 발생시킨다.', () => {
+    const newUserData = {
+      email: 'test@test.com',
+      name: 'test',
+      password: '123',
+      passwordCheck: '123',
+      id: null,
+    };
+
+    expect(() => checkUserDataValidate(newUserData)).toThrowError(ERROR_MESSAGE.PASSWORD_IS_INVALID);
+
+    newUserData.password = '123456';
+    newUserData.passwordCheck = '123456';
+
+    expect(() => checkUserDataValidate(newUserData)).toThrowError(ERROR_MESSAGE.PASSWORD_IS_INVALID);
+
+    newUserData.password = '123abc';
+    newUserData.passwordCheck = '123abc';
+
+    expect(() => checkUserDataValidate(newUserData)).toThrowError(ERROR_MESSAGE.PASSWORD_IS_INVALID);
+
+    newUserData.password = 'abc!@#';
+    newUserData.passwordCheck = 'abc!@#';
+
+    expect(() => checkUserDataValidate(newUserData)).toThrowError(ERROR_MESSAGE.PASSWORD_IS_INVALID);
+  });
 });
