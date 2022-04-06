bc2ed5fb87b2b06797ca2e0eafe7cd27d30f2c76
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-04-06 23:24:48 +0900

    test: 잘못된 이메일 주소로 계정 생성을 시도하면, 오류를 발생시킨다.

---
+++ src/js/__test__/userData.test.js
@@ -0,0 +1,16 @@
+import { ERROR_MESSAGE } from '../constants';
+import { checkUserDataValidate } from '../utils/userValidation';
+
+describe('회원가입 유효성 테스트', () => {
+  it('잘못된 이메일 주소로 계정 생성을 시도하면, 오류를 발생시킨다.', () => {
+    const newUserData = {
+      email: 'test@',
+      name: 'test',
+      password: '1q2w3e!!',
+      passwordCheck: '1q2w3e!!',
+      id: null,
+    };
+
+    expect(() => checkUserDataValidate(newUserData)).toThrowError(ERROR_MESSAGE.EMAIL_IS_INVALID);
+  });
+});
