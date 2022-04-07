701e6dae3c1710e6a68fc2ea44005b317cdbc3b6
KIM YEJI <rladpwl0512@gmail.com>
2022-04-08 04:07:57 +0900

    :white_check_mark: test(test): 로그아웃 테스트 구현

--- cypress/integration/user.spec.js
+++ cypress/integration/user.spec.js
@@ -51,6 +51,54 @@ describe('회원가입 테스트', () => {
   });
 });
 
+// describe('회원 정보 수정 테스트', () => {
+//   it('비밀번호가 10자리 미만인 경우, 에러 메시지를 확인할 수 있어야한다.', () => {
+//     cy.get('.profile-button').click();
+//     cy.get('.info-modify').click();
+//     cy.get('#info-name-input').type('로이');
+//     cy.get('#info-password-input').type('under');
+//     cy.get('#info-password-confirm-input').type('under');
+//     cy.get('.info-confirm-button').click();
+
+//     cy.on('window:alert', (str) => {
+//       expect(str).to.equal(ERROR_MESSAGE.IS_NOT_CORRECTED_PASSWORD);
+//     });
+//   });
+//   it('비밀번호에 영어 대문자, 소문자, 숫자 중 2종류 이상이 조합되지 않은 경우, 에러 메시지를 확인할 수 있어야한다.', () => {
+//     cy.get('#info-password-input').clear().type('onlyenglish');
+//     cy.get('#info-password-confirm-input').clear().type('onlyenglish');
+//     cy.get('.info-confirm-button').click();
+//     cy.on('window:alert', (str) => {
+//       expect(str).to.equal(ERROR_MESSAGE.IS_NOT_CORRECTED_PASSWORD);
+//     });
+//   });
+//   it('비밀번호 입력과 비밀번호 확인 입력이 다른 경우, 에러 메시지를 확인할 수 있어야한다.', () => {
+//     cy.get('#info-password-input').clear().type('notmatch0512');
+//     cy.get('#info-password-confirm-input').clear().type('notmatch512');
+//     cy.get('.info-confirm-button').click();
+//     cy.on('window:alert', (str) => {
+//       expect(str).to.equal(ERROR_MESSAGE.IS_NOT_MATCHED_PASSWORD);
+//     });
+//   });
+//   it('회원 정보 수정이 가능하다.', () => {
+//     cy.get('#info-password-input').clear().type('correct0512');
+//     cy.get('#info-password-confirm-input').clear().type('correct0512');
+//     const userInfo = JSON.parse(localStorage.getItem('userAuth'));
+//     console.log(userInfo);
+//     cy.get('.info-confirm-button').click();
+
+//     cy.get('#snackbar').should('have.text', SNACKBAR.MODIFY_SUCCESS);
+//   });
+// });
+
+describe('로그아웃이 가능하다.', () => {
+  it('로그아웃 버튼이 클릭되면, 로그아웃 완료 메시지를 확인할 수 있다.', () => {
+    cy.get('.profile-button').click();
+    cy.get('.logout').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(ERROR_MESSAGE.IS_NOT_MATCHED_PASSWORD);
+    });
+  });
+});
+
 it('로그인이 가능하다.', () => {});
-it('회원 정보 수정이 가능하다.', () => {});
-it('로그아웃이 가능하다.', () => {});
