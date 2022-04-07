01b8ddc41d7115a12d8830b8deedfa84452df03d
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-04-07 14:22:41 +0900

    ✨ : 유저 정보 획득 및 편집 API 추가, 토큰 인증 기능 추가

--- src/Api.ts
+++ src/Api.ts
@@ -1,12 +1,24 @@
-const HOST_NAME = 'https://vendingmachine.compy.life';
+import { USER_SESSION_SETTING } from 'Constants';
+import { getCookie } from 'Utils';
 
-interface IRequest {
-  status: boolean;
-  content: object;
-}
+const HOST_NAME = 'https://api.vendingmachine.compy.life';
 
-const request = async (url: string, option: RequestInit): Promise<IRequest> => {
-  const response: Response = await fetch(HOST_NAME + url, option);
+const request = async (
+  url: string,
+  option: RequestInit,
+  accessTokenEnable?: boolean,
+): Promise<IRequest> => {
+  const fetchOption = option;
+
+  if (accessTokenEnable) {
+    const headersOption = new Headers(option.headers);
+    const accessToken = getCookie(USER_SESSION_SETTING.TOKEN_COOKIE_NAME);
+
+    accessToken && headersOption.set('Authorization', `Bearer ${accessToken}`);
+    fetchOption.headers = headersOption;
+  }
+
+  const response: Response = await fetch(HOST_NAME + url, fetchOption);
   const data = await response.json();
 
   return { status: response.ok, content: data };
@@ -29,27 +41,51 @@ export const requestRegister = async (email: string, name: string, password: str
 };
 
 export const requestLogin = async (email: string, password: string) => {
-  const response = await request('/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
+  const response = await request(
+    '/login',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
     },
-    body: JSON.stringify({
-      email,
-      password,
-    }),
-  });
+    true,
+  );
 
   return response;
 };
 
-export const requestUserInfo = async (id: number, accessToken: string) => {
-  const response = await request(`/users/${id}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
+export const requestUserInfo = async (id: number) => {
+  const response = await request(
+    `/users/${id}`,
+    {
+      method: 'GET',
     },
-  });
+    true,
+  );
+
+  return response;
+};
+
+export const requestProfileEdit = async (id: number, name: string, password: string) => {
+  const response = await request(
+    `/users/${id}`,
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        password,
+      }),
+    },
+    true,
+  );
 
   return response;
 };
