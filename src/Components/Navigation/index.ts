265958a780737e6eb7333906ad552ed8fd515b60
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-04-07 14:25:44 +0900

    🐛 : 메뉴 포커스 효과가 변경되지 않는 버그 수정

--- src/Components/Navigation/index.ts
+++ src/Components/Navigation/index.ts
@@ -1,10 +1,17 @@
-import { convertStringToElement } from 'Utils';
+import { DEFAULT_PAGE } from 'Constants';
+import { $, convertStringToElement, getEntryPath } from 'Utils';
 import Component from 'Components/Abstract';
 import template from './template.html';
 import './styles.scss';
 
 export default class Navigation extends Component {
   template() {
-    return convertStringToElement(template);
+    const $template = convertStringToElement(template);
+
+    const pagePath = getEntryPath() || DEFAULT_PAGE;
+    const $focusNavMenu = $(`.nav-menu[data-route*="${pagePath}"]`, $template);
+    $focusNavMenu && $focusNavMenu.classList.add('selected');
+
+    return $template;
   }
 }
