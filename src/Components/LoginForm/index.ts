47be7bc695f74b7ab751c063dd3b0017f307c6f4
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-04-07 14:24:26 +0900

    ✨ : 로그인 폼 컴포넌트 추가

---
+++ src/Components/LoginForm/index.ts
@@ -0,0 +1,95 @@
+import Component from 'Components/Abstract';
+import UserSessionStore from 'Store/UserSessionStore';
+import ValidationInput from 'Components/@Shared/ValidationInput';
+import {
+  $,
+  addEventDelegate,
+  convertStringToElement,
+  isPassEmailRules,
+  routingEvent,
+  validateUserLogin,
+} from 'Utils';
+
+import template from './template.html';
+
+export default class LoginForm extends Component {
+  subscribeStore = [UserSessionStore];
+
+  $emailInput: HTMLInputElement;
+  $passwordInput: HTMLInputElement;
+
+  constructor(props) {
+    super(props);
+
+    this.renderMethodList = {
+      userSessionEvent: [this.onUserSessionEventListener],
+    };
+  }
+
+  template() {
+    const $fragment = document.createDocumentFragment();
+
+    $fragment.append(
+      this.createChildComponent<IValidationInputProps>(ValidationInput, {
+        name: 'email',
+        type: 'email',
+        label: '이메일',
+        placeholder: 'compy@compy.life',
+        isValidationCheck: isPassEmailRules,
+        errorMessage: '이메일 주소를 정확하게 입력해주세요.',
+      }),
+    );
+
+    $fragment.append(
+      this.createChildComponent<IValidationInputProps>(ValidationInput, {
+        name: 'password',
+        type: 'password',
+        label: '비밀번호',
+        placeholder: '비밀번호를 입력해주세요.',
+      }),
+    );
+
+    const $template = convertStringToElement(template);
+    $('#login-form', $template).prepend($fragment);
+
+    this.$emailInput = $('input[name="email"]', $template);
+    this.$passwordInput = $('input[name="password"]', $template);
+
+    return $template;
+  }
+
+  setEvents() {
+    addEventDelegate(this.$component, '#login-form', {
+      eventType: 'submit',
+      handler: this.onLoginSubmit,
+    });
+  }
+
+  onLoginSubmit = () => {
+    const inputEmail = this.$emailInput.value;
+    const inputPassword = this.$passwordInput.value;
+
+    try {
+      validateUserLogin(inputEmail, inputPassword);
+    } catch (error) {
+      alert(error.message);
+      return;
+    }
+
+    UserSessionStore.login(this.$emailInput.value, this.$passwordInput.value);
+  };
+
+  onUserSessionEventListener = ({ userSessionEvent }: IStoreUniqueState) => {
+    const { isDone, isError, message } = userSessionEvent;
+
+    if (isDone === false && isError === true) {
+      alert(message);
+      UserSessionStore.initSessionEvent();
+    }
+
+    if (isDone === true && isError === false) {
+      routingEvent('purchase');
+      UserSessionStore.initSessionEvent();
+    }
+  };
+}
