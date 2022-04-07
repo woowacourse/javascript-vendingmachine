103a630eff61ef261dc1b53519432bcbf9cc48ea
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-04-07 14:36:08 +0900

    ✨ : 회원 정보 시스템 라우터 연결

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,7 @@ new Router({
   product: () => import('Pages/ProductPage'),
   holdingAmount: () => import('Pages/HoldingAmountPage'),
   purchase: () => import('Pages/ProductPurchasePage'),
+  login: () => import('Pages/LoginPage'),
+  register: () => import('Pages/RegisterPage'),
+  profile: () => import('Pages/UserProfileEditPage'),
 });
