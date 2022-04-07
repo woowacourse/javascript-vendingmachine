01e92b8b47f67452bed348f5d9d43c606cd4740c
airman5573 <rpf5573@kakao.com>
2022-04-07 15:39:13 +0900

    :bug: fix(component): 라벨 이름을 변경한다

--- src/components/purchase-product/insert-money-form.ts
+++ src/components/purchase-product/insert-money-form.ts
@@ -13,9 +13,9 @@ class InsertMoneyForm extends Component {
   template(insertedMoney: number): string {
     return `
       <form onsubmit="return false">
-        <label for="charge-money-input">상품을 구매할 금액을 투입해주세요</label>
+        <label for="insert-money-input">상품을 구매할 금액을 투입해주세요</label>
         <div class="d-flex mb-4">
-          <input placeholder="금액" name="charge-money-input" class="form-control mr-4" />
+          <input placeholder="금액" name="insert-money-input" class="form-control mr-4" />
           <button type="button" class="btn btn-primary">투입</button>
         </div>
         <label class="mb-0">투입한 금액: ${insertedMoney.toLocaleString('ko-kr')}원</label>
@@ -32,7 +32,7 @@ class InsertMoneyForm extends Component {
   }
 
   onClickInsertBtn = () => {
-    const $input = this.querySelector('input[name="charge-money-input"') as HTMLInputElement;
+    const $input = this.querySelector('input[name="insert-money-input"') as HTMLInputElement;
     const money: string = $input.value;
     try {
       this.insertMoney(money);
