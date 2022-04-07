c821786e3ef16359ef61416e169359983cc963b0
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-04-07 16:27:21 +0900

    ♻️ : 이메일 규칙 수정

--- src/Utils/User/validator.ts
+++ src/Utils/User/validator.ts
@@ -1,8 +1,7 @@
 import { isStringLengthInRange } from 'Utils';
 
 export const isPassEmailRules = (email: string): boolean => {
-  const regex =
-    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/g;
+  const regex = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]*$/g;
   return regex.test(email);
 };
 
