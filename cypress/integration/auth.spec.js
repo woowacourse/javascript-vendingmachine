c6b86ddb3b1bfd961a09c02de0f6c115ec45b0dc
2019015414 <2019015414@hanyang.ac.kr>
2022-04-09 06:22:57 +0900

    test: 테스트 순서 변경
    연속된 과정으로 테스트 순서 변경

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -3,10 +3,6 @@ describe('관리자 인증 테스트', () => {
     cy.visit('dist/index.html');
   });
 
-  after(() => {
-    cy.logout();
-  });
-
   it('로그인했을 때 관리자 화면을 볼 수 있어야 한다.', () => {
     const email = 'test1@test1';
     const password = 'Password1234';
@@ -16,16 +12,9 @@ describe('관리자 인증 테스트', () => {
     cy.get('nav').should('be.visible');
   });
 
-  it('로그인 상태에서 로그아웃 버튼을 눌렀을 때 로그아웃이 되어야한다.', () => {
-    cy.logout();
-    cy.get('#login-button').should('be.visible');
-  });
-
   it('로그인 상태에서 회원 정보를 수정할 수 있어야한다.', () => {
     const email = 'test1@test1';
     const password = 'Password1234';
-    cy.get('#login-button').click();
-    cy.login(email, password);
 
     const oldName = 'old';
     cy.modifyInfo(email, oldName, password);
@@ -36,5 +25,10 @@ describe('관리자 인증 테스트', () => {
     cy.modifyInfo(email, newName, password);
 
     cy.get('#manager-name-button').contains('n');
+  });
+
+  it('로그인 상태에서 로그아웃 버튼을 눌렀을 때 로그아웃이 되어야한다.', () => {
+    cy.logout();
+    cy.get('#login-button').should('be.visible');
   });
 });
