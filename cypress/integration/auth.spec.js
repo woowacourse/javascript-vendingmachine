49b7cb969205072b50bbc7b38d62b9e6d94fefca
2019015414 <2019015414@hanyang.ac.kr>
2022-04-09 15:42:10 +0900

    test: test 수정
    after에서 원래 데이터로 변경

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -1,11 +1,22 @@
 describe('관리자 인증 테스트', () => {
+  const email = 'test1@test1';
+  const password = 'Password1234';
+
   beforeEach(() => {
     cy.visit('dist/index.html');
   });
 
+  after(() => {
+    cy.get('#login-button').click();
+    cy.login(email, password);
+
+    const oldName = 'old';
+    cy.modifyInfo(email, oldName, password);
+
+    cy.logout();
+  });
+
   it('로그인했을 때 관리자 화면을 볼 수 있어야 한다.', () => {
-    const email = 'test1@test1';
-    const password = 'Password1234';
     cy.get('nav').should('not.exist');
 
     cy.get('#login-button').click();
@@ -15,14 +26,6 @@ describe('관리자 인증 테스트', () => {
   });
 
   it('로그인 상태에서 회원 정보를 수정할 수 있어야한다.', () => {
-    const email = 'test1@test1';
-    const password = 'Password1234';
-
-    const oldName = 'old';
-    cy.modifyInfo(email, oldName, password);
-
-    cy.get('#manager-name-button').contains('o');
-
     const newName = 'new';
     cy.modifyInfo(email, newName, password);
 
