c8ce479dce32dd971c7640dfc80ffa3fafaf8d0c
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-04-09 20:56:33 +0900

    test: 잔돈 충전 테스트 케이스 추가
    경고 메세지가 뜨는 경우 추가

--- cypress/integration/coinRechargeTab.spec.js
+++ cypress/integration/coinRechargeTab.spec.js
@@ -1,4 +1,4 @@
-import { SELECTOR } from '../../src/ts/constant/selector';
+import { SELECTOR, SELECTOR_NAME } from '../../src/ts/constant/selector';
 
 describe('잔돈 충전 탭 테스트', () => {
   const email = `${Math.random().toString(36).substring(3, 8)}@naaver.com`;
@@ -10,15 +10,48 @@ describe('잔돈 충전 탭 테스트', () => {
     cy.register(email, name, password, confirmPassword);
   });
 
-  const cashInput = 1000;
-
   beforeEach(() => {
     cy.login(email, password);
     cy.get(SELECTOR.COIN_RECHARGE_TAB_BUTTON).click();
   });
 
-  it('금액을 입력하고 충전 버튼을 누르면 잔돈이 충전된다.', () => {
+  it('올바른 금액을 입력하고 충전 버튼을 누르면 총 잔돈이 갱신된다.', () => {
+    const cashInput = 1000;
     cy.chargeCoin(cashInput);
     cy.get(SELECTOR.CHARGED_AMOUNT).should('have.text', cashInput);
+  });
+
+  it('금액을 누적해서 충전하면 누적값만큼 총 잔돈이 갱신된다.', () => {
+    const prevCashInput = 1000;
+    const nextCashInput = 5000;
+    cy.chargeCoin(prevCashInput);
+    cy.chargeCoin(nextCashInput);
+    cy.get(SELECTOR.CHARGED_AMOUNT).should('have.text', prevCashInput + nextCashInput);
+  });
+
+  const numberOfInvalidInput = 1;
+
+  it('숫자가 아닌 값을 입력하고 충전 버튼을 누르면, 경고 메세지가 뜬다.', () => {
+    const cashInput = 'ㅁ';
+    cy.chargeCoin(cashInput);
+    cy.get('.snackbar').should('have.class', SELECTOR_NAME.SHOW);
+  });
+
+  it('10원 미만의 값을 입력하고 충전 버튼을 누르면, 경고 메세지가 뜬다.', () => {
+    const cashInput = 9;
+    cy.chargeCoin(cashInput);
+    cy.checkInvalidInput(numberOfInvalidInput);
+  });
+
+  it('10만원 이상 충전하려 하면, 경고 메세지가 뜬다.', () => {
+    const cashInput = 100001;
+    cy.chargeCoin(cashInput);
+    cy.checkInvalidInput(numberOfInvalidInput);
+  });
+
+  it('10원으로 나누어떨어지지 않는 값을 입력하고 충전 버튼을 누르면, 경고 메세지가 뜬다.', () => {
+    const cashInput = 9999;
+    cy.chargeCoin(cashInput);
+    cy.checkInvalidInput(numberOfInvalidInput);
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,6 +25,7 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 import { HASH } from '../../src/ts/constant/path';
+import { SELECTOR } from '../../src/ts/constant/selector';
 
 Cypress.Commands.add('register', (email, name, password, confirmPassword) => {
   cy.visit(`/${HASH.REGISTER_USER}`);
@@ -43,22 +44,26 @@ Cypress.Commands.add('login', (email, password) => {
 });
 
 Cypress.Commands.add('addItem', (itemName, itemPrice, itemQuantity) => {
-  cy.get('.item-info-input').eq(0).type(itemName);
-  cy.get('.item-info-input').eq(1).type(itemPrice);
-  cy.get('.item-info-input').eq(2).type(itemQuantity);
+  cy.get(SELECTOR.ITEM_INFO_INPUT).eq(0).type(itemName);
+  cy.get(SELECTOR.ITEM_INFO_INPUT).eq(1).type(itemPrice);
+  cy.get(SELECTOR.ITEM_INFO_INPUT).eq(2).type(itemQuantity);
   cy.get('.input-form-button').click();
 });
 
 Cypress.Commands.add('chargeCoin', (cashInput) => {
-  cy.get('.cash-charge-input').type(cashInput);
+  cy.get(SELECTOR.CASH_CHARGE_INPUT).clear().type(cashInput);
   cy.get('.input-form-button').click();
 });
 
 Cypress.Commands.add('insertMoney', (moneyInput) => {
-  cy.get('.item-purchase-input').type(moneyInput);
+  cy.get(SELECTOR.ITEM_PURCHASE_INPUT).type(moneyInput);
   cy.get('.input-form-button').click();
 });
 
 Cypress.Commands.add('purchaseItem', () => {
   cy.get('.purchase-item-button').eq(0).click();
+});
+
+Cypress.Commands.add('checkInvalidInput', (numberOfInvalidInput) => {
+  cy.get('input:invalid').should('have.length', numberOfInvalidInput);
 });
