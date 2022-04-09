7ff7bee9af4be895fa7449423fa9bbbbb898e6a6
2019015414 <2019015414@hanyang.ac.kr>
2022-04-09 22:15:01 +0900

    test: test 수정
    cypress-localstorage-commands를 이용하여 localStorage 관리

--- cypress/integration/user.spec.js
+++ cypress/integration/user.spec.js
@@ -1,14 +1,12 @@
 describe('사용자 테스트', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('dist/index.html');
 
     const email = 'test1@test1';
     const password = 'Password1234';
     cy.get('#login-button').click();
     cy.login(email, password);
-  });
 
-  it('관리자가 추가한 상품들을 사용자가 구매할 수 있어야 한다.', () => {
     // 상품 추가
     const product = {
       name: '상품1',
@@ -21,8 +19,25 @@ describe('사용자 테스트', () => {
     cy.get('#product-quantity-input').type(product.quantity);
     cy.get('#product-add-form button').click();
 
-    cy.logout();
+    // 잔돈 추가
+    cy.get('#charge-menu').click();
+    cy.get('#charge-amount-input').type(1000);
+    cy.get('#charge-form button').click();
 
+    cy.logout();
+    cy.saveLocalStorage();
+  });
+
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.visit('dist/index.html');
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+  });
+
+  it('관리자가 추가한 상품들을 사용자가 구매할 수 있어야 한다.', () => {
     // 상품이 추가되었는 지 확인
     cy.get('#purchase-tbody').find('tr').should('have.length', 1);
 
@@ -36,13 +51,6 @@ describe('사용자 테스트', () => {
   });
 
   it('관리자가 추가한 잔돈의 범위 내에서 투입한 금액을 반환 받을 수 있다.', () => {
-    // 잔돈 추가
-    cy.get('#charge-menu').click();
-    cy.get('#charge-amount-input').type(1000);
-    cy.get('#charge-form button').click();
-
-    cy.logout();
-
     cy.chargeUserAmount(2000);
 
     cy.get('#return-button').click();
