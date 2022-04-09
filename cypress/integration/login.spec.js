54587b8f5f195691dcdf3fa62b87126a68fd1fe4
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-04-09 21:45:45 +0900

    test: 로그인 테스트 케이스 추가

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,6 +1,7 @@
+import { THUMBNAIL_OPTION_VALUE } from '../../src/ts/constant/rule';
 import { SELECTOR } from '../../src/ts/constant/selector';
 
-describe('로그인 및 로그아웃 테스트', () => {
+describe('로그인 테스트', () => {
   const email = `${Math.random().toString(36).substring(3, 8)}@naaver.com`;
   const name = '김이박';
   const password = '1234!@#$asdf';
@@ -15,10 +16,27 @@ describe('로그인 및 로그아웃 테스트', () => {
     cy.get(SELECTOR.THUMBNAIL_BUTTON).should('be.visible');
   });
 
-  it('로그인 후, 썸네일 버튼을 누르고 로그아웃을 선택하면 로그아웃되고, 로그인 버튼으로 바뀐다.', () => {
+  it('로그인에 실패하면, 경고창이 뜬다.', () => {
+    const wrongPassword = '123412341234';
+    cy.login(email, wrongPassword);
+    cy.checkSnackbarVisibility();
+  });
+});
+
+describe('로그아웃 테스트', () => {
+  const email = `${Math.random().toString(36).substring(3, 8)}@naaver.com`;
+  const name = '김이박';
+  const password = '1234!@#$asdf';
+  const confirmPassword = '1234!@#$asdf';
+
+  before(() => {
+    cy.register(email, name, password, confirmPassword);
     cy.login(email, password);
+  });
+
+  it('썸네일 버튼을 누르고 로그아웃을 선택하면 로그아웃되고, 썸네일은 로그인 버튼으로 바뀐다.', () => {
     cy.get(SELECTOR.THUMBNAIL_BUTTON).click();
-    cy.get(SELECTOR.THUMBNAIL_OPTION).select('logout');
+    cy.get(SELECTOR.THUMBNAIL_OPTION).select(THUMBNAIL_OPTION_VALUE.LOGOUT);
     cy.get(SELECTOR.LOGIN_BUTTON).should('be.visible');
   });
 });
