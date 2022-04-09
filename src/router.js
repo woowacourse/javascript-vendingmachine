91dbfbdeea5f7c859ef52523328769b107563504
KIM YEJI <rladpwl0512@gmail.com>
2022-04-10 04:40:51 +0900

    :recycle: refactor(router): 필요없는 조건문 삭제

--- src/router.js
+++ src/router.js
@@ -114,39 +114,33 @@ export const updateUserInfo = () => {
 export const renderManagerView = () => {
   const currentHash = window.location.hash;
 
-  if (currentHash === '#!product-manage') {
-    renderTargetContainer(currentHash);
-  }
-  if (currentHash !== '#!product-manage') {
-    moveToPage('#!product-manage');
-  }
-
   $administratorMenu.show();
   updateUserInfo();
-
   window.onload = () => {
     $('.login-manager').classList.add('hidden');
     $('.profile-manager').classList.remove('hidden');
     $('.profile-manager-menu').classList.add('hidden');
   };
+  if (currentHash === '#!product-manage') {
+    renderTargetContainer(currentHash);
+    return;
+  }
+  moveToPage('#!product-manage');
 };
 
 export const renderUserView = () => {
   const currentHash = window.location.hash;
 
-  if (currentHash === '#!product-purchase') {
-    renderTargetContainer(currentHash);
-  }
-  if (currentHash !== '#!product-purchase') {
-    moveToPage('#!product-purchase');
-  }
-
   $administratorMenu.hide();
-
   window.addEventListener('DOMContentLoaded', () => {
     $('.login-manager').classList.remove('hidden');
     $('.profile-manager').classList.add('hidden');
   });
+  if (currentHash === '#!product-purchase') {
+    renderTargetContainer(currentHash);
+    return;
+  }
+  moveToPage('#!product-purchase');
 };
 
 const renderInitContainer = () => {
