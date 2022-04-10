f5dbeb958250757563e4a7ef6eee3051c25cd85b
Jeongmin Woo <greenblues1190@gmail.com>
2022-04-10 20:49:58 +0900

    chore: server 제거

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   resolve: {
     extensions: ['.js', '.css', '.ts'],
