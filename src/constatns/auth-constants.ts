19413302dbd06e9e15d185c2e485d743b4bc69af
jhy979 <jhy979@gmail.com>
2022-04-10 16:12:17 +0900

    feat: status code를 확인하여 API error check

--- src/constatns/auth-constants.ts
+++ src/constatns/auth-constants.ts
@@ -11,3 +11,12 @@ export const MEMBER = {
   NO_REPEATED_CHAR: '같은 문자를 4번 이상 사용하실 수 없습니다.',
   NO_SPACE_REQUIRED: '비밀번호는 공백 없이 입력해주세요.',
 };
+
+export const API_ERROR = {
+  NON_EXIST_MEMBER: '존재하지 않는 유저입니다.',
+  INVALID_API: '요청을 잘못하신 것 같습니다. API를 다시 확인해주세요.',
+  INVALID_LOGIN_INFO: '로그인 정보가 올바르지 않습니다. 다시 로그인해주세요.',
+  NON_AUTHORIZED_USER: '데이터를 요청할 수 있는 권한이 없습니다.',
+  CANNOT_FIND_PAGE: '페이지를 찾지 못했습니다.',
+  DEFAULT_ERROR: '에러가 발생했습니다. 관리자에게 문의해주세요.',
+};
