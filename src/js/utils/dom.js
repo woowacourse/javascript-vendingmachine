5f26ca43f5ca5603acdb40c7d2cba8ee1191ac8e
JUDONGHYEOK <judonghyeok@gmail.com>
2022-04-10 23:42:15 +0900

    fix: snackBar animation 활성화 상태에서 event 재 발생시 텍스트만 바뀌는 문제 해결
    - snackBar animation을 requestAnimationFrame을 사용하여 변경

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -5,7 +5,6 @@ export function selectDom(selector, baseElement = document) {
 export function createMainElement(template) {
   const mainElement = document.createElement('main');
   mainElement.insertAdjacentHTML('beforeend', template);
-
   return mainElement;
 }
 
@@ -15,11 +14,56 @@ export function createElementByTemplate(type, template) {
   return element;
 }
 
-export function generateSnackBar(text) {
-  const snackBarContainer = document.getElementById('snackbar');
-  snackBarContainer.className = 'show';
-  snackBarContainer.textContent = text;
-  setTimeout(() => {
-    snackBarContainer.className = snackBarContainer.className.replace('show', '');
-  }, 3000);
+function fadeOutAnimation(target, elasped, duration) {
+  const node = target;
+  node.style.transform = `translateY(${-30 + (30 * elasped) / duration}px)`;
+  node.style.opacity = 1 - elasped / duration;
+  if (elasped >= duration) {
+    node.classList.add('hide');
+  }
 }
+
+function fadeInAnimation(target, elasped, duration) {
+  const node = target;
+  node.classList.remove('hide');
+  node.style.transform = `translateY(-${(30 * elasped) / duration}px)`;
+  node.style.opacity = elasped / duration;
+}
+
+function animationCallBack(target, duration, cb) {
+  let start;
+  return function callBack(timestamp) {
+    if (!start) {
+      start = timestamp;
+    }
+    const elasped = timestamp - start;
+    cb(target, elasped, duration);
+    if (elasped < duration) {
+      requestAnimationFrame(callBack);
+    }
+  };
+}
+
+// eslint-disable-next-line max-lines-per-function
+export function snackBar() {
+  const snackBarContainer = document.getElementById('snackbar');
+  let timeoutID = null;
+  let isGenerated = false;
+  return function generate(text) {
+    if (isGenerated) {
+      clearTimeout(timeoutID);
+      isGenerated = false;
+      generate(text);
+      return;
+    }
+    isGenerated = true;
+    snackBarContainer.textContent = text;
+    requestAnimationFrame(animationCallBack(snackBarContainer, 500, fadeInAnimation));
+    timeoutID = setTimeout(() => {
+      requestAnimationFrame(animationCallBack(snackBarContainer, 500, fadeOutAnimation));
+      isGenerated = false;
+    }, 3000);
+  };
+}
+
+export const generateSnackBar = snackBar();
