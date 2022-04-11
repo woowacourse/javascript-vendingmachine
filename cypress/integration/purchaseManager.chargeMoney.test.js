cb27fb28f13901a75bb26a9ada557d2f84ab12f1
최광희 <polyckh@gmail.com>
2022-04-11 22:15:45 +0900

    test: 상품 구매 테스트 파일 생성

---
+++ cypress/integration/purchaseManager.chargeMoney.test.js
@@ -0,0 +1,41 @@
+describe('상품 구매 테스트', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:9000/');
+    cy.login();
+    cy.changeTab('purchase');
+  });
+
+  it('구매 금액을 충전할 수 있다.', () => {
+    const chargeMoney = 5000;
+
+    cy.chargeMoney(chargeMoney);
+
+    cy.get('.money-for-purchase-section__total-money').should(
+      'have.text',
+      chargeMoney
+    );
+  });
+
+  it('구매 금액은 누적해서 충전할 수 있다.', () => {
+    const chargeMoney = 5000;
+    const chargeMoney2 = 10000;
+
+    cy.chargeMoney(chargeMoney);
+    cy.chargeMoney(chargeMoney2);
+
+    cy.get('.money-for-purchase-section__total-money').should(
+      'have.text',
+      chargeMoney + chargeMoney2
+    );
+  });
+
+  it('남은 동전은 반환할 수 있다.', () => {
+    const chargeMoney = 5000;
+
+    cy.chargeMoney(chargeMoney);
+
+    cy.get('.money-for-purchase-return-button').click();
+
+    cy.get('.money-for-purchase-section__total-money').should('have.text', 0);
+  });
+});
