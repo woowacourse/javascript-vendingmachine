ee4bf0cdf9c6d3a7b119ed9c0d9212192e55b844
KIM YEJI <rladpwl0512@gmail.com>
2022-04-12 01:16:23 +0900

    :bug: fix(ui): snackbar를 재호출 시 이전 snackbar의 setTimeout 삭제

--- src/utils/showSnackbar.js
+++ src/utils/showSnackbar.js
@@ -1,12 +1,23 @@
+/* eslint-disable no-void */
 import { $ } from './dom';
+
+let timer;
 
 const showSnackbar = (message) => {
   const $snackbar = $('#snackbar');
-
   $snackbar.textContent = message;
-  $snackbar.classList.toggle('show');
-  setTimeout(() => {
-    $snackbar.classList.toggle('show');
+
+  if (timer) {
+    clearTimeout(timer); // 기존 timer 제거
+
+    $snackbar.classList.remove('show');
+    void $snackbar.offsetWidth;
+  }
+  $snackbar.classList.add('show');
+
+  timer = setTimeout(() => {
+    $snackbar.classList.remove('show');
+    timer = null;
   }, 3000);
 };
 
