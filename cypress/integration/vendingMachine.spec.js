afc459f41a3ac952b112ece51c2a7875a90665b4
jswith <typingjs@gmail.com>
2022-04-11 17:24:20 +0900

    :white_check_mark: test(test): 테스트 코드 수정

--- cypress/integration/vendingMachine.spec.js
+++ cypress/integration/vendingMachine.spec.js
@@ -25,46 +25,28 @@ describe('관리자 인증 UI 테스트', () => {
   });
 
   it('[예외] 회원가입 화면에서 비밀번호, 비밀번호 확인값이 일치하지 않으면 스낵바로 메시지를 표시한다.', () => {
-    cy.get('#signup-form__email').type('test@gmail.com');
-    cy.get('#signup-form__name').type('민초');
-    cy.get('#signup-form__password').type('1234567a');
-    cy.get('#signup-form__password-confirm').type('1234567b');
-
+    cy.typeSignupForm('test@gmail', '민초', '1234567a', '1234567b');
     cy.get('#signup-form__button').click();
 
     cy.get('#snack-bar').should('have.text', ERROR_MESSAGE.PASSWORD_CONFIRM);
   });
 
   it('[예외] 회원가입 화면에서 비밀번호의 조건이 맞지 않으면 스낵바로 메시지를 표시한다.', () => {
-    cy.get('#signup-form__email').clear();
-    cy.get('#signup-form__name').clear();
-    cy.get('#signup-form__password').clear();
-    cy.get('#signup-form__password-confirm').clear();
-
-    cy.get('#signup-form__email').type('test@gmail.com');
-    cy.get('#signup-form__name').type('민초');
-    cy.get('#signup-form__password').type('12345678');
-    cy.get('#signup-form__password-confirm').type('12345678');
-
+    cy.signupFormClear();
+    cy.typeSignupForm('test@gmail', '민초', '12345678', '12345678');
     cy.get('#signup-form__button').click();
 
     cy.get('#snack-bar').should('have.text', ERROR_MESSAGE.NOT_MATCH_PASSWORD_REGEXP);
   });
 
   it('회원가입 화면에서 입력 값의 조건이 맞으면 회원가입에 성공하고 로그인 페이지로 이동한다.', () => {
-    cy.get('#signup-form__email').clear();
-    cy.get('#signup-form__name').clear();
-    cy.get('#signup-form__password').clear();
-    cy.get('#signup-form__password-confirm').clear();
+    cy.intercept('POST', 'https://js-vendingmachine-server.herokuapp.com/signup').as('signupRequest');
 
-    cy.get('#signup-form__email').type(`${Math.random().toString(36).substring(8)}@gmail.com`);
-    cy.get('#signup-form__name').type('민초');
-    cy.get('#signup-form__password').type('1234567a');
-    cy.get('#signup-form__password-confirm').type('1234567a');
-
+    cy.signupFormClear();
+    cy.typeSignupForm(`${Math.random().toString(36).substring(8)}@gmail.com`, '민초', '1234567a', '1234567a');
     cy.get('#signup-form__button').click();
 
-    cy.wait(1000);
+    cy.wait('@signupRequest');
 
     cy.location().should((loc) => {
       expect(loc.href).to.eq('http://localhost:9000/javascript-vendingmachine/signin');
@@ -72,22 +54,20 @@ describe('관리자 인증 UI 테스트', () => {
   });
 
   it('[예외] 로그인 화면에서 입력한 값이 서버에 존재하지 않는 경우 스낵바로 메시지로 표시한다.', () => {
-    cy.get('#signin-form__email').type('notexist@notexist.com');
-    cy.get('#signin-form__password').type('1234567a');
-
+    cy.typeSigninForm('notexist@notexist.com', '1234567a');
     cy.get('#signin-form__button').click();
+
     cy.get('#snack-bar').should('have.text', ERROR_MESSAGE.NOT_MATCH_USER_INFO);
   });
 
   it('로그인에 성공하면 자판기 관리화면으로 이동한다.', () => {
-    cy.get('#signin-form__email').clear();
-    cy.get('#signin-form__password').clear();
-    cy.get('#signin-form__email').type('mincho@woowa.com');
-    cy.get('#signin-form__password').type('1234567a');
+    cy.intercept('POST', 'https://js-vendingmachine-server.herokuapp.com/signin').as('signinRequest');
 
+    cy.signinFormClear();
+    cy.typeSigninForm('mincho@woowa.com', '1234567a');
     cy.get('#signin-form__button').click();
 
-    cy.wait(1000);
+    cy.wait('@signinRequest');
 
     cy.location().should((loc) => {
       expect(loc.href).to.eq('http://localhost:9000/javascript-vendingmachine/');
@@ -113,15 +93,14 @@ describe('사용자 UI 테스트', () => {
   });
 
   it('정상 금액을 투입하면 투입한 금액이 화면에 반영된다.', () => {
-    cy.get('.purchase-form__money-input').type(CONFIGURATION.INPUT.MAX);
-    cy.get('.purchase-form__money-input-button').click();
+    cy.inputMoney(CONFIGURATION.INPUT.MAX);
+
     cy.get('.purchase-form__money-input-amount').should('have.text', '10,000');
   });
 
   it('[예외] 투입 금액 한도를 초과하면 메시지를 표시한다.', () => {
     cy.get('.purchase-form__money-input').clear();
-    cy.get('.purchase-form__money-input').type(CONFIGURATION.INPUT.MAX + 1);
-    cy.get('.purchase-form__money-input-button').click();
+    cy.inputMoney(CONFIGURATION.INPUT.MAX + 1);
 
     cy.get('.purchase-form').then(($purchaseForm) => expect($purchaseForm[0].checkValidity()).to.be.false);
   });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,28 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('signupFormClear', () => {
+  cy.get('#signup-form__email').clear();
+  cy.get('#signup-form__name').clear();
+  cy.get('#signup-form__password').clear();
+  cy.get('#signup-form__password-confirm').clear();
+});
+
+Cypress.Commands.add('signinFormClear', () => {
+  cy.get('#signin-form__email').clear();
+  cy.get('#signin-form__password').clear();
+});
+
+Cypress.Commands.add('typeSignupForm', (email, name, password, passwordConfirm) => {
+  cy.get('#signup-form__email').type(email);
+  cy.get('#signup-form__name').type(name);
+  cy.get('#signup-form__password').type(password);
+  cy.get('#signup-form__password-confirm').type(passwordConfirm);
+});
+
+Cypress.Commands.add('typeSigninForm', (email, password) => {
+  cy.get('#signin-form__email').type(email);
+  cy.get('#signin-form__password').type(password);
+});
+
+Cypress.Commands.add('inputMoney', (money) => {
+  cy.get('.purchase-form__money-input').type(money);
+  cy.get('.purchase-form__money-input-button').click();
+});
