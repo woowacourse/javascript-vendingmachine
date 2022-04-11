7ae74ea5498907d06964ac940f8789e731849fbd
2019015414 <2019015414@hanyang.ac.kr>
2022-04-11 13:56:21 +0900

    refactor: API 수정

--- src/js/utils/API.js
+++ src/js/utils/API.js
@@ -1,40 +1,14 @@
-export const signup = async (nameValue, emailValue, passwordValue) => {
-  return await fetch('https://json-web-server-ronci.herokuapp.com/register', {
-    method: 'POST',
-    body: JSON.stringify({
-      name: nameValue,
-      email: emailValue,
-      password: passwordValue,
-    }),
+const fetcher = async (method, path, body) =>
+  fetch(`https://json-web-server-ronci.herokuapp.com/${path}`, {
+    method,
+    body: JSON.stringify(body),
     headers: {
       'Content-Type': 'application/json',
     },
   });
-};
 
-export const login = async (loginEmailValue, loginPasswordValue) => {
-  return await fetch('https://json-web-server-ronci.herokuapp.com/login', {
-    method: 'POST',
-    body: JSON.stringify({
-      email: loginEmailValue,
-      password: loginPasswordValue,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-};
-
-export const modifyUserInfo = async (nameValue, emailValue, passwordValue, id) => {
-  await fetch(`https://json-web-server-ronci.herokuapp.com/users/${id}`, {
-    method: 'PATCH',
-    body: JSON.stringify({
-      name: nameValue,
-      email: emailValue,
-      password: passwordValue,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-};
+export const signup = (name, email, password) =>
+  fetcher('POST', 'register', { name, email, password });
+export const login = (email, password) => fetcher('POST', 'login', { email, password });
+export const modifyUserInfo = (name, email, password, id) =>
+  fetcher('PATCH', `users/${id}`, { name, email, password });
