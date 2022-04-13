fbc357ec227cdd67d15305015973728cb83f60e5
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-04-13 18:16:50 +0900

    :recycle: refactor: combine routes
    page routes와 tab routes를 합침

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,11 @@
 import { ERROR_MESSAGE, BASE_URL } from './constants';
 import { $, $$, showSnackbar } from './utils';
 
-interface Router {
-  path: string;
+interface Route {
+  path: string | string[];
   component: Element;
   permission: boolean;
+  children?: Route[];
 }
 
 const nav = $('.nav');
@@ -20,7 +21,7 @@ const auth = $('.auth');
 
 const isGranted = (pathname: string) => {
   const isLogin = !!localStorage.getItem('accessToken');
-  const element = [...pageRouters, ...tabRouters].find((router) => router.path === pathname);
+  const element = [...routes, ...routes[0].children].find((router) => router.path === pathname);
 
   if (!element) return;
 
@@ -43,40 +44,33 @@ const render = (path: string) => {
     return;
   }
 
-  renderPage(window.location.pathname);
-  renderTab(window.location.pathname);
+  routing(path);
 };
 
-const renderPage = (path: string) => {
-  const isVendingMachinePage =
-    path === BASE_URL + '/' || path === BASE_URL + '/charge' || path === BASE_URL + '/management';
+const routing = (path: string) => {
+  const targetRoute = routes.find((route) => route.path.includes(path));
 
-  pageRouters.forEach((router) => router.component.classList.toggle('hidden', router.path !== path));
-  pageRouters.find((router) => router.path === path)?.component.classList.remove('hidden');
+  routes.forEach((route) => route.component.classList.toggle('hidden', route.path !== path));
+  targetRoute.component.classList.remove('hidden');
 
-  auth.classList.toggle('hidden', !isVendingMachinePage);
-  $('vending-machine-page').classList.toggle('hidden', !isVendingMachinePage);
-};
+  auth.classList.toggle('hidden', !!!targetRoute.children);
+  targetRoute.children?.forEach((tab) => tab.component.classList.toggle('hidden', tab.path !== path));
 
-const renderTab = (path: string) => {
   $$('.focus-button').forEach((button) => button.classList.remove('focus-button'));
   $(`[route='${path}']`, nav)?.classList.add('focus-button');
-
-  const cur = tabRouters.find((route) => route.path === path)?.component ?? $('purchase-tab');
-  const prevs = tabRouters.filter((route) => route.path !== path);
-
-  cur.classList.remove('hidden');
-  prevs.forEach((p: Router) => p.component.classList.add('hidden'));
 };
 
-const tabRouters: Router[] = [
-  { path: BASE_URL + '/', component: $('purchase-tab'), permission: true },
-  { path: BASE_URL + '/charge', component: $('charge-tab'), permission: false },
-  { path: BASE_URL + '/management', component: $('product-management'), permission: false },
-];
-
-const pageRouters: Router[] = [
-  { path: BASE_URL + '/', component: $('vending-machine-page'), permission: true },
+const routes: Route[] = [
+  {
+    path: [BASE_URL + '/', BASE_URL + '/charge', BASE_URL + '/management'],
+    component: $('vending-machine-page'),
+    permission: true,
+    children: [
+      { path: BASE_URL + '/', component: $('purchase-tab'), permission: true },
+      { path: BASE_URL + '/charge', component: $('charge-tab'), permission: false },
+      { path: BASE_URL + '/management', component: $('product-management'), permission: false },
+    ],
+  },
   { path: BASE_URL + '/login', component: $('login-page'), permission: true },
   { path: BASE_URL + '/signup', component: $('signup-page'), permission: true },
   { path: BASE_URL + '/profile', component: $('profile-edit-page'), permission: false },
