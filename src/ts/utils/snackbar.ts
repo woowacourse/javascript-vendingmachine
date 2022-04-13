f62fe292a6db03f5385303eb0d8f6d932b41b1fc
woose28 <dldntmdsla@naver.com>
2022-04-14 02:15:00 +0900

    fix: throttle로 인한 스낵바 이슈 해결

--- src/ts/utils/snackbar.ts
+++ src/ts/utils/snackbar.ts
@@ -2,19 +2,33 @@ import { selectDom } from '.';
 import { ID, CLASS } from '../constant/selector';
 
 class Snackbar {
-  private static timerId: ReturnType<typeof setTimeout>;
+  private static timerId: ReturnType<typeof setTimeout> = null;
 
   private static snackbar: HTMLElement | null = selectDom(`#${ID.SNACKBAR}`);
+
+  private static SNACKBAR_DURATION = 3000;
 
   static show(message: string) {
     this.snackbar.textContent = message;
 
-    if (this.timerId) return;
-    this.snackbar.classList.toggle(CLASS.SHOW);
+    // 기존에 설정된 타이머가 있다면 타이머를 제거하고
+    // 새로운 타이머를 설정
+    if (this.timerId) {
+      clearTimeout(this.timerId);
+
+      this.setSnackbarTimer();
+      return;
+    }
+
+    this.snackbar.classList.add(CLASS.SHOW);
+    this.setSnackbarTimer();
+  }
+
+  private static setSnackbarTimer() {
     this.timerId = setTimeout(() => {
-      this.snackbar.classList.toggle(CLASS.SHOW);
+      this.snackbar.classList.remove(CLASS.SHOW);
       this.timerId = null;
-    }, 3000);
+    }, this.SNACKBAR_DURATION);
   }
 }
 
